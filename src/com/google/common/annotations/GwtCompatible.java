4211cf3a0e11ba822a37ebd45a149cd959015f8c
guava.mirrorbot@gmail.com <guava.mirrorbot@gmail.com@8138a162-5c33-11de-8abc-d1c337b90d21>
2010-08-30 16:01:59 +0000

    - Make GwtCompatible and GwtIncompatible @Documented in the open source version. - Update links to the documentations.
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=197273
    
    
    git-svn-id: https://guava-libraries.googlecode.com/svn/trunk@96 8138a162-5c33-11de-8abc-d1c337b90d21

--- src/com/google/common/annotations/GwtCompatible.java
+++ src/com/google/common/annotations/GwtCompatible.java
@@ -16,6 +16,7 @@
 
 package com.google.common.annotations;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -62,7 +63,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.CLASS)
 @Target({ ElementType.TYPE, ElementType.METHOD })
-
+@Documented
 @GwtCompatible
 public @interface GwtCompatible {
 
@@ -70,7 +71,7 @@ public @interface GwtCompatible {
    * When {@code true}, the annotated type or the type of the method return
    * value is GWT serializable.
    *
-   * @see <a href="http://code.google.com/docreader/#p=google-web-toolkit-doc-1-5&t=DevGuideSerializableTypes">
+   * @see <a href="http://code.google.com/webtoolkit/doc/latest/DevGuideServerCommunication.html#DevGuideSerializableTypes">
    *     Documentation about GWT serialization</a>
    */
   boolean serializable() default false;
@@ -80,7 +81,7 @@ public @interface GwtCompatible {
    * source (also known as super-source) is different from the implementation
    * used by the JVM.
    *
-   * @see <a href="http://code.google.com/docreader/#p=google-web-toolkit-doc-1-5&t=DevGuideModuleXml">
+   * @see <a href="http://code.google.com/webtoolkit/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
    *     Documentation about GWT emulated source</a>
    */
   boolean emulated() default false;
--- src/com/google/common/annotations/GwtIncompatible.java
+++ src/com/google/common/annotations/GwtIncompatible.java
@@ -16,6 +16,7 @@
 
 package com.google.common.annotations;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -36,7 +37,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.CLASS)
 @Target({ ElementType.TYPE, ElementType.METHOD, ElementType.FIELD })
-
+@Documented
 @GwtCompatible
 public @interface GwtIncompatible {
 
