c458d9964069e3aad104888ddf214e22ac26c727
guava.mirrorbot@gmail.com <guava.mirrorbot@gmail.com@8138a162-5c33-11de-8abc-d1c337b90d21>
2010-09-01 22:11:57 +0000

    Handle some edge cases in createParentDirs.
    R=kevinb
    DELTA=62  (54 added, 1 deleted, 7 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=201259
    
    
    git-svn-id: https://guava-libraries.googlecode.com/svn/trunk@102 8138a162-5c33-11de-8abc-d1c337b90d21

--- src/com/google/common/io/Files.java
+++ src/com/google/common/io/Files.java
@@ -446,9 +446,18 @@ public final class Files {
    */
   public static void createParentDirs(File file) throws IOException {
     File parent = file.getCanonicalFile().getParentFile();
-    // TODO(kevinb): return if parent is null
+    if (parent == null) {
+      /*
+       * The given directory is a filesystem root. All zero of its ancestors
+       * exist. This doesn't mean that the root itself exists -- consider x:\ on
+       * a Windows machine without such a drive -- or even that the caller can
+       * create it, but this method makes no such guarantees even for non-root
+       * files.
+       */
+      return;
+    }
     parent.mkdirs();
-    if (!parent.exists()) { // TODO(kevinb): change to isDirectory
+    if (!parent.isDirectory()) {
       throw new IOException("Unable to create parent directories of " + file);
     }
   }
