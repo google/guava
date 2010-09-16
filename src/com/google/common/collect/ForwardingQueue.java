6499316bc379dc1309b6a20eba375a269fd8918a
guava.mirrorbot@gmail.com <guava.mirrorbot@gmail.com@8138a162-5c33-11de-8abc-d1c337b90d21>
2010-09-16 20:43:42 +0000

    Provide default implementations for some methods in ForwardingQueue
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=224287
    
    
    git-svn-id: https://guava-libraries.googlecode.com/svn/trunk@121 8138a162-5c33-11de-8abc-d1c337b90d21

--- src/com/google/common/collect/ForwardingQueue.java
+++ src/com/google/common/collect/ForwardingQueue.java
@@ -16,8 +16,10 @@
 
 package com.google.common.collect;
 
+import com.google.common.annotations.Beta;
 import com.google.common.annotations.GwtCompatible;
 
+import java.util.NoSuchElementException;
 import java.util.Queue;
 
 /**
@@ -26,7 +28,18 @@ import java.util.Queue;
  * queue as desired per the <a
  * href="http://en.wikipedia.org/wiki/Decorator_pattern">decorator pattern</a>.
  *
+ * <p><b>Warning:</b> The methods of {@code ForwardingQueue} forward
+ * <b>indiscriminately</b> to the methods of the delegate. For example,
+ * overriding {@link #add} alone <b>will not</b> change the behavior of {@link
+ * #offer} which can lead to unexpected behavior. In this case, you should
+ * override {@code offer} as well, either providing your own implementation, or
+ * delegating to the provided {@code standardOffer} method.
+ *
+ * <p>The {@code standard} methods are not guaranteed to be thread-safe, even
+ * when all of the methods that they depend on are thread-safe.
+ *
  * @author Mike Bostock
+ * @author Louis Wasserman
  * @since 2 (imported from Google Collections Library)
  */
 @GwtCompatible
@@ -56,5 +69,50 @@ public abstract class ForwardingQueue<E> extends ForwardingCollection<E>
 
   public E element() {
     return delegate().element();
+  }
+
+  /**
+   * A sensible definition of {@link #offer} in terms of {@link #add}. If you
+   * override {@link #add}, you may wish to override {@link #offer} to forward
+   * to this implementation.
+   * 
+   * @since 7
+   */
+  @Beta protected boolean standardOffer(E e) {
+    try {
+      return add(e);
+    } catch (IllegalStateException caught) {
+      return false;
+    }
+  }
+
+  /**
+   * A sensible definition of {@link #peek} in terms of {@link #element}. If you
+   * override {@link #element}, you may wish to override {@link #peek} to
+   * forward to this implementation.
+   * 
+   * @since 7
+   */
+  @Beta protected E standardPeek() {
+    try {
+      return element();
+    } catch (NoSuchElementException caught) {
+      return null;
+    }
+  }
+
+  /**
+   * A sensible definition of {@link #poll} in terms of {@link #remove}. If you
+   * override {@link #remove}, you may wish to override {@link #poll} to forward
+   * to this implementation.
+   * 
+   * @since 7
+   */
+  @Beta protected E standardPoll() {
+    try {
+      return remove();
+    } catch (NoSuchElementException caught) {
+      return null;
+    }
   }
 }
