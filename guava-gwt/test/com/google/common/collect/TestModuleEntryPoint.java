1bf14f9852e41756fd658638bcdb9682e637d57c
guava.mirrorbot@gmail.com <guava.mirrorbot@gmail.com@8138a162-5c33-11de-8abc-d1c337b90d21>
2011-06-24 21:50:56 +0000

    Fix TestModuleEntryPoint.
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=2389
    
    
    git-svn-id: https://guava-libraries.googlecode.com/svn/trunk@490 8138a162-5c33-11de-8abc-d1c337b90d21

--- guava-gwt/test/com/google/common/collect/TestModuleEntryPoint.java
+++ guava-gwt/test/com/google/common/collect/TestModuleEntryPoint.java
@@ -18,116 +18,13 @@ package com.google.common.collect;
 
 import com.google.gwt.core.client.EntryPoint;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
- * A dummy entry point that accesses all GWT classes in
- * {@code com.google.common.collect}.
+ * A dummy entry point of the test module.
  *
  * @author Hayward Chan
  */
-@SuppressWarnings("unchecked")
 public class TestModuleEntryPoint implements EntryPoint {
 
-  @Override
-  public void onModuleLoad() {
-    // TODO: Auto generate this list.
-    // Files covered by GWT_SRCS
-    @SuppressWarnings("unused")
-    List<Class<?>> allClasses = Arrays.<Class<?>>asList(
-        AbstractIndexedListIterator.class,
-        AbstractIterator.class,
-        AbstractListMultimap.class,
-        AbstractMapEntry.class,
-        AbstractMultimap.class,
-        AbstractMultiset.class,
-        AbstractSetMultimap.class,
-        AbstractSortedSetMultimap.class,
-        BiMap.class,
-        ByFunctionOrdering.class,
-        ClassToInstanceMap.class,
-        Collections2.class,
-        ComparisonChain.class,
-        ComparatorOrdering.class,
-        CompoundOrdering.class,
-        ComputationException.class,
-        Constraint.class,
-        Constraints.class,
-        EmptyImmutableListMultimap.class,
-        EmptyImmutableMultiset.class,
-        EmptyImmutableSetMultimap.class,
-        ExplicitOrdering.class,
-        ForwardingCollection.class,
-        ForwardingConcurrentMap.class,
-        ForwardingIterator.class,
-        ForwardingList.class,
-        ForwardingListIterator.class,
-        ForwardingListMultimap.class,
-        ForwardingMap.class,
-        ForwardingMapEntry.class,
-        ForwardingMultimap.class,
-        ForwardingMultiset.class,
-        ForwardingObject.class,
-        ForwardingQueue.class,
-        ForwardingSet.class,
-        ForwardingSetMultimap.class,
-        ForwardingSortedMap.class,
-        ForwardingSortedSet.class,
-        ForwardingSortedSetMultimap.class,
-        GenericMapMaker.class,
-        Hashing.class,
-        ImmutableEntry.class,
-        ImmutableListMultimap.class,
-        LexicographicalOrdering.class,
-        ListMultimap.class,
-        Lists.class,
-        MapConstraint.class,
-        MapConstraints.class,
-        MapDifference.class,
-        Maps.class,
-        Multimap.class,
-        Multiset.class,
-        Multisets.class,
-        NaturalOrdering.class,
-        NullsFirstOrdering.class,
-        NullsLastOrdering.class,
-        ObjectArrays.class,
-        Ordering.class,
-        PeekingIterator.class,
-        ReverseNaturalOrdering.class,
-        ReverseOrdering.class,
-        SetMultimap.class,
-        SortedSetMultimap.class,
-        UnmodifiableIterator.class,
-        UsingToStringOrdering.class,
-
-        // Classes covered by :generated_supersource
-        AbstractBiMap.class,
-        AbstractMapBasedMultiset.class,
-        ArrayListMultimap.class,
-        EnumBiMap.class,
-        EnumHashBiMap.class,
-        EnumMultiset.class,
-        HashBiMap.class,
-        HashMultimap.class,
-        HashMultiset.class,
-        ImmutableListMultimap.class,
-        ImmutableMultimap.class,
-        ImmutableMultiset.class,
-        ImmutableSetMultimap.class,
-        Iterables.class,
-        Iterators.class,
-        LinkedHashMultimap.class,
-        LinkedHashMultiset.class,
-        LinkedListMultimap.class,
-        Multimaps.class,
-        ObjectArrays.class,
-        Sets.class,
-        Synchronized.class,
-        TreeMultimap.class,
-        TreeMultiset.class);
+  @Override public void onModuleLoad() {
   }
-
-  // Reference that make sure an RPC interface is referenced.
 }
