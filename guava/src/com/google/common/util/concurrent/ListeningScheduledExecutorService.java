ab3bd3a78d8acc6922f328269b4810c1a56541ce
guava.mirrorbot@gmail.com <guava.mirrorbot@gmail.com@8138a162-5c33-11de-8abc-d1c337b90d21>
2011-07-13 18:44:39 +0000

    rollback
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=2560
    
    
    git-svn-id: https://guava-libraries.googlecode.com/svn/trunk@526 8138a162-5c33-11de-8abc-d1c337b90d21

--- guava/src/com/google/common/util/concurrent/ListeningScheduledExecutorService.java
+++ guava/src/com/google/common/util/concurrent/ListeningScheduledExecutorService.java
@@ -18,16 +18,18 @@ package com.google.common.util.concurrent;
 
 import com.google.common.annotations.Beta;
 
-import java.util.concurrent.Callable;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
 /**
  * A {@link ScheduledExecutorService} that returns {@link ListenableFuture}
- * instances from its {@code ExecutorService} methods.
- * To create an instance from an existing {@link ScheduledExecutorService}, 
- * call {@link MoreExecutors#listeningDecorator(ScheduledExecutorService)}.
+ * instances from its {@code ExecutorService} methods.  Futures returned by the
+ * {@code schedule*} methods, by contrast, need not implement {@code
+ * ListenableFuture}.  (To create an instance from an existing {@link
+ * ScheduledExecutorService}, call {@link
+ * MoreExecutors#listeningDecorator(ScheduledExecutorService)}.
+ *
+ * <p>TODO(cpovirk): make at least the one-time schedule() methods return a
+ * ListenableFuture, too? But then we'll need ListenableScheduledFuture...
  *
  * @author Chris Povirk
  * @since Guava release 10
@@ -35,27 +37,4 @@ import java.util.concurrent.TimeUnit;
 @Beta
 public interface ListeningScheduledExecutorService
     extends ScheduledExecutorService, ListeningExecutorService {
-  /**
-   * Helper interface to implement both {@link ListenableFuture} and
-   * {@link ScheduledFuture}. 
-   */
-  public interface ListenableScheduledFuture<V> 
-      extends ScheduledFuture<V>, ListenableFuture<V> {
-  }
-
-  @Override
-  public ListenableScheduledFuture<?> schedule(
-      Runnable command, long delay, TimeUnit unit);
-
-  @Override
-  public <V> ListenableScheduledFuture<V> schedule(
-      Callable<V> callable, long delay, TimeUnit unit);
-
-  @Override
-  public ListenableScheduledFuture<?> scheduleAtFixedRate(
-      Runnable command, long initialDelay, long period, TimeUnit unit);
-
-  @Override
-  public ListenableScheduledFuture<?> scheduleWithFixedDelay(
-      Runnable command, long initialDelay, long delay, TimeUnit unit);
 }
