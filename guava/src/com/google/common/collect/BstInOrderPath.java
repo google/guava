dcaab87e6bc95a8e8162fecbc4a757ef534e062d
guava.mirrorbot@gmail.com <guava.mirrorbot@gmail.com@8138a162-5c33-11de-8abc-d1c337b90d21>
2011-07-27 21:29:45 +0000

    Unify BstInOrderPath.nextInOrder(), prevInOrder() to one function with a BstSide argument
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=2751
    
    
    git-svn-id: https://guava-libraries.googlecode.com/svn/trunk@573 8138a162-5c33-11de-8abc-d1c337b90d21

--- guava/src/com/google/common/collect/BstInOrderPath.java
+++ guava/src/com/google/common/collect/BstInOrderPath.java
@@ -15,8 +15,6 @@
 package com.google.common.collect;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.collect.BstSide.LEFT;
-import static com.google.common.collect.BstSide.RIGHT;
 
 import com.google.common.annotations.GwtCompatible;
 import com.google.common.base.Optional;
@@ -56,109 +54,73 @@ final class BstInOrderPath<N extends BstNode<?, N>> extends BstPath<N, BstInOrde
     return new BstInOrderPath<N>(tip.getChild(side), side, path);
   }
 
-  private final BstSide side;
+  private final BstSide sideExtension;
   private transient Optional<BstInOrderPath<N>> prevInOrder;
   private transient Optional<BstInOrderPath<N>> nextInOrder;
 
-  private BstInOrderPath(N tip, @Nullable BstSide side, @Nullable BstInOrderPath<N> tail) {
+  private BstInOrderPath(
+      N tip, @Nullable BstSide sideExtension, @Nullable BstInOrderPath<N> tail) {
     super(tip, tail);
-    this.side = side;
-    assert (side == null) == (tail == null);
+    this.sideExtension = sideExtension;
+    assert (sideExtension == null) == (tail == null);
   }
 
-  private Optional<BstInOrderPath<N>> computeNextInOrder() {
-    if (getTip().hasChild(RIGHT)) {
-      BstInOrderPath<N> path = extension(this, RIGHT);
-      while (path.getTip().hasChild(LEFT)) {
-        path = extension(path, LEFT);
+  private Optional<BstInOrderPath<N>> computeNextInOrder(BstSide side) {
+    if (getTip().hasChild(side)) {
+      BstInOrderPath<N> path = extension(this, side);
+      BstSide otherSide = side.other();
+      while (path.getTip().hasChild(otherSide)) {
+        path = extension(path, otherSide);
       }
       return Optional.of(path);
     } else {
       BstInOrderPath<N> current = this;
-      while (current.side == RIGHT) {
+      while (current.sideExtension == side) {
         current = current.getPrefix();
       }
       current = current.prefixOrNull();
-      if (current != null) {
-        current.prevInOrder = Optional.of(this);
-      }
       return Optional.fromNullable(current);
     }
   }
 
-  private Optional<BstInOrderPath<N>> computePrevInOrder() {
-    if (getTip().hasChild(LEFT)) {
-      BstInOrderPath<N> path = extension(this, LEFT);
-      while (path.getTip().hasChild(RIGHT)) {
-        path = extension(path, RIGHT);
-      }
-      return Optional.of(path);
-    } else {
-      BstInOrderPath<N> current = this;
-      while (current.side == LEFT) {
-        current = current.getPrefix();
-      }
-      current = current.prefixOrNull();
-      if (current != null) {
-        current.nextInOrder = Optional.of(this);
-      }
-      return Optional.fromNullable(current);
+  private Optional<BstInOrderPath<N>> nextInOrder(BstSide side) {
+    Optional<BstInOrderPath<N>> result;
+    switch (side) {
+      case LEFT:
+        result = prevInOrder;
+        return (result == null) ? prevInOrder = computeNextInOrder(side) : result;
+      case RIGHT:
+        result = nextInOrder;
+        return (result == null) ? nextInOrder = computeNextInOrder(side) : result;
+      default:
+        throw new AssertionError();
     }
   }
 
-  private Optional<BstInOrderPath<N>> nextInOrder() {
-    Optional<BstInOrderPath<N>> result = nextInOrder;
-    return (result == null) ? nextInOrder = computeNextInOrder() : result;
-  }
-
-  private Optional<BstInOrderPath<N>> prevInOrder() {
-    Optional<BstInOrderPath<N>> result = prevInOrder;
-    return (result == null) ? prevInOrder = computePrevInOrder() : result;
-  }
-
   /**
-   * Returns {@code true} if there is a next path in an in-order traversal.
+   * Returns {@code true} if there is a next path in an in-order traversal in the given direction.
    */
-  public boolean hasNext() {
-    return nextInOrder().isPresent();
+  public boolean hasNext(BstSide side) {
+    return nextInOrder(side).isPresent();
   }
 
   /**
-   * Returns {@code true} if there is a previous path in an in-order traversal.
-   */
-  public boolean hasPrev() {
-    return prevInOrder().isPresent();
-  }
-
-  /**
-   * Returns the next path in an in-order traversal.
+   * Returns the next path in an in-order traversal in the given direction.
    *
    * @throws NoSuchElementException if this would be the last path in an in-order traversal
    */
-  public BstInOrderPath<N> next() {
-    if (!hasNext()) {
+  public BstInOrderPath<N> next(BstSide side) {
+    if (!hasNext(side)) {
       throw new NoSuchElementException();
     }
-    return nextInOrder().get();
-  }
-
-  /**
-   * Returns the previous path in an in-order traversal.
-   *
-   * @throws NoSuchElementException if this would be the first path in an in-order traversal
-   */
-  public BstInOrderPath<N> prev() {
-    if (!hasPrev()) {
-      throw new NoSuchElementException();
-    }
-    return prevInOrder().get();
+    return nextInOrder(side).get();
   }
 
   /**
    * Returns the direction this path went in relative to its tail path, or {@code null} if this
    * path has no tail.
    */
-  public BstSide getSide() {
-    return side;
+  public BstSide getSideOfExtension() {
+    return sideExtension;
   }
 }
