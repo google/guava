8c3cc7e67132f8dcaae4bd214736a8ddf6611769
Charles Fry <fry@google.com>
2011-09-20 15:58:40 -0400

    Clone of Louis's UnsignedLong(s) documentation CL. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=24073780

--- guava/src/com/google/common/primitives/UnsignedLongs.java
+++ guava/src/com/google/common/primitives/UnsignedLongs.java
@@ -1,16 +1,14 @@
 /*
  * Copyright (C) 2011 The Guava Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
@@ -19,26 +17,27 @@ package com.google.common.primitives;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import com.google.common.annotations.Beta;
-import com.google.common.annotations.GwtCompatible;
-
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Comparator;
 
+import com.google.common.annotations.Beta;
+import com.google.common.annotations.GwtCompatible;
+
 /**
  * Static utility methods pertaining to {@code long} primitives that interpret values as
  * <i>unsigned</i> (that is, any negative value {@code x} is treated as the positive value
- * {@code 2^64 + x}). The methods for which signedness is not an issue are in {@link Longs}, as well
- * as signed versions of methods for which signedness is an issue.
- *
+ * {@code 2^64 + x}). The methods for which signedness is not an issue are in {@link Longs}, as
+ * well as signed versions of methods for which signedness is an issue.
+ * 
  * <p>In addition, this class provides several static methods for converting a {@code long} to a
- * {@code String} and a {@code String} to a {@code long} that treat the long as an unsigned number.
- *
+ * {@code String} and a {@code String} to a {@code long} that treat the {@code long} as an unsigned
+ * number.
+ * 
  * <p>Users of these utilities must be <i>extremely careful</i> not to mix up signed and unsigned
- * long values. When possible, it is recommended that the {@link UnsignedLong} wrapper class be
- * used, at a small efficiency penalty, to enforce the distinction in the type system.
- *
+ * {@code long} values. When possible, it is recommended that the {@link UnsignedLong} wrapper 
+ * class be used, at a small efficiency penalty, to enforce the distinction in the type system.
+ * 
  * @author Louis Wasserman
  * @author Brian Milch
  * @author Colin Evans
@@ -52,23 +51,22 @@ public final class UnsignedLongs {
   public static final long MAX_VALUE = -1L; // Equivalent to 2^64 - 1
 
   /**
-   * A (self-inverse) bijection which converts the ordering on unsigned longs
-   * to the ordering on longs, that is, {@code a <= b} as unsigned longs if and
-   * only if {@code rotate(a) <= rotate(b)} as signed longs.
+   * A (self-inverse) bijection which converts the ordering on unsigned longs to the ordering on
+   * longs, that is, {@code a <= b} as unsigned longs if and only if {@code rotate(a) <= rotate(b)}
+   * as signed longs.
    */
   private static long flip(long a) {
     return a ^ Long.MIN_VALUE;
   }
 
   /**
-   * Compares the two specified {@code long} values, treating them as unsigned
-   * values between {@code 0} and {@code 2^64 - 1} inclusive.
-   *
+   * Compares the two specified {@code long} values, treating them as unsigned values between
+   * {@code 0} and {@code 2^64 - 1} inclusive.
+   * 
    * @param a the first unsigned {@code long} to compare
    * @param b the second unsigned {@code long} to compare
-   * @return a negative value if {@code a} is less than {@code b}; a positive
-   *         value if {@code a} is greater than {@code b}; or zero if they are
-   *         equal
+   * @return a negative value if {@code a} is less than {@code b}; a positive value if {@code a} is
+   *         greater than {@code b}; or zero if they are equal
    */
   public static int compare(long a, long b) {
     return Longs.compare(flip(a), flip(b));
@@ -76,10 +74,10 @@ public final class UnsignedLongs {
 
   /**
    * Returns the least value present in {@code array}.
-   *
+   * 
    * @param array a <i>nonempty</i> array of unsigned {@code long} values
-   * @return the value present in {@code array} that is less than or equal to
-   *         every other value in the array according to {@link #compare}
+   * @return the value present in {@code array} that is less than or equal to every other value in
+   *         the array according to {@link #compare}
    * @throws IllegalArgumentException if {@code array} is empty
    */
   public static long min(long... array) {
@@ -96,10 +94,10 @@ public final class UnsignedLongs {
 
   /**
    * Returns the greatest value present in {@code array}.
-   *
+   * 
    * @param array a <i>nonempty</i> array of unsigned {@code long} values
-   * @return the value present in {@code array} that is greater than or equal
-   *         to every other value in the array according to {@link #compare}
+   * @return the value present in {@code array} that is greater than or equal to every other value
+   *         in the array according to {@link #compare}
    * @throws IllegalArgumentException if {@code array} is empty
    */
   public static long max(long... array) {
@@ -115,12 +113,11 @@ public final class UnsignedLongs {
   }
 
   /**
-   * Returns a string containing the supplied unsigned {@code long} values
-   * separated by {@code separator}. For example, {@code join("-", 1, 2, 3)}
-   * returns the string {@code "1-2-3"}.
-   *
-   * @param separator the text that should appear between consecutive values in
-   *     the resulting string (but not at the start or end)
+   * Returns a string containing the supplied unsigned {@code long} values separated by
+   * {@code separator}. For example, {@code join("-", 1, 2, 3)} returns the string {@code "1-2-3"}.
+   * 
+   * @param separator the text that should appear between consecutive values in the resulting
+   *        string (but not at the start or end)
    * @param array an array of unsigned {@code long} values, possibly empty
    */
   public static String join(String separator, long... array) {
@@ -139,19 +136,18 @@ public final class UnsignedLongs {
   }
 
   /**
-   * Returns a comparator that compares two {@code long} arrays
-   * lexicographically. That is, it compares, using {@link
-   * #compare(long, long)}), the first pair of values that follow any
-   * common prefix, or when one array is a prefix of the other, treats the
-   * shorter array as the lesser. For example,
-   * {@code [] < [1L] < [1L, 2L] < [2L]}.  Values are treated as unsigned.
-   *
-   * <p>The returned comparator is inconsistent with {@link
-   * Object#equals(Object)} (since arrays support only identity equality), but
-   * it is consistent with {@link Arrays#equals(long[], long[])}.
-   *
-   * @see <a href="http://en.wikipedia.org/wiki/Lexicographical_order">
-   *     Lexicographical order article at Wikipedia</a>
+   * Returns a comparator that compares two {@code long} arrays lexicographically. That is, it
+   * compares, using {@link #compare(long, long)}), the first pair of values that follow any common
+   * prefix, or when one array is a prefix of the other, treats the shorter array as the lesser.
+   * For example, {@code [] < [1L] < [1L, 2L] < [2L]}. Values are treated as unsigned.
+   * 
+   * <p>
+   * The returned comparator is inconsistent with {@link Object#equals(Object)} (since arrays
+   * support only identity equality), but it is consistent with
+   * {@link Arrays#equals(long[], long[])}.
+   * 
+   * @see <a href="http://en.wikipedia.org/wiki/Lexicographical_order">Lexicographical order
+   *      article at Wikipedia</a>
    */
   public static Comparator<long[]> lexicographicalComparator() {
     return LexicographicalComparator.INSTANCE;
@@ -173,11 +169,11 @@ public final class UnsignedLongs {
   }
 
   /**
-   * Returns dividend / divisor, where the dividend and divisor are treated as
-   * unsigned 64-bit quantities.
-   *
+   * Returns dividend / divisor, where the dividend and divisor are treated as unsigned 64-bit
+   * quantities.
+   * 
    * @param dividend the dividend (numerator)
-   * @param divisor  the divisor (denominator)
+   * @param divisor the divisor (denominator)
    * @throws ArithmeticException if divisor is 0
    */
   public static long divide(long dividend, long divisor) {
@@ -195,10 +191,9 @@ public final class UnsignedLongs {
     }
 
     /*
-     * Otherwise, approximate the quotient, check, and correct if necessary.
-     * Our approximation is guaranteed to be either exact or one less than the
-     * correct value. This follows from fact that floor(floor(x)/i) ==
-     * floor(x/i) for any real x and integer i != 0.  The proof is not
+     * Otherwise, approximate the quotient, check, and correct if necessary. Our approximation is
+     * guaranteed to be either exact or one less than the correct value. This follows from fact
+     * that floor(floor(x)/i) == floor(x/i) for any real x and integer i != 0. The proof is not
      * quite trivial.
      */
     long quotient = ((dividend >>> 1) / divisor) << 1;
@@ -207,14 +202,14 @@ public final class UnsignedLongs {
   }
 
   /**
-   * Returns dividend % divisor, where the dividend and divisor are treated as
-   * unsigned 64-bit quantities.
-   *
+   * Returns dividend % divisor, where the dividend and divisor are treated as unsigned 64-bit
+   * quantities.
+   * 
    * @param dividend the dividend (numerator)
-   * @param divisor  the divisor (denominator)
+   * @param divisor the divisor (denominator)
    * @throws ArithmeticException if divisor is 0
    */
-  static long remainder(long dividend, long divisor) {
+  public static long remainder(long dividend, long divisor) {
     if (divisor < 0) { // i.e., divisor >= 2^63:
       if (compare(dividend, divisor) < 0) {
         return dividend; // dividend < divisor
@@ -229,10 +224,9 @@ public final class UnsignedLongs {
     }
 
     /*
-     * Otherwise, approximate the quotient, check, and correct if necessary.
-     * Our approximation is guaranteed to be either exact or one less than the
-     * correct value. This follows from fact that floor(floor(x)/i) ==
-     * floor(x/i) for any real x and integer i != 0.  The proof is not
+     * Otherwise, approximate the quotient, check, and correct if necessary. Our approximation is
+     * guaranteed to be either exact or one less than the correct value. This follows from fact
+     * that floor(floor(x)/i) == floor(x/i) for any real x and integer i != 0. The proof is not
      * quite trivial.
      */
     long quotient = ((dividend >>> 1) / divisor) << 1;
@@ -241,30 +235,23 @@ public final class UnsignedLongs {
   }
 
   /**
-   * Returns the unsigned long value represented by the given decimal string.
-   *
-   * @throws NumberFormatException if the string does not contain a valid
-   * unsigned integer, or if the value represented is too large to fit in an
-   * unsigned long.
-   * @throws NullPointerException if {@code s} is null
+   * Returns the unsigned {@code long} value represented by the given decimal string.
+   * 
+   * @throws NumberFormatException if the string does not contain a valid unsigned {@code long}
+   *         value
    */
   public static long parseUnsignedLong(String s) {
     return parseUnsignedLong(s, 10);
   }
 
   /**
-   * Returns the unsigned long value represented by a string with the given
-   * radix.
-   *
-   * @param s the string containing the unsigned long representation to be
-   * parsed.
-   * @param radix the radix to use while parsing {@code s}; must be between
-   * Character.MIN_RADIX and Character.MAX_RADIX.
-   * @throws NumberFormatException if the string does not contain a valid
-   * unsigned integer with the given radix, or if the value represented is
-   * too large to fit in an unsigned long.  Also thrown if supplied radix is
-   * invalid.
-   * @throws NullPointerException if {@code s} is null
+   * Returns the unsigned {@code long} value represented by a string with the given radix.
+   * 
+   * @param s the string containing the unsigned {@code long} representation to be parsed.
+   * @param radix the radix to use while parsing {@code s}
+   * @throws NumberFormatException if the string does not contain a valid unsigned {@code long}
+   *         with the given radix, or if {@code radix} is not between {@link Character#MIN_RADIX}
+   *         and {@link Character#MAX_RADIX}.
    */
   public static long parseUnsignedLong(String s, int radix) {
     checkNotNull(s);
@@ -292,11 +279,10 @@ public final class UnsignedLongs {
   }
 
   /**
-   * Returns true if (current * radix) + digit is a number too large to be
-   * represented by an unsigned long. This is useful for detecting overflow
-   * while parsing a string representation of a number. Does not verify whether
-   * supplied radix is valid, passing an invalid radix will give undefined
-   * results or an ArrayIndexOutOfBoundsException.
+   * Returns true if (current * radix) + digit is a number too large to be represented by an
+   * unsigned long. This is useful for detecting overflow while parsing a string representation of
+   * a number. Does not verify whether supplied radix is valid, passing an invalid radix will give
+   * undefined results or an ArrayIndexOutOfBoundsException.
    */
   private static boolean overflowInParse(long current, int digit, int radix) {
     if (current >= 0) {
@@ -322,18 +308,17 @@ public final class UnsignedLongs {
   }
 
   /**
-   * Returns a string representation of {@code x} for the given radix, where
-   * {@code x} is treated as unsigned.
-   *
+   * Returns a string representation of {@code x} for the given radix, where {@code x} is treated
+   * as unsigned.
+   * 
    * @param x the value to convert to a string.
    * @param radix the radix to use while working with {@code x}
-   * @throws IllegalArgumentException if {@code radix} is not between
-   *         {@link Character#MIN_RADIX} and {@link Character#MAX_RADIX}.
+   * @throws IllegalArgumentException if {@code radix} is not between {@link Character#MIN_RADIX}
+   *         and {@link Character#MAX_RADIX}.
    */
   public static String toString(long x, int radix) {
     checkArgument(radix >= Character.MIN_RADIX && radix <= Character.MAX_RADIX,
-        "radix (%s) must be between Character.MIN_RADIX and Character.MAX_RADIX",
-        radix);
+        "radix (%s) must be between Character.MIN_RADIX and Character.MAX_RADIX", radix);
     if (x == 0) {
       // Simply return "0"
       return "0";
@@ -348,14 +333,14 @@ public final class UnsignedLongs {
         long bot = (x & 0xffffffffl) + ((top % radix) << 32);
         top /= radix;
         while ((bot > 0) || (top > 0)) {
-          buf[--i] = Character.forDigit((int)(bot % radix), radix);
+          buf[--i] = Character.forDigit((int) (bot % radix), radix);
           bot = (bot / radix) + ((top % radix) << 32);
           top /= radix;
         }
       } else {
         // Simple modulo/division approach
         while (x > 0) {
-          buf[--i] = Character.forDigit((int)(x % radix), radix);
+          buf[--i] = Character.forDigit((int) (x % radix), radix);
           x /= radix;
         }
       }
