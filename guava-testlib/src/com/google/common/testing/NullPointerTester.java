556976f81c236be7798e5d065a0dae09ca0cc1d3
Chris Povirk <cpovirk@google.com>
2011-12-16 14:02:04 -0500

    Fixing bug in NullPointerTester, where it fails spuriously for methods with no default value for @Nullable parameters. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=26289693

--- guava-testlib/src/com/google/common/testing/NullPointerTester.java
+++ guava-testlib/src/com/google/common/testing/NullPointerTester.java
@@ -292,7 +292,7 @@ public final class NullPointerTester {
     for (int i = 0; i < types.length; i++) {
       if (i != indexOfParamToSetToNull) {
         params[i] = defaults.get(types[i]);
-        if (!parameterIsPrimitiveOrNullable(func, indexOfParamToSetToNull)) {
+        if (!parameterIsPrimitiveOrNullable(func, i)) {
           Assert.assertTrue("No default value found for " + types[i].getName(),
               params[i] != null);
         }
