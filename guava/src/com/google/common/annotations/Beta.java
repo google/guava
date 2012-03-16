05ba41fc65d2bad97a35cc5f368e6785d287c7fa
Kurt Kluever <kak@google.com>
2012-03-16 18:15:21 -0400

    Dump some more blahblah into the @Beta annotation to try to clear up some persistent misconceptions. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=28454189

--- guava/src/com/google/common/annotations/Beta.java
+++ guava/src/com/google/common/annotations/Beta.java
@@ -22,14 +22,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Signifies that a public API (public class, method or field) is subject to
- * incompatible changes, or even removal, in a future release. An API bearing
- * this annotation is exempt from any compatibility guarantees made by its
- * containing library.
- *
- * @author Kevin Bourrillion
- */
 @Retention(RetentionPolicy.CLASS)
 @Target({
     ElementType.ANNOTATION_TYPE,
