56790c18cd659c9696e56bc7988b5723b506c34d
Chris Povirk <cpovirk@google.com>
2012-03-19 21:04:02 -0400

    Re-word Beta comment to work around MOE bug that strips anything with the word [] in it. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=28517528

--- guava/src/com/google/common/annotations/Beta.java
+++ guava/src/com/google/common/annotations/Beta.java
@@ -22,6 +22,22 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Signifies that a public API (public class, method or field) is subject to
+ * incompatible changes, or even removal, in a future release. An API bearing
+ * this annotation is exempt from any compatibility guarantees made by its
+ * containing library. Note that the presence of this annotation implies nothing
+ * about the quality or performance of the API in question, only the fact that
+ * it is not "API-frozen."
+ *
+ * <p>It is generally safe for <i>applications</i> to depend on beta APIs, at
+ * the cost of some extra work during upgrades. However it is generally
+ * inadvisable for <i>libraries</i> (which get included on users' CLASSPATHs,
+ * outside the library developers' control) to do so.
+ *
+ *
+ * @author Kevin Bourrillion
+ */
 @Retention(RetentionPolicy.CLASS)
 @Target({
     ElementType.ANNOTATION_TYPE,
