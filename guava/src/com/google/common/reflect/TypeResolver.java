1b23f4e098bcc4de59b5993b5577049bd2eebdd7
Chris Povirk <cpovirk@google.com>
2012-03-22 16:02:38 -0400

    Applied missing Apache license stuff to the files I forgot in previous round (oops) ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=28579696

--- guava/src/com/google/common/reflect/TypeResolver.java
+++ guava/src/com/google/common/reflect/TypeResolver.java
@@ -1,4 +1,18 @@
-// Copyright 2009 Google Inc. All Rights Reserved.
+/*
+ * Copyright (C) 2009 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.google.common.reflect;
 
@@ -27,7 +41,7 @@ import javax.annotation.Nullable;
  * parameter to the actual type argument used by the subclass, provided the type
  * argument is carried on the class declaration.
  * 
- * @author benyu@google.com (Jige Yu)
+ * @author Ben Yu
  */
 class TypeResolver {
   
