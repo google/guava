b2d7980cf1764e43ff5a21236a93e70909c244a4
Gregory Kick <gak@google.com>
2012-04-10 14:03:40 -0500

    Fix Splitter to handle patterns that have empty matches, like boundaries and look-behind/ahead patterns, without exploding. From Louis: http://codereview.appspot.com/5901060/ ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=29124245

--- guava/src/com/google/common/base/Splitter.java
+++ guava/src/com/google/common/base/Splitter.java
@@ -497,8 +497,15 @@ public final class Splitter {
     }
 
     @Override protected String computeNext() {
+      /*
+       * The returned string will be from the end of the last match to the
+       * beginning of the next one. nextStart is the start position of the
+       * returned substring, while offset is the place to start looking for a
+       * separator.
+       */
+      int nextStart = offset;
       while (offset != -1) {
-        int start = offset;
+        int start = nextStart;
         int end;
 
         int separatorPosition = separatorStart(offset);
@@ -509,6 +516,20 @@ public final class Splitter {
           end = separatorPosition;
           offset = separatorEnd(separatorPosition);
         }
+        if (offset == nextStart) {
+          /*
+           * This occurs when some pattern has an empty match, even if it
+           * doesn't match the empty string -- for example, if it requires
+           * lookahead or the like. The offset must be increased to look for
+           * separators beyond this point, without changing the start position
+           * of the next returned substring -- so nextStart stays the same.
+           */
+          offset++;
+          if (offset >= toSplit.length()) {
+            offset = -1;
+          }
+          continue;
+        }
 
         while (start < end && trimmer.matches(toSplit.charAt(start))) {
           start++;
@@ -518,6 +539,8 @@ public final class Splitter {
         }
 
         if (omitEmptyStrings && start == end) {
+          // Don't include the (unused) separator in next split string.
+          nextStart = offset;
           continue;
         }
 
