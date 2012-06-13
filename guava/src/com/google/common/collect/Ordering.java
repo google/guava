ee0a59572b9185e82474919c32a2d6a858efc33a
Kurt Kluever <kak@google.com>
2012-06-13 16:06:12 -0400

    Attempt to improve Ordering.allEqual() documentation. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=30975026

--- guava/src/com/google/common/collect/Ordering.java
+++ guava/src/com/google/common/collect/Ordering.java
@@ -34,6 +34,7 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.SortedMap;
 import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.annotation.Nullable;
@@ -174,9 +175,26 @@ public abstract class Ordering<T> implements Comparator<T> {
   // Ordering<Object> singletons
 
   /**
-   * Returns an ordering which treats all values as equal. This can be useful in conjunction with
-   * nullsFirst() or nullsLast() to treat all non-null objects as equal, or to make sorting a
-   * no-op.
+   * Returns an ordering which treats all values as equal, indicating "no
+   * ordering." Passing this ordering to any <i>stable</i> sort algorithm
+   * results in no change to the order of elements. Note especially that {@link
+   * #sortedCopy} and {@link #immutableSortedCopy} are stable, and in the
+   * returned instance these are implemented by simply copying the source list.
+   *
+   * <p>Example: <pre>   {@code
+   *
+   *   Ordering.allEqual().nullsLast().sortedCopy(
+   *       asList(t, null, e, s, null, t, null))}</pre>
+   *
+   * Assuming {@code t}, {@code e} and {@code s} are non-null, this returns
+   * {@code [t, e, s, t, null, null, null]} regardlesss of the true comparison
+   * order of those three values (which might not even implement {@link
+   * Comparable} at all).
+   *
+   * <p><b>Warning:</b> by definition, this comparator is not <i>consistent with
+   * equals</i> (as defined {@linkplain Comparator here}). Avoid its use in
+   * APIs, such as {@link TreeSet#TreeSet(Comparator)}, where such consistency
+   * is expected.
    *
    * <p>The returned comparator is serializable.
    */
