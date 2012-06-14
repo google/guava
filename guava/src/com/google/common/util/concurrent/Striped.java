aba87338f827f9ee79a69f3638933c54a8afaeb4
Kurt Kluever <kak@google.com>
2012-06-14 14:05:31 -0400

    Fix copyright notice, other Guava things on Striped. From Louis: http://codereview.appspot.com/6306084/ ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=31038311

--- guava/src/com/google/common/util/concurrent/Striped.java
+++ guava/src/com/google/common/util/concurrent/Striped.java
@@ -1,4 +1,18 @@
-// Copyright 2011 Google Inc. All Rights Reserved.
+/*
+ * Copyright (C) 2011 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.google.common.util.concurrent;
 
@@ -58,9 +72,10 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  * choosing either of these extremes, {@code Striped} allows the user to trade between required
  * concurrency and memory footprint. For example, if a set of tasks are CPU-bound, one could easily
  * create a very compact {@code Striped<Lock>} of {@code availableProcessors() * 4} stripes,
- * instead of possibly thousands locks which could be created in a {@code Map<K, Lock>} structure.
+ * instead of possibly thousands of locks which could be created in a {@code Map<K, Lock>}
+ * structure.
  *
- * @author andreou@google.com (Dimitris Andreou)
+ * @author Dimitris Andreou
  */
 @Beta
 public abstract class Striped<L> {
