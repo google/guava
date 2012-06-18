3f72054d6997c9a80e83cb2657dd8c96e6cf1fac
Chris Povirk <cpovirk@google.com>
2012-06-18 10:17:58 -0400

    Replace another instance of supertype-finding code in EventBus with TypeToken. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=31082777

--- guava/src/com/google/common/eventbus/EventBus.java
+++ guava/src/com/google/common/eventbus/EventBus.java
@@ -23,15 +23,13 @@ import com.google.common.base.Throwables;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Multimaps;
 import com.google.common.collect.SetMultimap;
-import com.google.common.collect.Sets;
+import com.google.common.reflect.TypeToken;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -155,34 +153,16 @@ public class EventBus {
   };
 
   /**
-   * A thread-safe cache for flattenHierarch(). The Class class is immutable.
+   * A thread-safe cache for flattenHierarchy(). The Class class is immutable.
    */
-  private LoadingCache<Class<?>, Set<Class<?>>> flattenHierarchyCache =
+  private final LoadingCache<Class<?>, Set<Class<?>>> flattenHierarchyCache =
       CacheBuilder.newBuilder()
           .weakKeys()
           .build(new CacheLoader<Class<?>, Set<Class<?>>>() {
+            @SuppressWarnings({"unchecked", "rawtypes"}) // safe cast
             @Override
             public Set<Class<?>> load(Class<?> concreteClass) throws Exception {
-              List<Class<?>> parents = Lists.newLinkedList();
-              Set<Class<?>> classes = Sets.newHashSet();
-
-              parents.add(concreteClass);
-
-              while (!parents.isEmpty()) {
-                Class<?> clazz = parents.remove(0);
-                classes.add(clazz);
-
-                Class<?> parent = clazz.getSuperclass();
-                if (parent != null) {
-                  parents.add(parent);
-                }
-
-                for (Class<?> iface : clazz.getInterfaces()) {
-                  parents.add(iface);
-                }
-              }
-
-              return classes;
+              return (Set) TypeToken.of(concreteClass).getTypes().rawTypes();
             }
           });
 
