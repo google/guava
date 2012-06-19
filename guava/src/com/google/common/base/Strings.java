3d6b39c2ccdec6a7866f894d1ffa394f1967dfd5
Chris Povirk <cpovirk@google.com>
2012-06-19 16:15:59 -0400

    Remove @Beta from Strings.common{Pre,Suf}fix. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=31180208

--- guava/src/com/google/common/base/Strings.java
+++ guava/src/com/google/common/base/Strings.java
@@ -19,7 +19,6 @@ package com.google.common.base;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import com.google.common.annotations.Beta;
 import com.google.common.annotations.GwtCompatible;
 import com.google.common.annotations.VisibleForTesting;
 
@@ -186,7 +185,6 @@ public final class Strings {
    *
    * @since 11.0
    */
-  @Beta
   public static String commonPrefix(CharSequence a, CharSequence b) {
     checkNotNull(a);
     checkNotNull(b);
@@ -210,7 +208,6 @@ public final class Strings {
    *
    * @since 11.0
    */
-  @Beta
   public static String commonSuffix(CharSequence a, CharSequence b) {
     checkNotNull(a);
     checkNotNull(b);
