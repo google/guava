ada07d6dc56625211c39c8e8a2ba36f4330dbedb
Colin Decker <cgdecker@google.com>
2012-07-02 12:39:22 -0400

    Remove the use of MoreAsserts from the AbstractServiceTest.  Apparently it breaks the maven build. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=31573774

--- guava-tests/test/com/google/common/util/concurrent/AbstractServiceTest.java
+++ guava-tests/test/com/google/common/util/concurrent/AbstractServiceTest.java
@@ -24,7 +24,6 @@ import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.Service.Listener;
 import com.google.common.util.concurrent.Service.State;
-import com.google.testing.util.MoreAsserts;
 
 import junit.framework.TestCase;
 
@@ -643,7 +642,7 @@ public class AbstractServiceTest extends TestCase {
 
     @Override public synchronized void starting() {
       assertTrue(stateHistory.isEmpty());
-      MoreAsserts.assertNotEqual(State.NEW, service.state());
+      assertNotSame(State.NEW, service.state());
       stateHistory.add(State.STARTING);
     }
 
@@ -652,7 +651,7 @@ public class AbstractServiceTest extends TestCase {
       stateHistory.add(State.RUNNING);
       assertTrue(service.start().isDone());
       assertEquals(State.RUNNING, service.startAndWait());
-      MoreAsserts.assertNotEqual(State.STARTING, service.state());
+      assertNotSame(State.STARTING, service.state());
     }
 
     @Override public synchronized void stopping(State from) {
@@ -662,7 +661,7 @@ public class AbstractServiceTest extends TestCase {
         assertTrue(service.start().isDone());
         assertEquals(State.STOPPING, service.startAndWait());
       }
-      MoreAsserts.assertNotEqual(from, service.state());
+      assertNotSame(from, service.state());
     }
 
     @Override public synchronized void terminated(State from) {
