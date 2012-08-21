fb8059d8e01b69600ec5f489543742cd09ddae63
Kurt Kluever <kak@google.com>
2012-08-21 00:12:58 -0400

    Improve Funnel javadocs about using an enum. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=32878801

--- guava/src/com/google/common/hash/Funnel.java
+++ guava/src/com/google/common/hash/Funnel.java
@@ -20,6 +20,20 @@ import java.io.Serializable;
 
 /**
  * An object which can send data from an object of type {@code T} into a {@code PrimitiveSink}.
+ *
+ * <p>Note that serialization of {@linkplain BloomFilter bloom filters} requires the proper
+ * serialization of funnels. When possible, it is recommended that funnels be implemented as a
+ * single-element enum to maintain serialization guarantees. See Effective Java (2nd Edition),
+ * Item 3: "Enforce the singleton property with a private constructor or an enum type". For example:
+ * <pre>   {@code
+ *   public enum PersonFunnel implements Funnel<Person> {
+ *     INSTANCE;
+ *     public void funnel(Person person, PrimitiveSink into) {
+ *       into.putString(person.getFirstName())
+ *           .putString(person.getLastName())
+ *           .putInt(person.getAge());
+ *     }
+ *   }</pre>
  * 
  * @author Dimitris Andreou
  * @since 11.0
@@ -32,7 +46,7 @@ public interface Funnel<T> extends Serializable {
    * is no requirement that this data be complete enough to fully reconstitute the object
    * later.
    *
-   * @since 12.0 (in 11.0 version, {@code PrimitiveSink} was still called {@code Sink})
+   * @since 12.0 (in Guava 11.0, {@code PrimitiveSink} was named {@code Sink})
    */
   void funnel(T from, PrimitiveSink into);
 }
