b3b6b1d3ceb35de964d6403c5cc291c980f5478f
Colin Decker <cgdecker@google.com>
2012-10-02 13:09:57 -0400

    Implement ImmutableRangeSet. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=34454241

---
+++ guava-tests/test/com/google/common/collect/ImmutableRangeSetTest.java
@@ -0,0 +1,257 @@
+/*
+ * Copyright (C) 2012 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.common.collect;
+
+import static org.junit.contrib.truth.Truth.ASSERT;
+
+import com.google.common.annotations.GwtIncompatible;
+import com.google.common.testing.SerializableTester;
+
+import java.util.Set;
+
+/**
+ * Tests for {@link ImmutableRangeSet}.
+ *
+ * @author Louis Wasserman
+ */
+@GwtIncompatible("ImmutableRangeSet")
+public class ImmutableRangeSetTest extends AbstractRangeSetTest {
+  public void testEmpty() {
+    ImmutableRangeSet<Integer> rangeSet = ImmutableRangeSet.of();
+
+    ASSERT.that(rangeSet.asRanges()).isEmpty();
+    assertEquals(ImmutableRangeSet.<Integer>all(), rangeSet.complement());
+    assertFalse(rangeSet.contains(0));
+    assertFalse(rangeSet.encloses(Range.singleton(0)));
+    assertTrue(rangeSet.enclosesAll(rangeSet));
+    assertTrue(rangeSet.isEmpty());
+  }
+
+  public void testAll() {
+    ImmutableRangeSet<Integer> rangeSet = ImmutableRangeSet.all();
+
+    ASSERT.that(rangeSet.asRanges()).hasContentsInOrder(Range.all());
+    assertTrue(rangeSet.contains(0));
+    assertTrue(rangeSet.encloses(Range.<Integer>all()));
+    assertTrue(rangeSet.enclosesAll(rangeSet));
+    assertEquals(ImmutableRangeSet.<Integer>of(), rangeSet.complement());
+  }
+
+  public void testSingleBoundedRange() {
+    ImmutableRangeSet<Integer> rangeSet = ImmutableRangeSet.of(Range.closedOpen(1, 5));
+
+    ASSERT.that(rangeSet.asRanges()).hasContentsInOrder(Range.closedOpen(1, 5));
+
+    assertTrue(rangeSet.encloses(Range.closed(3, 4)));
+    assertTrue(rangeSet.encloses(Range.closedOpen(1, 4)));
+    assertTrue(rangeSet.encloses(Range.closedOpen(1, 5)));
+    assertFalse(rangeSet.encloses(Range.greaterThan(2)));
+
+    assertTrue(rangeSet.contains(3));
+    assertFalse(rangeSet.contains(5));
+    assertFalse(rangeSet.contains(0));
+
+    RangeSet<Integer> expectedComplement = TreeRangeSet.create();
+    expectedComplement.add(Range.lessThan(1));
+    expectedComplement.add(Range.atLeast(5));
+
+    assertEquals(expectedComplement, rangeSet.complement());
+  }
+
+  public void testSingleBoundedBelowRange() {
+    ImmutableRangeSet<Integer> rangeSet = ImmutableRangeSet.of(Range.greaterThan(2));
+
+    ASSERT.that(rangeSet.asRanges()).hasContentsInOrder(Range.greaterThan(2));
+
+    assertTrue(rangeSet.encloses(Range.closed(3, 4)));
+    assertTrue(rangeSet.encloses(Range.greaterThan(3)));
+    assertFalse(rangeSet.encloses(Range.closedOpen(1, 5)));
+
+    assertTrue(rangeSet.contains(3));
+    assertTrue(rangeSet.contains(5));
+    assertFalse(rangeSet.contains(0));
+    assertFalse(rangeSet.contains(2));
+
+    assertEquals(ImmutableRangeSet.of(Range.atMost(2)), rangeSet.complement());
+  }
+
+  public void testSingleBoundedAboveRange() {
+    ImmutableRangeSet<Integer> rangeSet = ImmutableRangeSet.of(Range.atMost(3));
+
+    ASSERT.that(rangeSet.asRanges()).hasContentsInOrder(Range.atMost(3));
+
+    assertTrue(rangeSet.encloses(Range.closed(2, 3)));
+    assertTrue(rangeSet.encloses(Range.lessThan(1)));
+    assertFalse(rangeSet.encloses(Range.closedOpen(1, 5)));
+
+    assertTrue(rangeSet.contains(3));
+    assertTrue(rangeSet.contains(0));
+    assertFalse(rangeSet.contains(4));
+    assertFalse(rangeSet.contains(5));
+
+    assertEquals(ImmutableRangeSet.of(Range.greaterThan(3)), rangeSet.complement());
+  }
+
+  public void testMultipleBoundedRanges() {
+    ImmutableRangeSet<Integer> rangeSet = ImmutableRangeSet.<Integer>builder()
+        .add(Range.closed(5, 8)).add(Range.closedOpen(1, 3)).build();
+
+    ASSERT.that(rangeSet.asRanges()).hasContentsInOrder(Range.closedOpen(1, 3), Range.closed(5, 8));
+
+    assertTrue(rangeSet.encloses(Range.closed(1, 2)));
+    assertTrue(rangeSet.encloses(Range.open(5, 8)));
+    assertFalse(rangeSet.encloses(Range.closed(1, 8)));
+    assertFalse(rangeSet.encloses(Range.greaterThan(5)));
+
+    RangeSet<Integer> expectedComplement = ImmutableRangeSet.<Integer>builder()
+        .add(Range.lessThan(1))
+        .add(Range.closedOpen(3, 5))
+        .add(Range.greaterThan(8))
+        .build();
+
+    assertEquals(expectedComplement, rangeSet.complement());
+  }
+
+  public void testMultipleBoundedBelowRanges() {
+    ImmutableRangeSet<Integer> rangeSet = ImmutableRangeSet.<Integer>builder()
+        .add(Range.greaterThan(6)).add(Range.closedOpen(1, 3)).build();
+
+    ASSERT.that(rangeSet.asRanges())
+        .hasContentsInOrder(Range.closedOpen(1, 3), Range.greaterThan(6));
+
+    assertTrue(rangeSet.encloses(Range.closed(1, 2)));
+    assertTrue(rangeSet.encloses(Range.open(6, 8)));
+    assertFalse(rangeSet.encloses(Range.closed(1, 8)));
+    assertFalse(rangeSet.encloses(Range.greaterThan(5)));
+
+    RangeSet<Integer> expectedComplement = ImmutableRangeSet.<Integer>builder()
+        .add(Range.lessThan(1))
+        .add(Range.closed(3, 6))
+        .build();
+
+    assertEquals(expectedComplement, rangeSet.complement());
+  }
+
+  public void testMultipleBoundedAboveRanges() {
+    ImmutableRangeSet<Integer> rangeSet = ImmutableRangeSet.<Integer>builder()
+        .add(Range.atMost(0)).add(Range.closedOpen(2, 5)).build();
+
+    ASSERT.that(rangeSet.asRanges())
+        .hasContentsInOrder(Range.atMost(0), Range.closedOpen(2, 5));
+
+    assertTrue(rangeSet.encloses(Range.closed(2, 4)));
+    assertTrue(rangeSet.encloses(Range.open(-5, -2)));
+    assertFalse(rangeSet.encloses(Range.closed(1, 8)));
+    assertFalse(rangeSet.encloses(Range.greaterThan(5)));
+
+    RangeSet<Integer> expectedComplement = ImmutableRangeSet.<Integer>builder()
+        .add(Range.open(0, 2))
+        .add(Range.atLeast(5))
+        .build();
+
+    assertEquals(expectedComplement, rangeSet.complement());
+  }
+
+  public void testAddUnsupported() {
+    ImmutableRangeSet<Integer> rangeSet = ImmutableRangeSet.<Integer>builder()
+        .add(Range.closed(5, 8)).add(Range.closedOpen(1, 3)).build();
+
+    try {
+      rangeSet.add(Range.open(3, 4));
+      fail();
+    } catch (UnsupportedOperationException expected) {
+      // success
+    }
+  }
+
+  public void testAddAllUnsupported() {
+    ImmutableRangeSet<Integer> rangeSet = ImmutableRangeSet.<Integer>builder()
+        .add(Range.closed(5, 8)).add(Range.closedOpen(1, 3)).build();
+
+    try {
+      rangeSet.addAll(ImmutableRangeSet.<Integer>of());
+      fail();
+    } catch (UnsupportedOperationException expected) {
+      // success
+    }
+  }
+
+  public void testRemoveUnsupported() {
+    ImmutableRangeSet<Integer> rangeSet = ImmutableRangeSet.<Integer>builder()
+        .add(Range.closed(5, 8)).add(Range.closedOpen(1, 3)).build();
+
+    try {
+      rangeSet.remove(Range.closed(6, 7));
+      fail();
+    } catch (UnsupportedOperationException expected) {
+      // success
+    }
+  }
+
+  public void testRemoveAllUnsupported() {
+    ImmutableRangeSet<Integer> rangeSet = ImmutableRangeSet.<Integer>builder()
+        .add(Range.closed(5, 8)).add(Range.closedOpen(1, 3)).build();
+
+    try {
+      rangeSet.removeAll(ImmutableRangeSet.<Integer>of());
+      fail();
+    } catch (UnsupportedOperationException expected) {
+      // success
+    }
+
+    try {
+      rangeSet.removeAll(ImmutableRangeSet.of(Range.closed(6, 8)));
+      fail();
+    } catch (UnsupportedOperationException expected) {
+      // success
+    }
+  }
+
+  public void testExhaustive() {
+    @SuppressWarnings("unchecked")
+    ImmutableSet<Range<Integer>> ranges = ImmutableSet.of(
+        Range.<Integer>all(),
+        Range.<Integer>closedOpen(3, 5),
+        Range.singleton(1),
+        Range.lessThan(2),
+        Range.greaterThan(10),
+        Range.atMost(4),
+        Range.atLeast(3),
+        Range.closed(4, 6),
+        Range.closedOpen(1, 3),
+        Range.openClosed(5, 7),
+        Range.open(3, 4));
+    for (Set<Range<Integer>> subset : Sets.powerSet(ranges)) {
+      RangeSet<Integer> mutable = TreeRangeSet.create();
+      ImmutableRangeSet.Builder<Integer> builder = ImmutableRangeSet.builder();
+
+      for (Range<Integer> range : subset) {
+        mutable.add(range);
+        builder.add(range);
+      }
+
+      ImmutableRangeSet<Integer> built = builder.build();
+      assertEquals(mutable, built);
+      assertEquals(ImmutableRangeSet.copyOf(mutable), built);
+      assertEquals(mutable.complement(), built.complement());
+
+      for (int i = 0; i <= 11; i++) {
+        assertEquals(mutable.contains(i), built.contains(i));
+      }
+
+      SerializableTester.reserializeAndAssert(built);
+    }
+  }
+}
---
+++ guava/src/com/google/common/collect/ImmutableRangeSet.java
@@ -0,0 +1,321 @@
+/*
+ * Copyright (C) 2012 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.common.collect;
+
+import static com.google.common.base.Preconditions.checkElementIndex;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.SortedLists.KeyAbsentBehavior.NEXT_LOWER;
+import static com.google.common.collect.SortedLists.KeyPresentBehavior.ANY_PRESENT;
+
+import com.google.common.base.Function;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+/**
+ * An efficient immutable implementation of a {@link RangeSet}.
+ *
+ * @author Louis Wasserman
+ */
+final class ImmutableRangeSet<C extends Comparable> extends AbstractRangeSet<C>
+    implements Serializable {
+
+  @SuppressWarnings("unchecked")
+  private static final ImmutableRangeSet EMPTY = new ImmutableRangeSet(ImmutableList.of());
+
+  @SuppressWarnings("unchecked")
+  private static final ImmutableRangeSet ALL = new ImmutableRangeSet(ImmutableList.of(Range.all()));
+
+  /**
+   * Returns an empty immutable range set.
+   */
+  @SuppressWarnings("unchecked")
+  public static <C extends Comparable> ImmutableRangeSet<C> of() {
+    return EMPTY;
+  }
+
+  /**
+   * Returns an immutable range set containing the single range {@link Range#all()}.
+   */
+  @SuppressWarnings("unchecked")
+  public static <C extends Comparable> ImmutableRangeSet<C> all() {
+    return ALL;
+  }
+
+  /**
+   * Returns an immutable range set containing the specified single range. If {@link Range#isEmpty()
+   * range.isEmpty()}, this is equivalent to {@link ImmutableRangeSet#of()}.
+   */
+  public static <C extends Comparable> ImmutableRangeSet<C> of(Range<C> range) {
+    checkNotNull(range);
+    if (range.isEmpty()) {
+      return of();
+    } else if (range.equals(Range.all())) {
+      return all();
+    } else {
+      return new ImmutableRangeSet<C>(ImmutableList.of(range));
+    }
+  }
+
+  /**
+   * Returns an immutable copy of the specified {@code RangeSet}.
+   */
+  public static <C extends Comparable> ImmutableRangeSet<C> copyOf(RangeSet<C> rangeSet) {
+    checkNotNull(rangeSet);
+    if (rangeSet.isEmpty()) {
+      return of();
+    } else if (rangeSet.encloses(Range.<C>all())) {
+      return all();
+    }
+
+    if (rangeSet instanceof ImmutableRangeSet) {
+      ImmutableRangeSet<C> immutableRangeSet = (ImmutableRangeSet<C>) rangeSet;
+      if (!immutableRangeSet.isPartialView()) {
+        return immutableRangeSet;
+      }
+    }
+    return new ImmutableRangeSet<C>(ImmutableList.copyOf(rangeSet.asRanges()));
+  }
+
+  ImmutableRangeSet(ImmutableList<Range<C>> ranges) {
+    this.ranges = ranges;
+  }
+
+  private ImmutableRangeSet(ImmutableList<Range<C>> ranges, ImmutableRangeSet<C> complement) {
+    this.ranges = ranges;
+    this.complement = complement;
+  }
+
+  private transient final ImmutableList<Range<C>> ranges;
+
+  @Override
+  public boolean encloses(Range<C> otherRange) {
+    Function<Range<C>, Cut<C>> lowerBoundFunction = new Function<Range<C>, Cut<C>>() {
+      @Override
+      public Cut<C> apply(Range<C> input) {
+        return input.lowerBound;
+      }
+    };
+    int index = SortedLists.binarySearch(ranges,
+        lowerBoundFunction,
+        otherRange.lowerBound,
+        Ordering.natural(),
+        ANY_PRESENT,
+        NEXT_LOWER);
+    return index != -1 && ranges.get(index).encloses(otherRange);
+  }
+
+  @Override
+  public Range<C> rangeContaining(C value) {
+    Function<Range<C>, Cut<C>> lowerBoundFunction = new Function<Range<C>, Cut<C>>() {
+      @Override
+      public Cut<C> apply(Range<C> input) {
+        return input.lowerBound;
+      }
+    };
+    int index = SortedLists.binarySearch(ranges,
+        lowerBoundFunction,
+        Cut.belowValue(value),
+        Ordering.natural(),
+        ANY_PRESENT,
+        NEXT_LOWER);
+    if (index != -1) {
+      Range<C> range = ranges.get(index);
+      return range.contains(value) ? range : null;
+    }
+    return null;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return ranges.isEmpty();
+  }
+
+  @Override
+  public void add(Range<C> range) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void addAll(RangeSet<C> other) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void remove(Range<C> range) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void removeAll(RangeSet<C> other) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public ImmutableSet<Range<C>> asRanges() {
+    if (ranges.isEmpty()) {
+      return ImmutableSet.of();
+    }
+    return new RegularImmutableSortedSet<Range<C>>(ranges, new Comparator<Range<C>>() {
+      @Override
+      public int compare(Range<C> range1, Range<C> range2) {
+        return ComparisonChain.start().compare(range1.lowerBound, range2.lowerBound)
+            .compare(range1.upperBound, range2.upperBound).result();
+      }
+    });
+  }
+
+  private transient ImmutableRangeSet<C> complement;
+
+  private final class ComplementRanges extends ImmutableList<Range<C>> {
+    // True if the "positive" range set is empty or bounded below.
+    private final boolean positiveBoundedBelow;
+
+    // True if the "positive" range set is empty or bounded above.
+    private final boolean positiveBoundedAbove;
+
+    private final int size;
+
+    ComplementRanges() {
+      this.positiveBoundedBelow = ranges.get(0).hasLowerBound();
+      this.positiveBoundedAbove = Iterables.getLast(ranges).hasUpperBound();
+
+      int size = ranges.size() - 1;
+      if (positiveBoundedBelow) {
+        size++;
+      }
+      if (positiveBoundedAbove) {
+        size++;
+      }
+      this.size = size;
+    }
+
+    @Override
+    public int size() {
+      return size;
+    }
+
+    @Override
+    public Range<C> get(int index) {
+      checkElementIndex(index, size);
+
+      Cut<C> lowerBound;
+      if (positiveBoundedBelow) {
+        lowerBound = (index == 0) ? Cut.<C>belowAll() : ranges.get(index - 1).upperBound;
+      } else {
+        lowerBound = ranges.get(index).upperBound;
+      }
+
+      Cut<C> upperBound;
+      if (positiveBoundedAbove && index == size - 1) {
+        upperBound = Cut.<C>aboveAll();
+      } else {
+        upperBound = ranges.get(index + (positiveBoundedBelow ? 0 : 1)).lowerBound;
+      }
+
+      return new Range<C>(lowerBound, upperBound);
+    }
+
+    @Override
+    boolean isPartialView() {
+      return true;
+    }
+  }
+
+  @Override
+  public ImmutableRangeSet<C> complement() {
+    ImmutableRangeSet<C> result = complement;
+    if (result != null) {
+      return result;
+    } else if (ranges.isEmpty()) {
+      return complement = all();
+    } else if (ranges.size() == 1 && ranges.get(0).equals(Range.all())) {
+      return complement = of();
+    } else {
+      ImmutableList<Range<C>> complementRanges = new ComplementRanges();
+      result = complement = new ImmutableRangeSet<C>(complementRanges, this);
+    }
+    return result;
+  }
+
+  boolean isPartialView() {
+    return ranges.isPartialView();
+  }
+
+  /**
+   * Returns a new builder for an immutable range set.
+   */
+  public static <C extends Comparable> Builder<C> builder() {
+    return new Builder<C>();
+  }
+
+  /**
+   * A builder for immutable range sets. Duplicate or connected ranges are permitted, and will be
+   * merged in the resulting immutable range set.
+   */
+  public static class Builder<C extends Comparable> {
+    private final RangeSet<C> rangeSet;
+
+    public Builder() {
+      this.rangeSet = TreeRangeSet.create();
+    }
+
+    /**
+     * Add the specified range to this builder. Duplicate or connected ranges are permitted, and
+     * will be merged in the resulting immutable range set.
+     */
+    public Builder<C> add(Range<C> range) {
+      rangeSet.add(range);
+      return this;
+    }
+
+    /**
+     * Add all ranges from the specified range set to this builder. Duplicate or connected ranges
+     * are permitted, and will be merged in the resulting immutable range set.
+     */
+    public Builder<C> addAll(RangeSet<C> ranges) {
+      rangeSet.addAll(ranges);
+      return this;
+    }
+
+    /**
+     * Returns an {@code ImmutableRangeSet} containing the ranges added to this builder.
+     */
+    public ImmutableRangeSet<C> build() {
+      return copyOf(rangeSet);
+    }
+  }
+
+  private static final class SerializedForm<C extends Comparable> implements Serializable {
+    private final ImmutableList<Range<C>> ranges;
+
+    SerializedForm(ImmutableList<Range<C>> ranges) {
+      this.ranges = ranges;
+    }
+
+    Object readResolve() {
+      if (ranges.isEmpty()) {
+        return of();
+      } else if (ranges.equals(ImmutableList.of(Range.all()))) {
+        return all();
+      } else {
+        return new ImmutableRangeSet<C>(ranges);
+      }
+    }
+  }
+
+  Object writeReplace() {
+    return new SerializedForm<C>(ranges);
+  }
+}
