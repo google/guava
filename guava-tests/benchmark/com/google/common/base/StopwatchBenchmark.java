313f93c8c4ec073158317473dfbfb2aff29b1d65
Kurt Kluever <kak@google.com>
2013-01-03 16:20:56 -0500

    Fix calls to deprecated Stopwatch.elapsedTime(TimeUnit) by inlining that method's implementation ("elapsed(TimeUnit)"). ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=40480378

--- guava-tests/benchmark/com/google/common/base/StopwatchBenchmark.java
+++ guava-tests/benchmark/com/google/common/base/StopwatchBenchmark.java
@@ -35,7 +35,7 @@ public class StopwatchBenchmark extends SimpleBenchmark {
     for (int i = 0; i < reps; i++) {
       Stopwatch s = new Stopwatch().start();
       // here is where you would do something
-      total += s.elapsedTime(TimeUnit.NANOSECONDS);
+      total += s.elapsed(TimeUnit.NANOSECONDS);
     }
     return total;
   }
