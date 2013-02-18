e66c84b101f674401ffd4c5264b15a980f48e749
Chris Povirk <cpovirk@google.com>
2013-02-18 16:16:00 -0500

    Open source some more benchmarks. The Monitor benchmarks were left as internal when the class was migrated from Labs. My guess is that that was nothing more than "Caliper benchmarks can't be open-sourced yet." I notice that earlier CLs didn't open-source them, either. My guess is that that's just because they were excluded from the BUILD rule entirely and not merely marked with @GoogleInternal. My guess is that the same is true of the EventBus benchmark. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=42749026

---
+++ guava-tests/benchmark/com/google/common/eventbus/EventBusBenchmark.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2011 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.common.eventbus;
+
+import com.google.caliper.Benchmark;
+import com.google.caliper.runner.CaliperMain;
+
+/**
+ * Benchmark for {@link EventBus}.
+ *
+ * @author Eric Fellheimer
+ */
+public class EventBusBenchmark extends Benchmark {
+
+  private EventBus eventBus;
+
+  @Override
+  protected void setUp() {
+    eventBus = new EventBus("for benchmarking purposes");
+    eventBus.register(this);
+  }
+
+  public void timePostStrings(int reps) {
+    for (int i = 0; i < reps; i++) {
+      eventBus.post("hello there");
+    }
+  }
+
+  @Subscribe
+  public void handleStrings(String string) {
+    // Nothing to do here.
+  }
+
+  public static void main(String[] args) {
+    CaliperMain.main(EventBusBenchmark.class, args);
+  }
+}
