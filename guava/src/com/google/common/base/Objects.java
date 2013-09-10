d56e501e83d6c17b3a2cf0524f797a4b5f38fdbd
Chris Povirk <cpovirk@google.com>
2013-09-10 16:49:32 -0400

    Use @link and @linkplain in Objects.firstNonNull() javadocs.
    See https://code.google.com/p/guava-libraries/issues/detail?id=1526
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=52109962

--- guava/src/com/google/common/base/Objects.java
+++ guava/src/com/google/common/base/Objects.java
@@ -171,10 +171,11 @@ public final class Objects {
    * Returns the first of two given parameters that is not {@code null}, if
    * either is, or otherwise throws a {@link NullPointerException}.
    *
-   * <p><b>Note:</b> if {@code first} is represented as an {@code Optional<T>},
-   * this can be accomplished with {@code first.or(second)}. That approach also
-   * allows for lazy evaluation of the fallback instance, using
-   * {@code first.or(Supplier)}.
+   * <p><b>Note:</b> if {@code first} is represented as an {@link Optional},
+   * this can be accomplished with
+   * {@linkplain Optional#or(Object) first.or(second)}.
+   * That approach also allows for lazy evaluation of the fallback instance,
+   * using {@linkplain Optional#or(Supplier) first.or(Supplier)}.
    *
    * @return {@code first} if {@code first} is not {@code null}, or
    *     {@code second} if {@code first} is {@code null} and {@code second} is
