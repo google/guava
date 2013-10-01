5979791fe88d3b5c6aaa265e20ad40701aa4aff4
Chris Povirk <cpovirk@google.com>
2013-10-01 17:35:02 -0400

    Include common.xml's package-info in Guava. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=53568852

---
+++ guava/src/com/google/common/xml/package-info.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2012 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Escapers
+ * for
+ * XML.
+ *
+ * <p>This package is a part of the open-source
+ * <a href="http://guava-libraries.googlecode.com">Guava libraries</a>.
+ */
+@ParametersAreNonnullByDefault
+package com.google.common.xml;
+
+import javax.annotation.ParametersAreNonnullByDefault;
