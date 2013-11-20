837d163fef75c1062d4c1d0becb31c7b54f01561
Chris Povirk <cpovirk@google.com>
2013-11-20 12:44:46 -0500

    Specify that behavior is undefined for non-ASCII input. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=56954960

--- guava/src/com/google/common/base/CaseFormat.java
+++ guava/src/com/google/common/base/CaseFormat.java
@@ -21,7 +21,8 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import com.google.common.annotations.GwtCompatible;
 
 /**
- * Utility class for converting between various ASCII case formats.
+ * Utility class for converting between various ASCII case formats. Behavior is undefined for
+ * non-ASCII input.
  *
  * @author Mike Bostock
  * @since 1.0
