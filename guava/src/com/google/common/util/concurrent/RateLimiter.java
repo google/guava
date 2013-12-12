a6c0a1babcde36af1010ef36a3aa9fa2bfc9e3c4
Chris Povirk <cpovirk@google.com>
2013-12-12 11:46:02 -0500

    the the -> the ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=57915223

--- guava/src/com/google/common/util/concurrent/RateLimiter.java
+++ guava/src/com/google/common/util/concurrent/RateLimiter.java
@@ -98,7 +98,7 @@ public abstract class RateLimiter {
    * The simplest way to maintain a rate of QPS is to keep the timestamp of the last
    * granted request, and ensure that (1/QPS) seconds have elapsed since then. For example,
    * for a rate of QPS=5 (5 tokens per second), if we ensure that a request isn't granted
-   * earlier than 200ms after the the last one, then we achieve the intended rate.
+   * earlier than 200ms after the last one, then we achieve the intended rate.
    * If a request comes and the last request was granted only 100ms ago, then we wait for
    * another 100ms. At this rate, serving 15 fresh permits (i.e. for an acquire(15) request)
    * naturally takes 3 seconds.
