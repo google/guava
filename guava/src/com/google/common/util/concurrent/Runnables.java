fe592e08dd536532a18cb35eb2a706dd6af7b254
Colin Decker <cgdecker@google.com>
2013-12-19 17:36:16 -0500

    Open source Runnables.doNothing(). ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=58481831

---
+++ guava/src/com/google/common/util/concurrent/Runnables.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2013 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.common.util.concurrent;
+
+import com.google.common.annotations.Beta;
+import com.google.common.annotations.GwtCompatible;
+
+/**
+ * Static utility methods pertaining to the {@link Runnable} interface.
+ *
+ * @since 16.0
+ */
+@Beta
+@GwtCompatible
+public final class Runnables {
+
+  private static final Runnable EMPTY_RUNNABLE = new Runnable() {
+    @Override
+    public void run() {
+    }
+  };
+
+  /**
+   * Returns a {@link Runnable} instance that does nothing when run.
+   */
+  public static Runnable doNothing() {
+    return EMPTY_RUNNABLE;
+  }
+
+  private Runnables() {
+  }
+}
