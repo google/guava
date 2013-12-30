eb469f1d16935eff54f74ee16345a39350050afd
Chris Povirk <cpovirk@google.com>
2013-12-30 17:20:48 -0500

    Mark isSmall as @GwtIncompatible. It references the @GwtIncompatible type SmallCharMatcher. This works fine in prod mode, but it breaks in dev mode. See https://code.google.com/p/google-web-toolkit/issues/detail?id=8510 and the discussion on linked CL 39931458. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=58906855

--- guava-gwt/src-super/com/google/common/base/super/com/google/common/base/CharMatcher.java
+++ guava-gwt/src-super/com/google/common/base/super/com/google/common/base/CharMatcher.java
@@ -757,12 +757,6 @@ public abstract class CharMatcher implements Predicate<Character> {
     }
   }
 
-  private static boolean isSmall(int totalCharacters, int tableLength) {
-    return totalCharacters <= SmallCharMatcher.MAX_SIZE
-        && tableLength > (totalCharacters * 4 * Character.SIZE);
-        // err on the side of BitSetMatcher
-  }
-
   // Text processing routines
 
   /**
