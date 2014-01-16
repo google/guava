3b2035d0abf57f1cf434ed6b90b4e131543ced46
Colin Decker <cgdecker@google.com>
2014-01-16 12:26:53 -0500

    Fix up ExecutionQueue docs. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=59677053

--- guava/src/com/google/common/util/concurrent/ExecutionQueue.java
+++ guava/src/com/google/common/util/concurrent/ExecutionQueue.java
@@ -31,52 +31,106 @@ import javax.annotation.concurrent.GuardedBy;
 import javax.annotation.concurrent.ThreadSafe;
 
 /**
- * <p>A thread-safe queue of listeners, each with an associated {@code Executor}, that guarantees 
- * that every {@code Runnable} that is {@linkplain #add added} will be 
- * {@link Executor#execute(Runnable) executed} in the same order that it was added and also that 
- * execution is delayed until the next call to {@link #execute}.  This is particularly useful if
- * you need to ensure that listeners are <em>not</em> executed with a lock held. 
- * 
- * <p>While similar, this is different than {@link ExecutionList} which makes much looser guarantees
- * around ordering and has a notion of state (the executed bit) that this class does not have.
- * 
- * <p>Listeners are queued by calling {@link #add} and flushed on calls to {@link #execute()}. 
- * Because calls to {@link #add} may be concurrent with calls to {@link #execute} there is no 
- * guarantee that the queue will be empty after calling {@link #execute}.
+ * A thread-safe queue of listeners, each with an associated {@code Executor}, that guarantees
+ * that every {@code Runnable} that is {@linkplain #add added} will be
+ * {@link Executor#execute(Runnable) executed} in the same order that it was added.
  *
- * <p>Exceptions thrown by a listener will be propagated up to the executor. Any exception thrown 
- * during {@code Executor.execute} (e.g., a {@code RejectedExecutionException} or an exception 
+ * <p>While similar in structure and API to {@link ExecutionList}, this class differs in several
+ * ways:
+ *
+ * <ul>
+ *    <li>This class makes strict ordering guarantees. ExecutionList makes no ordering guarantees.
+ *    <li>{@link ExecutionQueue#execute} executes all currently pending listeners. Later calls
+ *        to {@link ExecutionQueue#add} are delayed until the <em>next</em> call to execute.
+ *        {@link ExecutionList#execute()} executes all current listeners and also causes immediate
+ *        execution on subsequent calls to {@link ExecutionList#add}.
+ * </ul>
+ *
+ * <p>These differences make {@link ExecutionQueue} suitable for when you need to execute callbacks
+ * multiple times in response to different events. ExecutionList is suitable for when you have a
+ * single event.
+ *
+ * <p>For example, this implements a simple atomic data structure that lets a listener
+ * asynchronously listen to changes to a value: <pre>   {@code
+ *   interface CountListener {
+ *     void update(int v);
+ *   }
+ *
+ *   class AtomicListenableCounter {
+ *     private int value;
+ *     private final ExecutionQueue queue = new ExecutionQueue();
+ *     private final CountListener listener;
+ *     private final Executor executor;
+ *
+ *     AtomicListenableCounter(CountListener listener, Executor executor) {
+ *       this.listener = listener;
+ *       this.exeucutor = executor;
+ *     }
+ *
+ *     void add(int amt) {
+ *       synchronized (this) {
+ *         v += amt;
+ *         final int currentValue = v;
+ *         queue.add(new Runnable() {
+ *           public void run() {
+ *             listener.update(currentValue);
+ *           }
+ *         }, executor);
+ *       }
+ *       queue.execute();
+ *   }
+ * }}</pre>
+ *
+ * <p>This AtomicListenableCounter allows a listener to be run asynchronously on every update and
+ * the ExecutionQueue enforces that:
+ *
+ * <ul>
+ *   <li>The listener is never run with the lock held (even if the executor is the
+ *       {@link MoreExecutors#sameThreadExecutor()})
+ *   <li>The listeners are never run out of order
+ *   <li>Each added listener is called only once.
+ * </ul>
+ *
+ * <p>Exceptions thrown by a listener will be propagated up to the executor. Any exception thrown
+ * during {@code Executor.execute} (e.g., a {@code RejectedExecutionException} or an exception
  * thrown by {@linkplain MoreExecutors#sameThreadExecutor inline execution}) will be caught and
  * logged.
  *
  * @author Luke Sandberg
  */
-@ThreadSafe final class ExecutionQueue {
+@ThreadSafe
+final class ExecutionQueue {
   private static final Logger logger = Logger.getLogger(ExecutionQueue.class.getName());
 
   /** The listeners to execute in order.  */
-  private final ConcurrentLinkedQueue<RunnableExecutorPair> queuedListeners = 
+  private final ConcurrentLinkedQueue<RunnableExecutorPair> queuedListeners =
       Queues.newConcurrentLinkedQueue();
   /**
-   * This lock is used with {@link RunnableExecutorPair#submit} to ensure that each listener is 
+   * This lock is used with {@link RunnableExecutorPair#submit} to ensure that each listener is
    * executed at most once.
    */
   private final ReentrantLock lock = new ReentrantLock();
 
   /**
    * Adds the {@code Runnable} and accompanying {@code Executor} to the queue of listeners to
-   * execute. 
+   * execute.
+   *
+   * <p>Note: This method will never directly invoke {@code executor.execute(runnable)}, though your
+   * runnable may be executed before it returns if another thread has concurrently called
+   * {@link #execute}.
    */
-  void add(Runnable runnable, Executor executor) {
+  public void add(Runnable runnable, Executor executor) {
     queuedListeners.add(new RunnableExecutorPair(runnable, executor));
   }
 
   /**
-   * Executes all listeners in the queue. However, note that listeners added concurrently with this
-   * method may be executed as part of this call or not, so there is no guarantee that the queue is
-   * empty after calling this method.
+   * Executes all listeners in the queue.
+   *
+   * <p>Note that there is no guarantee that concurrently {@linkplain #add added} listeners will be
+   * executed prior to the return of this method, only that all calls to {@link #add} that
+   * happen-before this call will be executed.
    */
-  void execute() {
+  public void execute() {
     // We need to make sure that listeners are submitted to their executors in the correct order. So
     // we cannot remove a listener from the queue until we know that it has been submited to its
     // executor.  So we use an iterator and only call remove after submit.  This iterator is 'weakly
@@ -85,6 +139,9 @@ import javax.annotation.concurrent.ThreadSafe;
     // iterator).  This is fine because 1. our contract says we may not execute all concurrently
     // added items and 2. calling listener.submit is idempotent, so it is safe (and generally cheap)
     // to call it multiple times.
+    // TODO(user): we are relying on an underdocumented feature of ConcurrentLinkedQueue, the
+    // general strategy in other JDK libraries appears to be bring-your-own-queue :(  Consider doing
+    // that.
     Iterator<RunnableExecutorPair> iterator = queuedListeners.iterator();
     while (iterator.hasNext()) {
       iterator.next().submit();
@@ -115,12 +172,12 @@ import javax.annotation.concurrent.ThreadSafe;
      */
     @GuardedBy("lock")
     private boolean hasBeenExecuted = false;
-  
+
     RunnableExecutorPair(Runnable runnable, Executor executor) {
       this.runnable = checkNotNull(runnable);
       this.executor = checkNotNull(executor);
     }
-  
+
     /** Submit this listener to its executor */
     private void submit() {
       lock.lock();
@@ -142,7 +199,7 @@ import javax.annotation.concurrent.ThreadSafe;
         }
       }
     }
-  
+
     @Override public final void run() {
       // If the executor was the sameThreadExecutor then we might still be holding the lock and
       // hasBeenExecuted may not have been assigned yet so we unlock now to ensure that we are not
