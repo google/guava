2390c423db30b6e45409b8327b666f0e36098435
Chris Povirk <cpovirk@google.com>
2014-03-19 12:53:59 -0400

    Run some common.collect.testing tests under GWT. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=63261660

---
+++ guava-gwt/test/com/google/common/collect/testing/IteratorTesterTest_gwt.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2008 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.common.collect.testing;
+public class IteratorTesterTest_gwt extends com.google.gwt.junit.client.GWTTestCase {
+@Override public String getModuleName() {
+  return "com.google.common.collect.testing.testModule";
+}
+public void testCanCatchDifferentContents() throws Exception {
+  com.google.common.collect.testing.IteratorTesterTest testCase = new com.google.common.collect.testing.IteratorTesterTest();
+  testCase.testCanCatchDifferentContents();
+}
+
+public void testCanCatchDifferentLengthOfIteration() throws Exception {
+  com.google.common.collect.testing.IteratorTesterTest testCase = new com.google.common.collect.testing.IteratorTesterTest();
+  testCase.testCanCatchDifferentLengthOfIteration();
+}
+
+public void testCanCatchDifferentRemoveBehaviour() throws Exception {
+  com.google.common.collect.testing.IteratorTesterTest testCase = new com.google.common.collect.testing.IteratorTesterTest();
+  testCase.testCanCatchDifferentRemoveBehaviour();
+}
+
+public void testCanCatchSunJavaBug6529795InTargetIterator() throws Exception {
+  com.google.common.collect.testing.IteratorTesterTest testCase = new com.google.common.collect.testing.IteratorTesterTest();
+  testCase.testCanCatchSunJavaBug6529795InTargetIterator();
+}
+
+public void testCanWorkAroundSunJavaBug6529795InTargetIterator() throws Exception {
+  com.google.common.collect.testing.IteratorTesterTest testCase = new com.google.common.collect.testing.IteratorTesterTest();
+  testCase.testCanWorkAroundSunJavaBug6529795InTargetIterator();
+}
+
+public void testMismatchedException() throws Exception {
+  com.google.common.collect.testing.IteratorTesterTest testCase = new com.google.common.collect.testing.IteratorTesterTest();
+  testCase.testMismatchedException();
+}
+
+public void testMissingException() throws Exception {
+  com.google.common.collect.testing.IteratorTesterTest testCase = new com.google.common.collect.testing.IteratorTesterTest();
+  testCase.testMissingException();
+}
+
+public void testSimilarException() throws Exception {
+  com.google.common.collect.testing.IteratorTesterTest testCase = new com.google.common.collect.testing.IteratorTesterTest();
+  testCase.testSimilarException();
+}
+
+public void testUnexpectedException() throws Exception {
+  com.google.common.collect.testing.IteratorTesterTest testCase = new com.google.common.collect.testing.IteratorTesterTest();
+  testCase.testUnexpectedException();
+}
+
+public void testUnknownOrder() throws Exception {
+  com.google.common.collect.testing.IteratorTesterTest testCase = new com.google.common.collect.testing.IteratorTesterTest();
+  testCase.testUnknownOrder();
+}
+
+public void testUnknownOrderUnrecognizedElement() throws Exception {
+  com.google.common.collect.testing.IteratorTesterTest testCase = new com.google.common.collect.testing.IteratorTesterTest();
+  testCase.testUnknownOrderUnrecognizedElement();
+}
+
+public void testVerifyCanThrowAssertionThatFailsTest() throws Exception {
+  com.google.common.collect.testing.IteratorTesterTest testCase = new com.google.common.collect.testing.IteratorTesterTest();
+  testCase.testVerifyCanThrowAssertionThatFailsTest();
+}
+
+public void testVerifyGetsCalled() throws Exception {
+  com.google.common.collect.testing.IteratorTesterTest testCase = new com.google.common.collect.testing.IteratorTesterTest();
+  testCase.testVerifyGetsCalled();
+}
+}
