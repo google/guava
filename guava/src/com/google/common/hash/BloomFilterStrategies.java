5c4ea63c433436163723bb4e79c104bcc54d80a1
Kurt Kluever <kak@google.com>
2014-03-31 14:31:56 -0400

    Add a new BF strategy. The old one is subtly broken. For more information, see: https://code.google.com/p/guava-libraries/issues/detail?id=1119 ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=63970075

--- guava/src/com/google/common/hash/BloomFilterStrategies.java
+++ guava/src/com/google/common/hash/BloomFilterStrategies.java
@@ -18,6 +18,7 @@ import static com.google.common.base.Preconditions.checkArgument;
 
 import com.google.common.math.LongMath;
 import com.google.common.primitives.Ints;
+import com.google.common.primitives.Longs;
 
 import java.math.RoundingMode;
 import java.util.Arrays;
@@ -32,6 +33,7 @@ import java.util.Arrays;
  * on their ordinal for BloomFilter serialization.
  *
  * @author Dimitris Andreou
+ * @author Kurt Alfred Kluever
  */
 enum BloomFilterStrategies implements BloomFilter.Strategy {
   /**
@@ -42,40 +44,100 @@ enum BloomFilterStrategies implements BloomFilter.Strategy {
   MURMUR128_MITZ_32() {
     @Override public <T> boolean put(T object, Funnel<? super T> funnel,
         int numHashFunctions, BitArray bits) {
+      long bitSize = bits.bitSize();
       long hash64 = Hashing.murmur3_128().hashObject(object, funnel).asLong();
       int hash1 = (int) hash64;
       int hash2 = (int) (hash64 >>> 32);
+
       boolean bitsChanged = false;
       for (int i = 1; i <= numHashFunctions; i++) {
-        int nextHash = hash1 + i * hash2;
-        if (nextHash < 0) {
-          nextHash = ~nextHash;
+        int combinedHash = hash1 + (i * hash2);
+        // Flip all the bits if it's negative (guaranteed positive number)
+        if (combinedHash < 0) {
+          combinedHash = ~combinedHash;
         }
-        bitsChanged |= bits.set(nextHash % bits.bitSize());
+        bitsChanged |= bits.set(combinedHash % bitSize);
       }
       return bitsChanged;
     }
 
     @Override public <T> boolean mightContain(T object, Funnel<? super T> funnel,
         int numHashFunctions, BitArray bits) {
+      long bitSize = bits.bitSize();
       long hash64 = Hashing.murmur3_128().hashObject(object, funnel).asLong();
       int hash1 = (int) hash64;
       int hash2 = (int) (hash64 >>> 32);
+
       for (int i = 1; i <= numHashFunctions; i++) {
-        int nextHash = hash1 + i * hash2;
-        if (nextHash < 0) {
-          nextHash = ~nextHash;
+        int combinedHash = hash1 + (i * hash2);
+        // Flip all the bits if it's negative (guaranteed positive number)
+        if (combinedHash < 0) {
+          combinedHash = ~combinedHash;
         }
-        if (!bits.get(nextHash % bits.bitSize())) {
+        if (!bits.get(combinedHash % bitSize)) {
           return false;
         }
       }
       return true;
     }
+  },
+  /**
+   * This strategy uses all 128 bits of {@link Hashing#murmur3_128} when hashing. It looks
+   * different than the implementation in MURMUR128_MITZ_32 because we're avoiding the
+   * multiplication in the loop and doing a (much simpler) += hash2. We're also changing the
+   * index to a positive number by AND'ing with Long.MAX_VALUE instead of flipping the bits.
+   */
+  MURMUR128_MITZ_64() {
+    @Override
+    public <T> boolean put(T object, Funnel<? super T> funnel,
+        int numHashFunctions, BitArray bits) {
+      long bitSize = bits.bitSize();
+      byte[] bytes = Hashing.murmur3_128().hashObject(object, funnel).getBytesInternal();
+      long hash1 = lowerEight(bytes);
+      long hash2 = upperEight(bytes);
+
+      boolean bitsChanged = false;
+      long combinedHash = hash1 + hash2;
+      for (int i = 0; i < numHashFunctions; i++) {
+        // Make the combined hash positive and indexable
+        bitsChanged |= bits.set((combinedHash & Long.MAX_VALUE) % bitSize);
+        combinedHash += hash2;
+      }
+      return bitsChanged;
+    }
+
+    @Override
+    public <T> boolean mightContain(T object, Funnel<? super T> funnel,
+        int numHashFunctions, BitArray bits) {
+      long bitSize = bits.bitSize();
+      byte[] bytes = Hashing.murmur3_128().hashObject(object, funnel).getBytesInternal();
+      long hash1 = lowerEight(bytes);
+      long hash2 = upperEight(bytes);
+
+      long combinedHash = hash1 + hash2;
+      for (int i = 0; i < numHashFunctions; i++) {
+        // Make the combined hash positive and indexable
+        if (!bits.get((combinedHash & Long.MAX_VALUE) % bitSize)) {
+          return false;
+        }
+        combinedHash += hash2;
+      }
+      return true;
+    }
+
+    private /* static */ long lowerEight(byte[] bytes) {
+      return Longs.fromBytes(
+          bytes[7], bytes[6], bytes[5], bytes[4], bytes[3], bytes[2], bytes[1], bytes[0]);
+    }
+
+    private /* static */ long upperEight(byte[] bytes) {
+      return Longs.fromBytes(
+          bytes[15], bytes[14], bytes[13], bytes[12], bytes[11], bytes[10], bytes[9], bytes[8]);
+    }
   };
 
   // Note: We use this instead of java.util.BitSet because we need access to the long[] data field
-  static class BitArray {
+  static final class BitArray {
     final long[] data;
     long bitCount;
 
