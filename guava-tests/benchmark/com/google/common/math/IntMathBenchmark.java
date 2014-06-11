1a8ec5098d5dc075c88e4b9ae4af846905c3ee23
Chris Povirk <cpovirk@google.com>
2014-06-11 13:34:30 -0400

    Add a benchmark for [Google-internal -- this CL will be rolled back in Guava] isPrime on positive inputs.  The input distribution is logarithmic. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=68733770

--- guava-tests/benchmark/com/google/common/math/IntMathBenchmark.java
+++ guava-tests/benchmark/com/google/common/math/IntMathBenchmark.java
@@ -46,8 +46,8 @@ public class IntMathBenchmark {
       exponent[i] = randomExponent();
       factorial[i] = RANDOM_SOURCE.nextInt(50);
       binomial[i] = RANDOM_SOURCE.nextInt(factorial[i] + 1);
-      positive[i] = randomPositiveBigInteger(Integer.SIZE - 2).intValue();
-      nonnegative[i] = randomNonNegativeBigInteger(Integer.SIZE - 2).intValue();
+      positive[i] = randomPositiveBigInteger(Integer.SIZE - 1).intValue();
+      nonnegative[i] = randomNonNegativeBigInteger(Integer.SIZE - 1).intValue();
       ints[i] = RANDOM_SOURCE.nextInt();
     }
   }
@@ -93,6 +93,17 @@ public class IntMathBenchmark {
     for (int i = 0; i < reps; i++) {
       int j = i & ARRAY_MASK;
       tmp += IntMath.binomial(factorial[j], binomial[j]);
+    }
+    return tmp;
+  }
+  
+  @Benchmark int isPrime(int reps) {
+    int tmp = 0;
+    for (int i = 0; i < reps; i++) {
+      int j = i & ARRAY_MASK;
+      if (IntMath.isPrime(positive[j])) {
+        tmp++;
+      }
     }
     return tmp;
   }
