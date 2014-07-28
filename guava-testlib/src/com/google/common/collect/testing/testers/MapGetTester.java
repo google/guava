328daca5b9116fb6de39ecb8d170b9945fcb1103
Christian Edward Gruber <cgruber@google.com>
2014-07-28 06:12:44 -0700

    Fix CollectionFeature.ALLOWS_NULL_QUERIES to MapFeature.ALLOWS_NULL_KEY_QUERIES.
    From leventov.ru: https://code.google.com/p/guava-libraries/issues/detail?id=1649#c10
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=71683488

--- guava-testlib/src/com/google/common/collect/testing/testers/MapGetTester.java
+++ guava-testlib/src/com/google/common/collect/testing/testers/MapGetTester.java
@@ -16,14 +16,13 @@
 
 package com.google.common.collect.testing.testers;
 
-import static com.google.common.collect.testing.features.CollectionFeature.ALLOWS_NULL_QUERIES;
 import static com.google.common.collect.testing.features.CollectionSize.ZERO;
 import static com.google.common.collect.testing.features.MapFeature.ALLOWS_NULL_KEYS;
+import static com.google.common.collect.testing.features.MapFeature.ALLOWS_NULL_KEY_QUERIES;
 
 import com.google.common.annotations.GwtCompatible;
 import com.google.common.collect.testing.AbstractMapTester;
 import com.google.common.collect.testing.WrongType;
-import com.google.common.collect.testing.features.CollectionFeature;
 import com.google.common.collect.testing.features.CollectionSize;
 import com.google.common.collect.testing.features.MapFeature;
 
@@ -47,12 +46,12 @@ public class MapGetTester<K, V> extends AbstractMapTester<K, V> {
     assertNull("get(notPresent) should return null", get(samples.e3.getKey()));
   }
 
-  @CollectionFeature.Require(ALLOWS_NULL_QUERIES)
+  @MapFeature.Require(ALLOWS_NULL_KEY_QUERIES)
   public void testGet_nullNotContainedButAllowed() {
     assertNull("get(null) should return null", get(null));
   }
 
-  @CollectionFeature.Require(absent = ALLOWS_NULL_QUERIES)
+  @MapFeature.Require(absent = ALLOWS_NULL_KEY_QUERIES)
   public void testGet_nullNotContainedAndUnsupported() {
     try {
       assertNull("get(null) should return null or throw", get(null));
