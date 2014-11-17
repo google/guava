7ac551423cddb0c81f1307e3b101c0b41cf34883
hamptos <hamptos@google.com>
2014-11-17 11:30:25 -0800

    Add a constructor to VerifyException to permit a cause to provided alongside a message. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=80128106

--- guava/src/com/google/common/base/VerifyException.java
+++ guava/src/com/google/common/base/VerifyException.java
@@ -36,4 +36,24 @@ public class VerifyException extends RuntimeException {
   public VerifyException(@Nullable String message) {
     super(message);
   }
+
+  /**
+   * Constructs a {@code VerifyException} with the cause {@code cause} and a message that is
+   * {@code null} if {@code cause} is null, and {@code cause.toString()} otherwise.
+   *
+   * @since 19.0
+   */
+  public VerifyException(@Nullable Throwable cause) {
+    super(cause);
+  }
+
+  /**
+   * Constructs a {@code VerifyException} with the message {@code message} and the cause
+   * {@code cause}.
+   *
+   * @since 19.0
+   */
+  public VerifyException(@Nullable String message, @Nullable Throwable cause) {
+    super(message, cause);
+  }
 }
