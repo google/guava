e07fb263aef0dec3060c82ac8836a4f01661abc7
dborowitz <dborowitz@google.com>
2015-01-14 10:12:20 -0800

    Fix comment typo in Maps: 0,75 -> 0.75.
    This may have been correct in the original author's locale, but it's at
    odds with the standard Java syntax.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=83948565

--- guava-gwt/src-super/com/google/common/collect/super/com/google/common/collect/Maps.java
+++ guava-gwt/src-super/com/google/common/collect/super/com/google/common/collect/Maps.java
@@ -188,7 +188,7 @@ public final class Maps {
     if (expectedSize < Ints.MAX_POWER_OF_TWO) {
       // This is the calculation used in JDK8 to resize when a putAll
       // happens; it seems to be the most conservative calculation we
-      // can make.  0,75 is the default load factor.
+      // can make.  0.75 is the default load factor.
       return (int) ((float) expectedSize / 0.75F + 1.0F);
     }
     return Integer.MAX_VALUE; // any large value
