6b76b9276557db9d9472f8a869cc819c70cfb3cb
cpovirk <cpovirk@google.com>
2015-01-22 10:42:57 -0800

    Annotate AsyncFunction parameter as @Nullable. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=84545868

--- guava/src/com/google/common/util/concurrent/AsyncFunction.java
+++ guava/src/com/google/common/util/concurrent/AsyncFunction.java
@@ -20,6 +20,8 @@ import com.google.common.annotations.GwtCompatible;
 
 import java.util.concurrent.Future;
 
+import javax.annotation.Nullable;
+
 /**
  * Transforms a value, possibly asynchronously. For an example usage and more
  * information, see {@link Futures#transform(ListenableFuture, AsyncFunction)}.
@@ -37,5 +39,5 @@ public interface AsyncFunction<I, O> {
    * <p>Throwing an exception from this method is equivalent to returning a
    * failing {@code Future}.
    */
-  ListenableFuture<O> apply(I input) throws Exception;
+  ListenableFuture<O> apply(@Nullable I input) throws Exception;
 }
