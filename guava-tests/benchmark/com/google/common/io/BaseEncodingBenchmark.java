87caee69fa9a53850ea31a8dd8287e2dacd2083f
yetanothernerd <yetanothernerd@google.com>
2015-01-27 02:39:53 -0800

    Add BaseEncoding benchmarks for decode and stream methods ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=84860248

--- guava-tests/benchmark/com/google/common/io/BaseEncodingBenchmark.java
+++ guava-tests/benchmark/com/google/common/io/BaseEncodingBenchmark.java
@@ -18,12 +18,20 @@ import com.google.caliper.BeforeExperiment;
 import com.google.caliper.Benchmark;
 import com.google.caliper.Param;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Random;
 
 /**
  * Benchmark for {@code BaseEncoding} performance.
  */
 public class BaseEncodingBenchmark {
+  private static final int INPUTS_COUNT = 0x1000;
+  private static final int INPUTS_MASK = 0xFFF;
+
   enum EncodingOption {
     BASE64(BaseEncoding.base64()),
     BASE64_URL(BaseEncoding.base64Url()),
@@ -44,21 +52,56 @@ public class BaseEncodingBenchmark {
   @Param({"10", "100", "10000"})
   int n;
   
-  private final byte[][] inputs = new byte[0x1000][];
+  private final byte[][] encodingInputs = new byte[INPUTS_COUNT][];
+  private final String[] decodingInputs = new String[INPUTS_COUNT];
 
   @BeforeExperiment
   public void setUp() {
     Random rng = new Random();
-    for (int i = 0; i < inputs.length; i++) {
-      inputs[i] = new byte[n];
-      rng.nextBytes(inputs[i]);
+    for (int i = 0; i < encodingInputs.length; i++) {
+      encodingInputs[i] = new byte[n];
+      rng.nextBytes(encodingInputs[i]);
+      decodingInputs[i] = encoding.encoding.encode(encodingInputs[i]);
     }
   }
   
   @Benchmark public int encode(int reps) {
     int tmp = 0;
     for (int i = 0; i < reps; i++) {
-      tmp += System.identityHashCode(encoding.encoding.encode(inputs[i & 0xFFF]));
+      tmp += System.identityHashCode(encoding.encoding.encode(encodingInputs[i & INPUTS_MASK]));
+    }
+    return tmp;
+  }
+
+  @Benchmark public int decode(int reps) {
+    int tmp = 0;
+    for (int i = 0; i < reps; i++) {
+      tmp += System.identityHashCode(encoding.encoding.decode(decodingInputs[i & INPUTS_MASK]));
+    }
+    return tmp;
+  }
+
+  @Benchmark public int encodingStream(int reps) throws IOException {
+    int tmp = 0;
+    for (int i = 0; i < reps; i++) {
+      StringWriter target = new StringWriter(2 * n);
+      OutputStream encodingStream = encoding.encoding.encodingStream(target);
+      encodingStream.write(encodingInputs[i & INPUTS_MASK]);
+      encodingStream.close();
+      tmp += target.getBuffer().length();
+    }
+    return tmp;
+  }
+
+  @Benchmark public int decodingStream(int reps) throws IOException {
+    int tmp = 0;
+    byte[] target = new byte[n];
+    for (int i = 0; i < reps; i++) {
+      StringReader source = new StringReader(decodingInputs[i & INPUTS_MASK]);
+      InputStream decodingStream = encoding.encoding.decodingStream(source);
+      decodingStream.read(target);
+      decodingStream.close();
+      tmp += target[0];
     }
     return tmp;
   }
