1ea3ff8fdeb285bb5c4fd1d3d5cc8781216d0a2c
joshv <joshv@google.com>
2015-05-06 08:19:44 -0700

    Give people who have never used UnmodifiableIterator before a hint. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=92927557

--- guava/src/com/google/common/collect/UnmodifiableIterator.java
+++ guava/src/com/google/common/collect/UnmodifiableIterator.java
@@ -23,6 +23,10 @@ import java.util.Iterator;
 /**
  * An iterator that does not support {@link #remove}.
  *
+ * <p>{@code UnmodifiableIterator} is used primarily in conjunction with implementations of
+ * {@link ImmutableCollection}, such as {@link ImmutableList}. You can, however, convert an existing
+ * iterator to an {@code UnmodifiableIterator} using {@link Iterators#unmodifiableIterator}.
+ *
  * @author Jared Levy
  * @since 2.0
  */
