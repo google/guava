fca77ba84b21b2629988295ded816807eb3033d5
Colin Decker <cgdecker@google.com>
2015-06-08 18:14:41 -0500

    Add scripts for updating Javadoc and JDiff.

---
+++ updatejavadocs.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -e -u
+
+if [ $# -eq 1 ]; then
+  REF=$1
+elif [ $# -eq 0 ]; then
+  REF=master
+else
+  echo "Usage: updatejavadoc <branch-or-tag>" >&2
+  exit 1
+fi
+
+if git diff --name-only | grep . -q ; then
+  echo "Uncommitted changes found. Aborting." >&2
+  exit 1
+fi
+
+# Make temp dir
+DOCTEMP=$(mktemp -d -t guava-$REF-docs)
+
+# Checkout the main code at the specified version
+git checkout -q $REF
+
+# Generate Javadoc and move it to temp dir
+mvn clean javadoc:javadoc -pl guava
+mv guava/target/site/apidocs/* $DOCTEMP/
+rm -fr guava/target
+
+# Get the version name we want to use under the releases/ directory
+if [[ $REF =~ v\d+.* ]]; then
+  # For a version tag (e.g. v18.0), remove the v
+  VERSION=${REF:1}
+elif [ $REF == "master" ]; then
+  # The master branch docs should be under "snapshot"
+  VERSION=snapshot
+else
+  VERSION=$REF
+fi
+
+# Switch back to gh-pages branch
+git checkout -q gh-pages
+
+DOCSDIR=releases/$VERSION/api/docs
+
+mkdir -p $DOCSDIR
+rm -fr $DOCSDIR
+mv $DOCTEMP $DOCSDIR
