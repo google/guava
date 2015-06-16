a6ce7b511bff96f39c56e598143557dcff08db86
cgdecker <cgdecker@google.com>
2015-06-15 17:58:47 -0700

    Automatically release snapshots to Maven and Javadoc/JDiff to gh-pages on push.
    - Based off of the Dagger config
    - The Javadoc/JDiff updates are handled by https://github.com/google/guava/blob/gh-pages/updaterelease.sh
    - Encrypted env variables have already been added for google/guava
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=96064069

---
+++ util/update_snapshot_docs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# see http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/ for details
+
+set -e -u
+
+if [ "$TRAVIS_REPO_SLUG" == "google/guava" ] && \
+   [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && \
+   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
+   [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo "Publishing Javadoc and JDiff..."
+
+  cd $HOME
+  git clone -q -b gh-pages https://${GH_TOKEN}@github.com/google/guava gh-pages > /dev/null
+  cd gh-pages
+
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+
+  ./updaterelease.sh snapshot
+
+  git push -fq origin gh-pages > /dev/null
+
+  echo "Javadoc and JDiff published to gh-pages."
+fi
