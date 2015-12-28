c06786e48a2acc141d8ce934cf1324af20401faf
Lee Carver <leeca@pnambic.com>
2015-12-28 11:05:08 -0800

    Remove unaccessible and unnecessary legacy XML_ESCAPER.

--- guava/src/com/google/common/xml/XmlEscapers.java
+++ guava/src/com/google/common/xml/XmlEscapers.java
@@ -113,7 +113,6 @@ public class XmlEscapers {
     return XML_ATTRIBUTE_ESCAPER;
   }
 
-  private static final Escaper XML_ESCAPER;
   private static final Escaper XML_CONTENT_ESCAPER;
   private static final Escaper XML_ATTRIBUTE_ESCAPER;
   static {
@@ -149,7 +148,6 @@ public class XmlEscapers {
     XML_CONTENT_ESCAPER = builder.build();
     builder.addEscape('\'', "&apos;");
     builder.addEscape('"', "&quot;");
-    XML_ESCAPER = builder.build();
     builder.addEscape('\t', "&#x9;");
     builder.addEscape('\n', "&#xA;");
     builder.addEscape('\r', "&#xD;");
