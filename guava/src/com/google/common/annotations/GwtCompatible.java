a55f0896718693e112ca95107fb0f0e9384f13af
cpovirk <cpovirk@google.com>
2016-01-11 05:16:05 -0800

    Reformat. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=111843256

--- guava/src/com/google/common/annotations/GwtCompatible.java
+++ guava/src/com/google/common/annotations/GwtCompatible.java
@@ -1,17 +1,15 @@
 /*
  * Copyright (C) 2009 The Guava Authors
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package com.google.common.annotations;
@@ -23,12 +21,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * The presence of this annotation on a type indicates that the type may be
- * used with the
- * <a href="http://code.google.com/webtoolkit/">Google Web Toolkit</a> (GWT).
- * When applied to a method, the return type of the method is GWT compatible.
- * It's useful to indicate that an instance created by factory methods has a GWT
- * serializable type.  In the following example,
+ * The presence of this annotation on a type indicates that the type may be used with the <a
+ * href="http://code.google.com/webtoolkit/">Google Web Toolkit</a> (GWT). When applied to a method,
+ * the return type of the method is GWT compatible. It's useful to indicate that an instance created
+ * by factory methods has a GWT serializable type. In the following example,
  *
  * <pre style="code">
  * {@literal @}GwtCompatible
@@ -40,9 +36,9 @@ import java.lang.annotation.Target;
  *   }
  * }
  * </pre>
- * <p>The return value of {@code Lists.newArrayList(E[])} has GWT
- * serializable type.  It is also useful in specifying contracts of interface
- * methods.  In the following example,
+ *
+ * <p>The return value of {@code Lists.newArrayList(E[])} has GWT serializable type. It is also
+ * useful in specifying contracts of interface methods. In the following example,
  *
  * <pre style="code">
  * {@literal @}GwtCompatible
@@ -52,11 +48,12 @@ import java.lang.annotation.Target;
  *   &lt;E> List&lt;E> newArrayList(E... elements);
  * }
  * </pre>
- * <p>The {@code newArrayList(E[])} method of all implementations of {@code
- * ListFactory} is expected to return a value with a GWT serializable type.
  *
- * <p>Note that a {@code GwtCompatible} type may have some {@link
- * GwtIncompatible} methods.
+ * <p>The {@code newArrayList(E[])} method of all implementations of {@code ListFactory} is expected
+ * to return a value with a GWT serializable type.
+ *
+ * <p>Note that a {@code GwtCompatible} type may have some {@link GwtIncompatible} methods.
+ *
  *
  * @author Charles Fry
  * @author Hayward Chan
@@ -68,8 +65,8 @@ import java.lang.annotation.Target;
 public @interface GwtCompatible {
 
   /**
-   * When {@code true}, the annotated type or the type of the method return
-   * value is GWT serializable.
+   * When {@code true}, the annotated type or the type of the method return value is GWT
+   * serializable.
    *
    * @see <a href="http://code.google.com/webtoolkit/doc/latest/DevGuideServerCommunication.html#DevGuideSerializableTypes">
    *     Documentation about GWT serialization</a>
@@ -77,9 +74,8 @@ public @interface GwtCompatible {
   boolean serializable() default false;
 
   /**
-   * When {@code true}, the annotated type is emulated in GWT. The emulated
-   * source (also known as super-source) is different from the implementation
-   * used by the JVM.
+   * When {@code true}, the annotated type is emulated in GWT. The emulated source (also known as
+   * super-source) is different from the implementation used by the JVM.
    *
    * @see <a href="http://code.google.com/webtoolkit/doc/latest/DevGuideOrganizingProjects.html#DevGuideModules">
    *     Documentation about GWT emulated source</a>
