6cca4e75672e214007598cb805e660e82259696a
cpovirk <cpovirk@google.com>
2016-02-18 07:59:10 -0800

    More reformatting. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=114963965

--- guava/src/com/google/thirdparty/publicsuffix/TrieParser.java
+++ guava/src/com/google/thirdparty/publicsuffix/TrieParser.java
@@ -1,17 +1,15 @@
 /*
  * Copyright (C) 2008 The Guava Authors
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package com.google.thirdparty.publicsuffix;
@@ -32,18 +30,17 @@ final class TrieParser {
   private static final Joiner PREFIX_JOINER = Joiner.on("");
 
   /**
-   * Parses a serialized trie representation of a map of reversed public
-   * suffixes into an immutable map of public suffixes.
+   * Parses a serialized trie representation of a map of reversed public suffixes into an immutable
+   * map of public suffixes.
    */
   static ImmutableMap<String, PublicSuffixType> parseTrie(CharSequence encoded) {
     ImmutableMap.Builder<String, PublicSuffixType> builder = ImmutableMap.builder();
     int encodedLen = encoded.length();
     int idx = 0;
     while (idx < encodedLen) {
-      idx += doParseTrieToBuilder(
-          Lists.<CharSequence>newLinkedList(),
-          encoded.subSequence(idx, encodedLen),
-          builder);
+      idx +=
+          doParseTrieToBuilder(
+              Lists.<CharSequence>newLinkedList(), encoded.subSequence(idx, encodedLen), builder);
     }
     return builder.build();
   }
@@ -51,8 +48,8 @@ final class TrieParser {
   /**
    * Parses a trie node and returns the number of characters consumed.
    *
-   * @param stack The prefixes that preceed the characters represented by this
-   *     node. Each entry of the stack is in reverse order.
+   * @param stack The prefixes that preceed the characters represented by this node. Each entry of
+   *     the stack is in reverse order.
    * @param encoded The serialized trie.
    * @param builder A map builder to which all entries will be added.
    * @return The number of characters consumed from {@code encoded}.
@@ -67,7 +64,7 @@ final class TrieParser {
     char c = '\0';
 
     // Read all of the characters for this node.
-    for ( ; idx < encodedLen; idx++) {
+    for (; idx < encodedLen; idx++) {
       c = encoded.charAt(idx);
       if (c == '&' || c == '?' || c == '!' || c == ':' || c == ',') {
         break;
