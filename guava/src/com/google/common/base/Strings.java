907f05f02b50895a4d7515b40e3ec77d16f3c6e5
goktug <goktug@google.com>
2016-02-25 22:15:58 -0800

    Automated g4 rollback of changelist 108345794.
    *** Reason for rollback ***
    
    Causes performance regression in J2CL transpiler and requires incorrect code to be accepted.
    
    *** Original change description ***
    
    Automated g4 rollback of changelist 108184450.
    
    *** Reason for rollback ***
    
    Change no longer needed, j2cl transpiler was updated.
    
    *** Original change description ***
    
    Remove import for java.util.Formatter since it's needed only for the javadoc.
    This class is not supported by GWT and in the new version of GWT (J2CL) we plan to strip not needed imports only if the class/method is marked with @GwtIncompatible.
    
    ***
    
    ***
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=115640681

--- guava/src/com/google/common/base/Strings.java
+++ guava/src/com/google/common/base/Strings.java
@@ -20,8 +20,6 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import com.google.common.annotations.GwtCompatible;
 import com.google.common.annotations.VisibleForTesting;
 
-import java.util.Formatter;
-
 import javax.annotation.Nullable;
 
 /**
@@ -79,7 +77,7 @@ public final class Strings {
    * <li>{@code padStart("2010", 3, '0')} returns {@code "2010"}
    * </ul>
    *
-   * <p>See {@link Formatter} for a richer set of formatting capabilities.
+   * <p>See {@link java.util.Formatter} for a richer set of formatting capabilities.
    *
    * @param string the string which should appear at the end of the result
    * @param minLength the minimum length the resulting string must have. Can be zero or negative, in
@@ -110,7 +108,7 @@ public final class Strings {
    * <li>{@code padEnd("2010", 3, '!')} returns {@code "2010"}
    * </ul>
    *
-   * <p>See {@link Formatter} for a richer set of formatting capabilities.
+   * <p>See {@link java.util.Formatter} for a richer set of formatting capabilities.
    *
    * @param string the string which should appear at the beginning of the result
    * @param minLength the minimum length the resulting string must have. Can be zero or negative, in
