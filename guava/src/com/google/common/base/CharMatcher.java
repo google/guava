71b34733742f6381fc2f384121567e02968aba71
cgdecker <cgdecker@google.com>
2016-03-05 08:26:31 -0800

    Fix documentation issues with CharMatcher.matchesNoneOf.
    Fixes external issue https://github.com/google/guava/issues/2410.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=116450985

--- guava/src/com/google/common/base/CharMatcher.java
+++ guava/src/com/google/common/base/CharMatcher.java
@@ -541,10 +541,10 @@ public abstract class CharMatcher implements Predicate<Character> {
    * {@code !matchesAnyOf(sequence)}.
    *
    * <p>The default implementation iterates over the sequence, invoking {@link #matches} for each
-   * character, until this returns {@code false} or the end is reached.
+   * character, until this returns {@code true} or the end is reached.
    *
    * @param sequence the character sequence to examine, possibly empty
-   * @return {@code true} if this matcher matches every character in the sequence, including when
+   * @return {@code true} if this matcher matches no characters in the sequence, including when
    *     the sequence is empty
    */
   public boolean matchesNoneOf(CharSequence sequence) {
