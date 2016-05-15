27c4c6100c9d2a71317eb3044e1fe60289701cb4
Jakub Malek <j.malek@findersystems.pl>
2016-05-15 12:51:15 +0200

    Removed usage of java.util.function package

--- guava-tests/test/com/google/common/base/PreconditionsTest.java
+++ guava-tests/test/com/google/common/base/PreconditionsTest.java
@@ -30,7 +30,6 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 
 import junit.framework.AssertionFailedError;
 import junit.framework.TestCase;
