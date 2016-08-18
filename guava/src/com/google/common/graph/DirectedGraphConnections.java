eb62df758064852a95a8095b4d3b2ff61a225cd4
jasexton <jasexton@google.com>
2016-08-18 10:20:50 -0700

    Optimize directed graphs to consume less memory, at the cost of additional code complexity. Some very rough benchmarks also seem to indicate this has slightly faster runtime. I suspect this is due to improved cache performance/locality when using a single Map.
    Memory: Directed mutable graphs consume less memory across the board, with graphs of small degree benefiting the most (memory consumption around 75% of previous). Directed immutable graph memory consumption is more of a wash - going down slightly for graphs of small degree and up for graphs of large degree.
    
    Runtime: Adding a few million edges seems to be about 10% faster.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=130653738

--- guava/src/com/google/common/graph/DirectedGraphConnections.java
+++ guava/src/com/google/common/graph/DirectedGraphConnections.java
@@ -17,17 +17,23 @@
 package com.google.common.graph;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.graph.GraphConstants.INNER_CAPACITY;
 import static com.google.common.graph.GraphConstants.INNER_LOAD_FACTOR;
+import static com.google.common.graph.Graphs.checkNonNegative;
+import static com.google.common.graph.Graphs.checkPositive;
 
+import com.google.common.collect.AbstractIterator;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Sets;
+import com.google.common.collect.UnmodifiableIterator;
+import java.util.AbstractSet;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import javax.annotation.Nullable;
 
 /**
  * An implementation of {@link GraphConnections} for directed graphs.
@@ -37,63 +43,207 @@ import java.util.Set;
  * @param <V> Value parameter type
  */
 final class DirectedGraphConnections<N, V> implements GraphConnections<N, V> {
-  private final Set<N> predecessors;
-  private final Map<N, V> successorValues;
+  /**
+   * A wrapper class to indicate a node is both a predecessor and successor while still providing
+   * the successor value.
+   */
+  private static final class PredAndSucc {
+    private final Object successorValue;
 
-  private DirectedGraphConnections(Set<N> predecessors, Map<N, V> successorValues) {
-    this.predecessors = checkNotNull(predecessors, "predecessors");
-    this.successorValues = checkNotNull(successorValues, "successorValues");
+    PredAndSucc(Object userValue) {
+      this.successorValue = userValue;
+    }
+  }
+  private static final Object PRED = new Object();
+
+  // Every value in this map must either be an instance of PredAndSucc with a successorValue of
+  // type V, PRED (representing predecessor), or an instance of type V (representing successor).
+  private final Map<N, Object> adjacentNodeValues;
+
+  private int predecessorCount;
+  private int successorCount;
+
+  private DirectedGraphConnections(
+      Map<N, Object> adjacentNodeValues, int predecessorCount, int successorCount) {
+    this.adjacentNodeValues = checkNotNull(adjacentNodeValues, "adjacentNodeValues");
+    this.predecessorCount = checkNonNegative(predecessorCount);
+    this.successorCount = checkNonNegative(successorCount);
+    checkState(predecessorCount <= adjacentNodeValues.size()
+        && successorCount <= adjacentNodeValues.size());
   }
 
   static <N, V> DirectedGraphConnections<N, V> of() {
+    // We store predecessors and successors in the same map, so double the initial capacity.
+    int initialCapacity = INNER_CAPACITY * 2;
     return new DirectedGraphConnections<N, V>(
-        new HashSet<N>(INNER_CAPACITY, INNER_LOAD_FACTOR),
-        new HashMap<N, V>(INNER_CAPACITY, INNER_LOAD_FACTOR));
+        new HashMap<N, Object>(initialCapacity, INNER_LOAD_FACTOR), 0, 0);
   }
 
   static <N, V> DirectedGraphConnections<N, V> ofImmutable(
       Set<N> predecessors, Map<N, V> successorValues) {
-    return new DirectedGraphConnections<N, V>(
-        ImmutableSet.copyOf(predecessors), ImmutableMap.copyOf(successorValues));
+    Map<N, Object> adjacentNodeValues = new HashMap<N, Object>();
+    adjacentNodeValues.putAll(successorValues);
+    for (N predecessor : predecessors) {
+      Object value = adjacentNodeValues.put(predecessor, PRED);
+      if (value != null) {
+        adjacentNodeValues.put(predecessor, new PredAndSucc(value));
+      }
+    }
+    return new DirectedGraphConnections<N, V>(ImmutableMap.copyOf(adjacentNodeValues),
+        predecessors.size(), successorValues.size());
   }
 
   @Override
   public Set<N> adjacentNodes() {
-    return Sets.union(predecessors, successorValues.keySet());
+    return Collections.unmodifiableSet(adjacentNodeValues.keySet());
   }
 
   @Override
   public Set<N> predecessors() {
-    return Collections.unmodifiableSet(predecessors);
+    return new AbstractSet<N>() {
+      @Override
+      public UnmodifiableIterator<N> iterator() {
+        final Iterator<Entry<N, Object>> entries = adjacentNodeValues.entrySet().iterator();
+        return new AbstractIterator<N>() {
+          @Override
+          protected N computeNext() {
+            while (entries.hasNext()) {
+              Entry<N, Object> entry = entries.next();
+              if (isPredecessor(entry.getValue())) {
+                return entry.getKey();
+              }
+            }
+            return endOfData();
+          }
+        };
+      }
+
+      @Override
+      public int size() {
+        return predecessorCount;
+      }
+
+      @Override
+      public boolean contains(@Nullable Object obj) {
+        return isPredecessor(adjacentNodeValues.get(obj));
+      }
+    };
   }
 
   @Override
   public Set<N> successors() {
-    return Collections.unmodifiableSet(successorValues.keySet());
+    return new AbstractSet<N>() {
+      @Override
+      public UnmodifiableIterator<N> iterator() {
+        final Iterator<Entry<N, Object>> entries = adjacentNodeValues.entrySet().iterator();
+        return new AbstractIterator<N>() {
+          @Override
+          protected N computeNext() {
+            while (entries.hasNext()) {
+              Entry<N, Object> entry = entries.next();
+              if (isSuccessor(entry.getValue())) {
+                return entry.getKey();
+              }
+            }
+            return endOfData();
+          }
+        };
+      }
+
+      @Override
+      public int size() {
+        return successorCount;
+      }
+
+      @Override
+      public boolean contains(@Nullable Object obj) {
+        return isSuccessor(adjacentNodeValues.get(obj));
+      }
+    };
   }
 
+  @SuppressWarnings("unchecked")
   @Override
   public V value(Object node) {
-    return successorValues.get(node);
+    Object value = adjacentNodeValues.get(node);
+    if (value == PRED) {
+      return null;
+    }
+    if (value instanceof PredAndSucc) {
+      return (V) ((PredAndSucc) value).successorValue;
+    }
+    return (V) value;
   }
 
+  @SuppressWarnings("unchecked")
   @Override
   public void removePredecessor(Object node) {
-    predecessors.remove(node);
+    Object previousValue = adjacentNodeValues.get(node);
+    if (previousValue == PRED) {
+      adjacentNodeValues.remove(node);
+      checkNonNegative(--predecessorCount);
+    } else if (previousValue instanceof PredAndSucc) {
+      adjacentNodeValues.put((N) node, ((PredAndSucc) previousValue).successorValue);
+      checkNonNegative(--predecessorCount);
+    }
   }
 
+  @SuppressWarnings("unchecked")
   @Override
   public V removeSuccessor(Object node) {
-    return successorValues.remove(node);
+    Object previousValue = adjacentNodeValues.get(node);
+    if (previousValue == null || previousValue == PRED) {
+      return null;
+    } else if (previousValue instanceof PredAndSucc) {
+      adjacentNodeValues.put((N) node, PRED);
+      checkNonNegative(--successorCount);
+      return (V) ((PredAndSucc) previousValue).successorValue;
+    } else { // successor
+      adjacentNodeValues.remove(node);
+      checkNonNegative(--successorCount);
+      return (V) previousValue;
+    }
   }
 
   @Override
   public void addPredecessor(N node, V unused) {
-    predecessors.add(node);
+    Object previousValue = adjacentNodeValues.put(node, PRED);
+    if (previousValue == null) {
+      checkPositive(++predecessorCount);
+    } else if (previousValue instanceof PredAndSucc) {
+      // Restore previous PredAndSucc object.
+      adjacentNodeValues.put(node, previousValue);
+    } else if (previousValue != PRED) { // successor
+      // Do NOT use method parameter value 'unused'. In directed graphs, successors store the value.
+      adjacentNodeValues.put(node, new PredAndSucc(previousValue));
+      checkPositive(++predecessorCount);
+    }
   }
 
+  @SuppressWarnings("unchecked")
   @Override
   public V addSuccessor(N node, V value) {
-    return successorValues.put(node, value);
+    Object previousValue = adjacentNodeValues.put(node, value);
+    if (previousValue == null) {
+      checkPositive(++successorCount);
+      return null;
+    } else if (previousValue instanceof PredAndSucc) {
+      adjacentNodeValues.put(node, new PredAndSucc(value));
+      return (V) ((PredAndSucc) previousValue).successorValue;
+    } else if (previousValue == PRED) {
+      adjacentNodeValues.put(node, new PredAndSucc(value));
+      checkPositive(++successorCount);
+      return null;
+    } else { // successor
+      return (V) previousValue;
+    }
+  }
+
+  private static boolean isPredecessor(@Nullable Object value) {
+    return (value == PRED) || (value instanceof PredAndSucc);
+  }
+
+  private static boolean isSuccessor(@Nullable Object value) {
+    return (value != PRED) && (value != null);
   }
 }
