d7a12d09fe0f4ee389a7a9bde75415a64fa249cd
jasexton <jasexton@google.com>
2016-08-19 08:16:45 -0700

    Add NetworkMutationTest (parallel to GraphMutationTest).
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=130753566

--- guava-tests/test/com/google/common/graph/GraphMutationTest.java
+++ guava-tests/test/com/google/common/graph/GraphMutationTest.java
@@ -28,33 +28,38 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 /**
- * Tests for repeated node and edge addition and removal.
+ * Tests for repeated node and edge addition and removal in a {@link Graph}.
  */
 @RunWith(JUnit4.class)
 
-public final class MutationTest {
-  private static final int NUM_TRIALS = 100;
+public final class GraphMutationTest {
+  private static final int NUM_TRIALS = 50;
   private static final int NUM_NODES = 100;
   private static final int NUM_EDGES = 1000;
+  private static final int NODE_POOL_SIZE = 1000; // must be >> NUM_NODES
 
   @Test
   public void directedBasicGraph() {
-    testBasicGraph(BasicGraphBuilder.directed());
+    testBasicGraphMutation(BasicGraphBuilder.directed());
   }
 
   @Test
   public void undirectedBasicGraph() {
-    testBasicGraph(BasicGraphBuilder.undirected());
+    testBasicGraphMutation(BasicGraphBuilder.undirected());
   }
 
-  private static void testBasicGraph(BasicGraphBuilder<? super Integer> graphBuilder) {
+  private static void testBasicGraphMutation(BasicGraphBuilder<? super Integer> graphBuilder) {
     Random gen = new Random(42); // Fixed seed so test results are deterministic.
 
     for (int trial = 0; trial < NUM_TRIALS; ++trial) {
       MutableBasicGraph<Integer> graph = graphBuilder.allowsSelfLoops(true).build();
 
+      assertThat(graph.nodes()).isEmpty();
+      assertThat(graph.edges()).isEmpty();
+      AbstractGraphTest.validateGraph(graph);
+
       while (graph.nodes().size() < NUM_NODES) {
-        graph.addNode(gen.nextInt());
+        graph.addNode(gen.nextInt(NODE_POOL_SIZE));
       }
       ArrayList<Integer> nodeList = new ArrayList<Integer>(graph.nodes());
       while (graph.edges().size() < NUM_EDGES) {
@@ -62,6 +67,8 @@ public final class MutationTest {
       }
       ArrayList<Endpoints<Integer>> edgeList = new ArrayList<Endpoints<Integer>>(graph.edges());
 
+      assertThat(graph.nodes()).hasSize(NUM_NODES);
+      assertThat(graph.edges()).hasSize(NUM_EDGES);
       AbstractGraphTest.validateGraph(graph);
 
       Collections.shuffle(edgeList, gen);
@@ -93,9 +100,11 @@ public final class MutationTest {
       assertThat(graph.edges()).isEmpty(); // no edges can remain if there's no nodes
       AbstractGraphTest.validateGraph(graph);
 
+      Collections.shuffle(nodeList, gen);
       for (Integer node : nodeList) {
         assertThat(graph.addNode(node)).isTrue();
       }
+      Collections.shuffle(edgeList, gen);
       for (Endpoints<Integer> edge : edgeList) {
         assertThat(graph.putEdge(edge.nodeA(), edge.nodeB())).isTrue();
       }
