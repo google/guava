cd9a472e2588ec53109be8a1927cd4cb2af0f0d6
lowasser <lowasser@google.com>
2016-11-09 12:50:28 -0800

    Add mutable Table collector to Tables, including a version with a merge function to match Collectors.toMap and ImmutableMap.toImmutableMap.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=138673160

--- guava-tests/test/com/google/common/collect/TablesTest.java
+++ guava-tests/test/com/google/common/collect/TablesTest.java
@@ -18,9 +18,13 @@ package com.google.common.collect;
 
 import com.google.common.annotations.GwtCompatible;
 import com.google.common.annotations.GwtIncompatible;
+import com.google.common.base.Equivalence;
 import com.google.common.collect.Table.Cell;
+import com.google.common.testing.CollectorTester;
 import com.google.common.testing.EqualsTester;
 import com.google.common.testing.SerializableTester;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
 import junit.framework.TestCase;
 
 /**
@@ -30,6 +34,88 @@ import junit.framework.TestCase;
  */
 @GwtCompatible(emulated = true)
 public class TablesTest extends TestCase {
+
+  public void testToTable() {
+    Collector<Cell<String, String, Integer>, ?, Table<String, String, Integer>> collector =
+        Tables.toTable(Cell::getRowKey, Cell::getColumnKey, Cell::getValue, HashBasedTable::create);
+    Equivalence<Table<String, String, Integer>> equivalence =
+        Equivalence.equals().<Cell<String, String, Integer>>pairwise().onResultOf(Table::cellSet);
+    CollectorTester.of(collector, equivalence)
+        .expectCollects(
+            new ImmutableTable.Builder<String, String, Integer>()
+                .put("one", "uno", 1)
+                .put("two", "dos", 2)
+                .put("three", "tres", 3)
+                .build(),
+            Tables.immutableCell("one", "uno", 1),
+            Tables.immutableCell("two", "dos", 2),
+            Tables.immutableCell("three", "tres", 3));
+  }
+
+  public void testToTableNullMerge() {
+    Collector<Cell<String, String, Integer>, ?, Table<String, String, Integer>> collector =
+        Tables.toTable(
+            Cell::getRowKey,
+            Cell::getColumnKey,
+            Cell::getValue,
+            (Integer v1, Integer v2) -> null,
+            HashBasedTable::create);
+    Equivalence<Table<String, String, Integer>> equivalence =
+        Equivalence.equals().<Cell<String, String, Integer>>pairwise().onResultOf(Table::cellSet);
+    CollectorTester.of(collector, equivalence)
+        .expectCollects(
+            ImmutableTable.of(),
+            Tables.immutableCell("one", "uno", 1),
+            Tables.immutableCell("one", "uno", 2));
+  }
+
+  public void testToTableNullValues() {
+    Collector<Cell<String, String, Integer>, ?, Table<String, String, Integer>> collector =
+        Tables.toTable(
+            Cell::getRowKey,
+            Cell::getColumnKey,
+            Cell::getValue,
+            () -> ArrayTable.create(ImmutableList.of("one"), ImmutableList.of("uno")));
+    try {
+      Stream.of(Tables.immutableCell("one", "uno", (Integer) null)).collect(collector);
+      fail("Expected NullPointerException");
+    } catch (NullPointerException expected) {
+    }
+  }
+
+  public void testToTableConflict() {
+    Collector<Cell<String, String, Integer>, ?, Table<String, String, Integer>> collector =
+        Tables.toTable(Cell::getRowKey, Cell::getColumnKey, Cell::getValue, HashBasedTable::create);
+    try {
+      Stream.of(Tables.immutableCell("one", "uno", 1), Tables.immutableCell("one", "uno", 2))
+          .collect(collector);
+      fail("Expected IllegalStateException");
+    } catch (IllegalStateException expected) {
+    }
+  }
+
+  public void testToTableMerging() {
+    Collector<Cell<String, String, Integer>, ?, Table<String, String, Integer>> collector =
+        Tables.toTable(
+            Cell::getRowKey,
+            Cell::getColumnKey,
+            Cell::getValue,
+            Integer::sum,
+            HashBasedTable::create);
+    Equivalence<Table<String, String, Integer>> equivalence =
+        Equivalence.equals().<Cell<String, String, Integer>>pairwise().onResultOf(Table::cellSet);
+    CollectorTester.of(collector, equivalence)
+        .expectCollects(
+            new ImmutableTable.Builder<String, String, Integer>()
+                .put("one", "uno", 1)
+                .put("two", "dos", 6)
+                .put("three", "tres", 3)
+                .build(),
+            Tables.immutableCell("one", "uno", 1),
+            Tables.immutableCell("two", "dos", 2),
+            Tables.immutableCell("three", "tres", 3),
+            Tables.immutableCell("two", "dos", 4));
+  }
 
   @GwtIncompatible // SerializableTester
   public void testImmutableEntrySerialization() {
