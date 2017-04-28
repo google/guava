f35bb61b508e7976994da1f91786340b4923cb8e
ronshapiro <ronshapiro@google.com>
2017-04-28 08:34:29 -0700

    Fix deploy_snapshot for android snapshots
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=154542742

--- util/deploy_snapshot.sh
+++ util/deploy_snapshot.sh
@@ -4,19 +4,19 @@
 
 set -e -u
 
+function mvn_deploy() {
+  mvn clean source:jar javadoc:jar deploy \
+    --settings="$(dirname $0)/settings.xml" -DskipTests=true "$@"
+}
+
 if [ "$TRAVIS_REPO_SLUG" == "google/guava" ] && \
    [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
    [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "Publishing Maven snapshot..."
 
-  mvn_command=(
-    mvn clean source:jar javadoc:jar deploy --settings="$(dirname $0)/settings.xml" -DskipTests=true
-  )
-  "${mvn_command[@]}"
-
-  mvn_command+=(-f android/pom.xml
-  "${mvn_command[@]}"              
+  mvn_deploy
+  mvn_deploy -f android/pom.xml
 
   echo "Maven snapshot published."
 fi
