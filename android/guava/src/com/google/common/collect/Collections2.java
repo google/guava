78dab8037a51b63e0e0a92ac0c4a427591d97219
lowasser <lowasser@google.com>
2017-05-17 10:14:27 -0700

    Stick to IntMath in Collections2.orderedPermutations.size.
    Effects on Collections2ProGuard:
    89279 bytes => 85910 bytes
    38 classes => 36 classes
    202 methods => 199 methods
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=156321417

--- android/guava/src/com/google/common/collect/Collections2.java
+++ android/guava/src/com/google/common/collect/Collections2.java
@@ -19,7 +19,6 @@ package com.google.common.collect;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.collect.CollectPreconditions.checkNonnegative;
-import static com.google.common.math.LongMath.binomial;
 
 import com.google.common.annotations.Beta;
 import com.google.common.annotations.GwtCompatible;
@@ -464,27 +463,23 @@ public final class Collections2 {
      */
     private static <E> int calculateSize(
         List<E> sortedInputList, Comparator<? super E> comparator) {
-      long permutations = 1;
+      int permutations = 1;
       int n = 1;
       int r = 1;
       while (n < sortedInputList.size()) {
         int comparison = comparator.compare(sortedInputList.get(n - 1), sortedInputList.get(n));
         if (comparison < 0) {
           // We move to the next non-repeated element.
-          permutations *= binomial(n, r);
+          permutations = IntMath.saturatedMultiply(permutations, IntMath.binomial(n, r));
           r = 0;
-          if (!isPositiveInt(permutations)) {
+          if (permutations == Integer.MAX_VALUE) {
             return Integer.MAX_VALUE;
           }
         }
         n++;
         r++;
       }
-      permutations *= binomial(n, r);
-      if (!isPositiveInt(permutations)) {
-        return Integer.MAX_VALUE;
-      }
-      return (int) permutations;
+      return IntMath.saturatedMultiply(permutations, IntMath.binomial(n, r));
     }
 
     @Override
@@ -708,9 +703,5 @@ public final class Collections2 {
       map.put(e, map.get(e) + 1);
     }
     return map.entrySet();
-  }
-
-  private static boolean isPositiveInt(long n) {
-    return n >= 0 && n <= Integer.MAX_VALUE;
   }
 }
