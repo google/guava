54087fa7ccc66021f11e3d1bf478f44542ad9856
lowasser <lowasser@google.com>
2017-05-17 11:16:08 -0700

    Simplify Iterators.concat and optimize it for minimizing allocations, while preserving the smart behavior for nested Iterators.concats.
    Effects on IterablesProGuard:
    97559 bytes => 95471 bytes
    57 classes => 54 classes
    277 methods => 266 methods
    
    Effects on IteratorsProGuard:
    76937 bytes => 75212 bytes
    35 classes => 32 classes
    175 methods => 170 methods
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=156329581

--- android/guava/src/com/google/common/collect/FluentIterable.java
+++ android/guava/src/com/google/common/collect/FluentIterable.java
@@ -24,7 +24,6 @@ import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
 import com.google.common.base.Predicate;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
@@ -191,7 +190,7 @@ public abstract class FluentIterable<E> implements Iterable<E> {
    */
   @Beta
   public static <T> FluentIterable<T> concat(Iterable<? extends T> a, Iterable<? extends T> b) {
-    return concat(Arrays.asList(checkNotNull(a), checkNotNull(b)));
+    return concatNoDefensiveCopy(a, b);
   }
 
   /**
@@ -210,7 +209,7 @@ public abstract class FluentIterable<E> implements Iterable<E> {
   @Beta
   public static <T> FluentIterable<T> concat(
       Iterable<? extends T> a, Iterable<? extends T> b, Iterable<? extends T> c) {
-    return concat(Arrays.asList(checkNotNull(a), checkNotNull(b), checkNotNull(c)));
+    return concatNoDefensiveCopy(a, b, c);
   }
 
   /**
@@ -233,8 +232,7 @@ public abstract class FluentIterable<E> implements Iterable<E> {
       Iterable<? extends T> b,
       Iterable<? extends T> c,
       Iterable<? extends T> d) {
-    return concat(
-        Arrays.asList(checkNotNull(a), checkNotNull(b), checkNotNull(c), checkNotNull(d)));
+    return concatNoDefensiveCopy(a, b, c, d);
   }
 
   /**
@@ -254,11 +252,28 @@ public abstract class FluentIterable<E> implements Iterable<E> {
    */
   @Beta
   public static <T> FluentIterable<T> concat(Iterable<? extends T>... inputs) {
-    List<Iterable<? extends T>> list = new ArrayList<>(inputs.length);
+    return concatNoDefensiveCopy(Arrays.copyOf(inputs, inputs.length));
+  }
+
+  /**
+   * Concatenates a varargs array of iterables without making a defensive copy of the array.
+   */
+  private static <T> FluentIterable<T> concatNoDefensiveCopy(
+      final Iterable<? extends T>... inputs) {
     for (Iterable<? extends T> input : inputs) {
-      list.add(checkNotNull(input));
+      checkNotNull(input);
     }
-    return concat(list);
+    return new FluentIterable<T>() {
+      @Override
+      public Iterator<T> iterator() {
+        @SuppressWarnings("unchecked")
+        Iterator<? extends T>[] iterators = new Iterator[inputs.length];
+        for (int i = 0; i < inputs.length; i++) {
+          iterators[i] = inputs[i].iterator();
+        }
+        return Iterators.concatNoDefensiveCopy(iterators);
+      }
+    };
   }
 
   /**
@@ -282,7 +297,7 @@ public abstract class FluentIterable<E> implements Iterable<E> {
     return new FluentIterable<T>() {
       @Override
       public Iterator<T> iterator() {
-        return Iterators.concat(Iterables.transform(inputs, Iterables.<T>toIterator()).iterator());
+        return Iterators.concat(Iterators.transform(inputs.iterator(), Iterables.<T>toIterator()));
       }
     };
   }
