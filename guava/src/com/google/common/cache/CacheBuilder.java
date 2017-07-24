1d2cb71bf1c9a00d5fbefae4a11bdaa7e13ef02f
Igor Kryvenko <kryvenko7igor.@gmail.com>
2017-07-25 01:11:54 +0300

    hotfix: change CacheBuilder constructor to private, and rewrite tests

--- guava/src/com/google/common/cache/CacheBuilder.java
+++ guava/src/com/google/common/cache/CacheBuilder.java
@@ -233,8 +233,8 @@ public final class CacheBuilder<K, V> {
 
   Supplier<? extends StatsCounter> statsCounterSupplier = NULL_STATS_COUNTER;
 
-  // TODO(fry): make constructor private and update tests to use newBuilder
-  CacheBuilder() {}
+
+  private CacheBuilder() {}
 
   /**
    * Constructs a new {@code CacheBuilder} instance with default settings, including strong keys,
