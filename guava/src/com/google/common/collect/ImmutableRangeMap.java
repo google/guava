f2500af33c02cf1952d11497ef337cac77bd3051
cpovirk <cpovirk@google.com>
2017-08-15 08:36:23 -0700

    Fix @since tags for collectors that didn't make 23.0.
    (pointed out in https://github.com/google/guava/issues/2750#issuecomment-321129730)
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=165311302

--- guava/src/com/google/common/collect/ImmutableRangeMap.java
+++ guava/src/com/google/common/collect/ImmutableRangeMap.java
@@ -52,7 +52,7 @@ public class ImmutableRangeMap<K extends Comparable<?>, V> implements RangeMap<K
    * Returns a {@code Collector} that accumulates the input elements into a new {@code
    * ImmutableRangeMap}. As in {@link Builder}, overlapping ranges are not permitted.
    *
-   * @since 23.0
+   * @since 24.0
    */
   @Beta
   public static <T, K extends Comparable<? super K>, V>
