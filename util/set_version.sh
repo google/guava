157f1d2af826d0c558c7772d2698fbbaec28b0b1
ronshapiro <ronshapiro@google.com>
2017-09-27 13:20:38 -0700

    Update set_version.sh to handle new versioning scheme and ignore snapshot versions since those are no longer chaning.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=170242268

--- util/set_version.sh
+++ util/set_version.sh
@@ -8,13 +8,7 @@ if (( $# != 1 )); then
 fi
 
 version="$1"
-if [[ "${version}" =~ ^(.+)-SNAPSHOT$ ]]; then
-  android_version="${BASH_REMATCH[1]}-android-SNAPSHOT"
-else
-  android_version="${version}-android"
-fi
 
-mvn versions:set versions:commit -DnewVersion="${version}"
-mvn versions:set versions:commit -DnewVersion="${android_version}" \
-  -f android
+mvn versions:set versions:commit -DnewVersion="${version}-jre"
+mvn versions:set versions:commit -DnewVersion="${version}-android" -f android
 git commit -am "Set version numbers to ${version}"
