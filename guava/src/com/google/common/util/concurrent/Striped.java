01d18f2c3db799d144cc8e3308da3fb2ade19417
lowasser <lowasser@google.com>
2017-10-17 13:23:08 -0700

    Using Object instead of a specific type irritates some thread safety tests.  Fix that.
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=172504355

--- guava/src/com/google/common/util/concurrent/Striped.java
+++ guava/src/com/google/common/util/concurrent/Striped.java
@@ -321,9 +321,9 @@ public abstract class Striped<L> {
     private final Lock delegate;
 
     @SuppressWarnings("unused")
-    private final Object strongReference;
+    private final WeakSafeReadWriteLock strongReference;
 
-    WeakSafeLock(Lock delegate, Object strongReference) {
+    WeakSafeLock(Lock delegate, WeakSafeReadWriteLock strongReference) {
       this.delegate = delegate;
       this.strongReference = strongReference;
     }
@@ -344,9 +344,9 @@ public abstract class Striped<L> {
     private final Condition delegate;
 
     @SuppressWarnings("unused")
-    private final Object strongReference;
+    private final WeakSafeReadWriteLock strongReference;
 
-    WeakSafeCondition(Condition delegate, Object strongReference) {
+    WeakSafeCondition(Condition delegate, WeakSafeReadWriteLock strongReference) {
       this.delegate = delegate;
       this.strongReference = strongReference;
     }
