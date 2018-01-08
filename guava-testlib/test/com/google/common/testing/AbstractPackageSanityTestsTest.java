10ccb819ae4946fe6143590e3fedaea60eaf223f
cpovirk <cpovirk@google.com>
2018-01-08 12:38:07 -0800

    Let our internal Android test runner run AbstractPackageSanityTestsTest's nested test class directly.
    This is the path of least resistance, as detailed in the internal CL description.
    
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=181202500

--- guava-testlib/test/com/google/common/testing/AbstractPackageSanityTestsTest.java
+++ guava-testlib/test/com/google/common/testing/AbstractPackageSanityTestsTest.java
@@ -30,8 +30,22 @@ import junit.framework.TestCase;
  * @author Ben Yu
  */
 public class AbstractPackageSanityTestsTest extends TestCase {
+  /*
+   * This is a public type so that the Android test runner can create an instance directly as it
+   * insists upon doing. It then runs the test, which behaves exactly like this package's existing
+   * PackageSanityTests. (The test would run on the JVM, too, if not for the suppression below, and
+   * that would be a problem because it violates small-test rules. Note that we strip the
+   * suppression externally, but it's OK because we don't enforce test-size rules there.)
+   *
+   * We'd just use PackageSanityTests directly, saving us from needing this separate type, but we're
+   * currently skipping MediumTests on Android, and we skip them by not making them present at
+   * runtime at all. I could just make _this_ test a MediumTest, but then it wouldn't run on
+   * Android.... The right long-term fix is probably to get MediumTests running under Android by
+   * default and then suppress them strategically as needed.
+   */
+  public static final class ConcretePackageSanityTests extends AbstractPackageSanityTests {}
 
-  private final AbstractPackageSanityTests sanityTests = new AbstractPackageSanityTests() {};
+  private final AbstractPackageSanityTests sanityTests = new ConcretePackageSanityTests();
 
   public void testFindClassesToTest_testClass() {
     assertThat(findClassesToTest(ImmutableList.of(EmptyTest.class))).isEmpty();
