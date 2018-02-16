06b08063e2dbf3386a12e5bb9be9cec6432994ae
cpovirk <cpovirk@google.com>
2018-02-16 14:13:05 -0800

    Add @Override to some serialization methods.
    The problem with this is that they're not overrides under GWT. The solution is to mark them @GwtIncompatible -- probably not a bad idea, anyway, in case they ever come to depend on libraries not available under GWT.
    
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=186045036

--- guava/src/com/google/common/collect/ImmutableMultiset.java
+++ guava/src/com/google/common/collect/ImmutableMultiset.java
@@ -402,9 +402,8 @@ public abstract class ImmutableMultiset<E> extends ImmutableMultisetGwtSerializa
       return ImmutableMultiset.this.hashCode();
     }
 
-    // We can't label this with @Override, because it doesn't override anything
-    // in the GWT emulated version.
-    // TODO(cpovirk): try making all copies of this method @GwtIncompatible instead
+    @GwtIncompatible
+    @Override
     Object writeReplace() {
       return new EntrySetSerializedForm<E>(ImmutableMultiset.this);
     }
@@ -412,6 +411,7 @@ public abstract class ImmutableMultiset<E> extends ImmutableMultisetGwtSerializa
     private static final long serialVersionUID = 0;
   }
 
+  @GwtIncompatible
   static class EntrySetSerializedForm<E> implements Serializable {
     final ImmutableMultiset<E> multiset;
 
@@ -424,8 +424,8 @@ public abstract class ImmutableMultiset<E> extends ImmutableMultisetGwtSerializa
     }
   }
 
-  // We can't label this with @Override, because it doesn't override anything
-  // in the GWT emulated version.
+  @GwtIncompatible
+  @Override
   Object writeReplace() {
     return new SerializedForm(this);
   }
