8140d630fbe143499b50fe65a73c59aa0677cb64
nitu <nitu.choudhary@yahoo.com>
2018-03-28 10:27:55 -0700

    Renamed the packages

---
+++ cache/src/com/github/lafa/cache/base/package-info.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2007 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+/**
+ * Basic utility libraries and interfaces.
+ *
+ * <p>This package is a part of the open-source <a href="http://github.com/google/guava">Guava</a>
+ * library.
+ *
+ * <h2>Contents</h2>
+ *
+ * <h3>String-related utilities</h3>
+ *
+ * <ul>
+ *   <li>{@link com.github.lafa.cache.base.Ascii}
+ *   <li>{@link com.google.common.base.CaseFormat}
+ *   <li>{@link com.github.lafa.cache.base.CharMatcher}
+ *   <li>{@link com.google.common.base.Charsets}
+ *   <li>{@link com.github.lafa.cache.base.Joiner}
+ *   <li>{@link com.github.lafa.cache.base.Splitter}
+ *   <li>{@link com.google.common.base.Strings}
+ * </ul>
+ *
+ * <h3>Function types</h3>
+ *
+ * <ul>
+ *   <li>{@link com.github.lafa.cache.base.Function}, {@link com.google.common.base.Functions}
+ *   <li>{@link com.github.lafa.cache.base.Predicate}, {@link com.google.common.base.Predicates}
+ *   <li>{@link com.github.lafa.cache.base.Equivalence}
+ *   <li>{@link com.google.common.base.Converter}
+ *   <li>{@link com.github.lafa.cache.base.Supplier}, {@link com.github.lafa.cache.base.Suppliers}
+ * </ul>
+ *
+ * <h3>Other</h3>
+ *
+ * <ul>
+ *   <li>{@link com.google.common.base.Defaults}
+ *   <li>{@link com.google.common.base.Enums}
+ *   <li>{@link com.github.lafa.cache.base.Objects}
+ *   <li>{@link com.google.common.base.Optional}
+ *   <li>{@link com.github.lafa.cache.base.Preconditions}
+ *   <li>{@link com.github.lafa.cache.base.Stopwatch}
+ *   <li>{@link com.github.lafa.cache.base.Throwables}
+ * </ul>
+ *
+ */
+@CheckReturnValue
+@ParametersAreNonnullByDefault
+package com.github.lafa.cache.base;
+
+import com.google.errorprone.annotations.CheckReturnValue;
+import javax.annotation.ParametersAreNonnullByDefault;
