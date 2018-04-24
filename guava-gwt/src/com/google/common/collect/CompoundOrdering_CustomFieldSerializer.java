f89ece5721b2f637fe754937ff1f3c86d80bb196
cpovirk <cpovirk@google.com>
2018-04-24 11:05:03 -0700

    Avoid presizing arrays.
    RELNOTES=Fixed Denial of Service vulnerability for servers that use Guava and deserialize attacker data: [CVE-2018-10237](https://github.com/google/guava/wiki/CVE-2018-10237).
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=194113840

--- guava-gwt/src/com/google/common/collect/CompoundOrdering_CustomFieldSerializer.java
+++ guava-gwt/src/com/google/common/collect/CompoundOrdering_CustomFieldSerializer.java
@@ -36,7 +36,7 @@ public class CompoundOrdering_CustomFieldSerializer {
   public static CompoundOrdering<Object> instantiate(SerializationStreamReader reader)
       throws SerializationException {
     int n = reader.readInt();
-    List<Comparator<Object>> comparators = new ArrayList<>(n);
+    List<Comparator<Object>> comparators = new ArrayList<>();
     for (int i = 0; i < n; i++) {
       comparators.add((Comparator<Object>) reader.readObject());
     }
