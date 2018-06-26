1b94234578817593da7275fc4afaa88a996355f9
Hadrien Kohl <hadrien.kohl@gmail.com>
2018-06-26 15:12:08 -0700

    Implement Lists.cartesianProduct.indexOf efficiently.
    Fixes https://github.com/google/guava/pull/2544/
    
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=202202752

--- guava/src/com/google/common/collect/CartesianList.java
+++ guava/src/com/google/common/collect/CartesianList.java
@@ -67,6 +67,28 @@ final class CartesianList<E> extends AbstractList<List<E>> implements RandomAcce
   }
 
   @Override
+  public int indexOf(Object o) {
+    if (!(o instanceof List)) {
+      return -1;
+    }
+    List<?> list = (List<?>) o;
+    if (list.size() != axes.size()) {
+      return -1;
+    }
+    ListIterator<?> itr = list.listIterator();
+    int computedIndex = 0;
+    while (itr.hasNext()) {
+      int axisIndex = itr.nextIndex();
+      int elemIndex = axes.get(axisIndex).indexOf(itr.next());
+      if (elemIndex == -1) {
+        return -1;
+      }
+      computedIndex += elemIndex * axesSizeProduct[axisIndex + 1];
+    }
+    return computedIndex;
+  }
+
+  @Override
   public ImmutableList<E> get(final int index) {
     checkElementIndex(index, size());
     return new ImmutableList<E>() {
@@ -97,20 +119,6 @@ final class CartesianList<E> extends AbstractList<List<E>> implements RandomAcce
 
   @Override
   public boolean contains(@Nullable Object o) {
-    if (!(o instanceof List)) {
-      return false;
-    }
-    List<?> list = (List<?>) o;
-    if (list.size() != axes.size()) {
-      return false;
-    }
-    ListIterator<?> itr = list.listIterator();
-    while (itr.hasNext()) {
-      int index = itr.nextIndex();
-      if (!axes.get(index).contains(itr.next())) {
-        return false;
-      }
-    }
-    return true;
+    return indexOf(o) != -1;
   }
 }
