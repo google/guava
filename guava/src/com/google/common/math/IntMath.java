e48a41b70e2962048f0ac33f8172db65ae977200
nd <nhat.ldinh@gmail.com>
2018-07-02 12:18:29 -0700

    added annotation

--- guava/src/com/google/common/math/IntMath.java
+++ guava/src/com/google/common/math/IntMath.java
@@ -211,7 +211,7 @@ public final class IntMath {
 
   // maxLog10ForLeadingZeros[i] == floor(log10(2^(Long.SIZE - i)))
   @VisibleForTesting
-  static final @IndexFor(value = {"powersOf10", "halfPowersOf10"}) byte[] maxLog10ForLeadingZeros = {
+  static final @IndexFor(value = {"powersOf10", "halfPowersOf10"}) byte @MinLen(33)[] maxLog10ForLeadingZeros = {
     9, 9, 9, 8, 8, 8, 7, 7, 7, 6, 6, 6, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 3, 2, 2, 2, 1, 1, 1, 0, 0, 0,
     0
   };
