f6324b4b543695135f8e5d84222a94b1c991a26f
nd <nhat.ldinh@gmail.com>
2018-07-25 15:53:21 -0700

    correction

--- guava/src/com/google/common/math/LongMath.java
+++ guava/src/com/google/common/math/LongMath.java
@@ -202,8 +202,8 @@ public final class LongMath {
           won't return 0 and cause an error */
           "upperbound:assignment.type.incompatible",/* (2): except for element at index 0 in `maxLog10ForLeadingZeros`, the rest
           can be indexed for `powersOf10` */
-          "upperbound:array.access.unsafe.high"/*(1): `Integer.numberOfLeadingZeros(x)` return an int value range from 0 to 32.
-          Since maxLog10ForLeadingZeros has min length of 32, array access is safe.
+          "upperbound:array.access.unsafe.high"/*(1): `Integer.numberOfLeadingZeros(x)` return an int value range from 0 to 64.
+          Since maxLog10ForLeadingZeros has min length of 64, array access is safe.
           */})
   static @IndexFor(value = {"powersOf10", "halfPowersOf10"}) int log10Floor(long x) {
     /*
