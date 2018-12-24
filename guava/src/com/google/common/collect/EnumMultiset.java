4e375d45c70385a1b9e8b672dd66a4e7f795fe5f
Oleksii Bondar <Oleksii.Bondar@nordstrom.com>
2018-12-24 14:51:47 -0800

    remove unused and/or update with proper suppress warning types

--- guava/src/com/google/common/collect/EnumMultiset.java
+++ guava/src/com/google/common/collect/EnumMultiset.java
@@ -107,7 +107,6 @@ public final class EnumMultiset<E extends Enum<E>> extends AbstractMultiset<E>
    * Returns {@code element} cast to {@code E}, if it actually is a nonnull E. Otherwise, throws
    * either a NullPointerException or a ClassCastException as appropriate.
    */
-  @SuppressWarnings("unchecked")
   void checkIsE(@Nullable Object element) {
     checkNotNull(element);
     if (!isActuallyE(element)) {
