dd3c1391646098008046c8f687c471ea53236dad
majorde <majorde@google.com>
2019-01-22 13:00:24 -0800

    Swap the parameter order of the Collections.disjoint call in Sets.intersection
    Collections.disjoint is implemented in such a way that it has better
    performance if its 2nd parameter is the smaller of the two when both params are
    instances of Set. The Sets.intersection javadoc says it has better performance
    when set1 is the smaller set, but that is currently not the case for the
    isEmpty operation specifically.
    
    Our team ran into a pathological case of this where we check for an empty
    intersection of two sets, one being ~5 orders of magnitude larger than the
    other.
    
    RELNOTES=Update isEmpty operation of intersection to follow the smaller set first advice in the javadoc.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=230394245

--- android/guava/src/com/google/common/collect/Sets.java
+++ android/guava/src/com/google/common/collect/Sets.java
@@ -784,7 +784,7 @@ public final class Sets {
 
       @Override
       public boolean isEmpty() {
-        return Collections.disjoint(set1, set2);
+        return Collections.disjoint(set2, set1);
       }
 
       @Override
