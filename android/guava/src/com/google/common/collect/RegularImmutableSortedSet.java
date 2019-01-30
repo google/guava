3d35188141ca572ac6ba9e3fe180242d5f511dc8
Oleksii Bondar <alexBondar.kha@gmail.com>
2019-01-30 11:51:19 -0800

    Remove unused imports and warnings.
    Fixes #3356
    
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=231638663

--- android/guava/src/com/google/common/collect/RegularImmutableSortedSet.java
+++ android/guava/src/com/google/common/collect/RegularImmutableSortedSet.java
@@ -37,7 +37,7 @@ import org.checkerframework.checker.nullness.compatqual.NullableDecl;
  * @author Louis Wasserman
  */
 @GwtCompatible(serializable = true, emulated = true)
-@SuppressWarnings("serial")
+@SuppressWarnings({"serial", "rawtypes"})
 final class RegularImmutableSortedSet<E> extends ImmutableSortedSet<E> {
   static final RegularImmutableSortedSet<Comparable> NATURAL_EMPTY_SET =
       new RegularImmutableSortedSet<>(ImmutableList.<Comparable>of(), Ordering.natural());
