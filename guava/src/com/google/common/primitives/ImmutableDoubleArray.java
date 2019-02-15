7eafede7552490351956e8038bb07596b227e3ef
kevinb <kevinb@google.com>
2019-02-15 14:22:08 -0800

    Modernize book reference.
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=234212413

--- guava/src/com/google/common/primitives/ImmutableDoubleArray.java
+++ guava/src/com/google/common/primitives/ImmutableDoubleArray.java
@@ -41,8 +41,8 @@ import org.checkerframework.checker.nullness.qual.Nullable;
  * <p>Advantages compared to {@code double[]}:
  *
  * <ul>
- *   <li>All the many well-known advantages of immutability (read <i>Effective Java</i>, second
- *       edition, Item 15).
+ *   <li>All the many well-known advantages of immutability (read <i>Effective Java</i>, third
+ *       edition, Item 17).
  *   <li>Has the value-based (not identity-based) {@link #equals}, {@link #hashCode}, and {@link
  *       #toString} behavior you expect.
  *   <li>Offers useful operations beyond just {@code get} and {@code length}, so you don't have to
