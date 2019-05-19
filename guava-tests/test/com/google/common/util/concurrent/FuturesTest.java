df25b6fcc8dd859b926fb7ef95f5b1fb34147862
cpovirk <cpovirk@google.com>
2019-05-19 10:34:00 -0700

    Remove CheckedFuture utilities from util.concurrent.Futures.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=248947498

--- guava-tests/test/com/google/common/util/concurrent/FuturesTest.java
+++ guava-tests/test/com/google/common/util/concurrent/FuturesTest.java
@@ -46,7 +46,6 @@ import static com.google.common.util.concurrent.TestPlatform.clearInterrupt;
 import static com.google.common.util.concurrent.TestPlatform.getDoneFromTimeoutOverload;
 import static com.google.common.util.concurrent.Uninterruptibles.awaitUninterruptibly;
 import static com.google.common.util.concurrent.Uninterruptibles.getUninterruptibly;
-import static java.lang.Thread.currentThread;
 import static java.util.Arrays.asList;
 import static java.util.concurrent.Executors.newSingleThreadExecutor;
 import static java.util.concurrent.Executors.newSingleThreadScheduledExecutor;
