09bf107763561ae37ddd99cf024d6c5604eedfff
cpovirk <cpovirk@google.com>
2019-07-11 07:06:40 -0700

    Clarify that SortedSetMultimap exposes the values for a key in sorted order but does *not* necessarily expose the keys in sorted order.
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=257601507

--- guava/src/com/google/common/collect/SortedSetMultimap.java
+++ guava/src/com/google/common/collect/SortedSetMultimap.java
@@ -22,6 +22,7 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedMap;
 import java.util.SortedSet;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
@@ -95,7 +96,11 @@ public interface SortedSetMultimap<K, V> extends SetMultimap<K, V> {
    *
    * <p><b>Note:</b> The returned map's values are guaranteed to be of type {@link SortedSet}. To
    * obtain this map with the more specific generic type {@code Map<K, SortedSet<V>>}, call {@link
-   * Multimaps#asMap(SortedSetMultimap)} instead.
+   * Multimaps#asMap(SortedSetMultimap)} instead. <b>However</b>, the returned map <i>itself</i> is
+   * not necessarily a {@link SortedMap}: A {@code SortedSetMultimap} must expose the <i>values</i>
+   * for a given key in sorted order, but it need not expose the <i>keys</i> in sorted order.
+   * Individual {@code SortedSetMultimap} implementations, like those built with {@link
+   * MultimapBuilder#treeKeys()}, may make additional guarantees.
    */
   @Override
   Map<K, Collection<V>> asMap();
