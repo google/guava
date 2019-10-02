e2362def3408478a4a74d24671aef39171a1e93f
KantLeung <goVoidgo@gmail.com>
2019-10-02 08:15:10 -0700

    Simplify some code.
    Fixes #3580
    
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=272440567

--- android/guava/src/com/google/common/cache/LocalCache.java
+++ android/guava/src/com/google/common/cache/LocalCache.java
@@ -3815,9 +3815,7 @@ class LocalCache<K, V> extends AbstractMap<K, V> implements ConcurrentMap<K, V> 
         }
         sum -= segments[i].modCount;
       }
-      if (sum != 0L) {
-        return false;
-      }
+      return sum == 0L;
     }
     return true;
   }
