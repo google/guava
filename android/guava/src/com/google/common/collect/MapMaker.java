cc2f4e68bd5a68309d0cd875ee55afea1ed3774f
cpovirk <cpovirk@google.com>
2019-10-22 10:51:07 -0700

    Fix Javadoc errors (and sometimes warnings).
    These are mostly (perhaps entirely) problems new with Java 11's javadoc (or at least with Java 9+).
    
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=276098398

--- android/guava/src/com/google/common/collect/MapMaker.java
+++ android/guava/src/com/google/common/collect/MapMaker.java
@@ -66,10 +66,10 @@ import org.checkerframework.checker.nullness.compatqual.MonotonicNonNullDecl;
  * present in the map to be reclaimed by the garbage collector. Entries with reclaimed keys or
  * values may be removed from the map on each map modification or on occasional map accesses; such
  * entries may be counted by {@link Map#size}, but will never be visible to read or write
- * operations. A partially-reclaimed entry is never exposed to the user. Any {@link java.util.Entry}
+ * operations. A partially-reclaimed entry is never exposed to the user. Any {@link Map.Entry}
  * instance retrieved from the map's {@linkplain Map#entrySet entry set} is a snapshot of that
  * entry's state at the time of retrieval; such entries do, however, support {@link
- * java.util.Entry#setValue}, which simply calls {@link Map#put} on the entry's key.
+ * Map.Entry#setValue}, which simply calls {@link Map#put} on the entry's key.
  *
  * <p>The maps produced by {@code MapMaker} are serializable, and the deserialized maps retain all
  * the configuration properties of the original map. During deserialization, if the original map had
--- guava/src/com/google/common/collect/ArrayListMultimapGwtSerializationDependencies.java
+++ guava/src/com/google/common/collect/ArrayListMultimapGwtSerializationDependencies.java
@@ -24,7 +24,7 @@ import java.util.Map;
  * A dummy superclass to support GWT serialization of the element types of an {@link
  * ArrayListMultimap}. The GWT supersource for this class contains a field for each type.
  *
- * <p>For details about this hack, see {@link GwtSerializationDependencies}, which takes the same
+ * <p>For details about this hack, see {@code GwtSerializationDependencies}, which takes the same
  * approach but with a subclass rather than a superclass.
  *
  * <p>TODO(cpovirk): Consider applying this subclass approach to our other types.
