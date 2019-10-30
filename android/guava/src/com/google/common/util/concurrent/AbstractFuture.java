82b7589dfbfc5f2b6f68d6a6a1bb34e4633f4f3c
edalquist <edalquist@google.com>
2019-10-30 09:53:52 -0700

    Fix toString exception handling change from 957c37116fb2cc52b2f8b405871b65a84917f2ca.
    This is not an ideal code fix but it restores the previous functionality and adds a test that verifies the exception handling logic.
    
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=277525740

--- android/guava/src/com/google/common/util/concurrent/AbstractFuture.java
+++ android/guava/src/com/google/common/util/concurrent/AbstractFuture.java
@@ -1143,7 +1143,13 @@ public abstract class AbstractFuture<V> extends InternalFutureFailureAccess
     if (o == this) {
       return "this future";
     }
-    return String.valueOf(o);
+    try {
+      return String.valueOf(o);
+    } catch (RuntimeException e) {
+      // Don't call getMessage or toString() on the exception, in case the exception thrown by the
+      // user object is implemented with bugs similar to the user object.
+      return "Exception thrown from implementation: " + e.getClass();
+    }
   }
 
   /**
