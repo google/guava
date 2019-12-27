28b36c80447729d1edeffedd70dc4a7ac91e76ce
ghm <ghm@google.com>
2019-12-27 02:27:08 -0800

    Assign the result of contains() and containsAll() to a variable.
    This satisfies the CheckReturnValue checker.
    []
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=287279350

--- android/guava-tests/test/com/google/common/collect/SynchronizedQueueTest.java
+++ android/guava-tests/test/com/google/common/collect/SynchronizedQueueTest.java
@@ -161,8 +161,8 @@ public class SynchronizedQueueTest extends TestCase {
     create().add("foo");
     create().addAll(ImmutableList.of("foo"));
     create().clear();
-    create().contains("foo");
-    create().containsAll(ImmutableList.of("foo"));
+    boolean unused = create().contains("foo");
+    boolean unused2 = create().containsAll(ImmutableList.of("foo"));
     create().equals(new ArrayDeque<>(ImmutableList.of("foo")));
     create().hashCode();
     create().isEmpty();
