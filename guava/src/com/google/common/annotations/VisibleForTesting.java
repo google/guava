592202a6223b3c7834e495489e49a8628c50421f
adonovan <adonovan@google.com>
2020-06-02 09:38:49 -0700

    guava: disavow VisibleForTesting
    Johannes and I invented this class in the early days of Blaze,
    and in hindsight it was clearly a mistake; it encourages self-deception.
    This change adds a comment discouraging its further use.
    
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=314347092

--- guava/src/com/google/common/annotations/VisibleForTesting.java
+++ guava/src/com/google/common/annotations/VisibleForTesting.java
@@ -18,6 +18,13 @@ package com.google.common.annotations;
  * Annotates a program element that exists, or is more widely visible than otherwise necessary, only
  * for use in test code.
  *
+ * <p><b>Do not use this interface</b> for public or protected declarations: it is a fig leaf for
+ * bad design, and it does not prevent anyone from using the declaration---and experience has shown
+ * that they will. If the method breaks the encapsulation of its class, then its internal
+ * representation will be hard to change. Instead, use <a
+ * href="http://errorprone.info/bugpattern/RestrictedApiChecker">RestrictedApiChecker</a>, which
+ * enforces fine-grained visibility policies.
+ *
  * @author Johannes Henkel
  */
 @GwtCompatible
