3bb0d85dd8f0e404684bede39c06bc0602b3f0c5
kak <kak@google.com>
2020-06-16 13:23:36 -0700

    Add a Guava wiki entry about finding the min/max for various circumstances.
    RELNOTES=n/a
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=316745738

--- android/guava/src/com/google/common/collect/Comparators.java
+++ android/guava/src/com/google/common/collect/Comparators.java
@@ -108,8 +108,10 @@ public final class Comparators {
   /**
    * Returns the minimum of the two values. If the values compare as 0, the first is returned.
    *
-   * <p>To find the minimum of more than two values, use {@code Collections.min(Arrays.asList(a, b,
-   * c))} (static imports recommended).
+   * <p>The recommended solution for finding the {@code minimum} of some values depends on the type
+   * of your data and the number of elements you have. Read more in the Guava User Guide article on
+   * <a href="https://github.com/google/guava/wiki/CollectionUtilitiesExplained#comparators">{@code
+   * Comparators}</a>.
    *
    * @param a first value to compare, returned if less than or equal to b.
    * @param b second value to compare.
@@ -125,8 +127,10 @@ public final class Comparators {
    * Returns the minimum of the two values, according to the given comparator. If the values compare
    * as equal, the first is returned.
    *
-   * <p>To find the minimum of more than two values, use {@code Collections.min(Arrays.asList(a, b,
-   * c), comparator)} (static imports recommended).
+   * <p>The recommended solution for finding the {@code minimum} of some values depends on the type
+   * of your data and the number of elements you have. Read more in the Guava User Guide article on
+   * <a href="https://github.com/google/guava/wiki/CollectionUtilitiesExplained#comparators">{@code
+   * Comparators}</a>.
    *
    * @param a first value to compare, returned if less than or equal to b
    * @param b second value to compare.
@@ -142,8 +146,10 @@ public final class Comparators {
   /**
    * Returns the maximum of the two values. If the values compare as 0, the first is returned.
    *
-   * <p>To find the maximum of more than two values, use {@code Collections.max(Arrays.asList(a, b,
-   * c))} (static imports recommended).
+   * <p>The recommended solution for finding the {@code maximum} of some values depends on the type
+   * of your data and the number of elements you have. Read more in the Guava User Guide article on
+   * <a href="https://github.com/google/guava/wiki/CollectionUtilitiesExplained#comparators">{@code
+   * Comparators}</a>.
    *
    * @param a first value to compare, returned if greater than or equal to b.
    * @param b second value to compare.
@@ -159,8 +165,10 @@ public final class Comparators {
    * Returns the maximum of the two values, according to the given comparator. If the values compare
    * as equal, the first is returned.
    *
-   * <p>To find the maximum of more than two values, use {@code Collections.max(Arrays.asList(a, b,
-   * c), comparator)} (static imports recommended).
+   * <p>The recommended solution for finding the {@code maximum} of some values depends on the type
+   * of your data and the number of elements you have. Read more in the Guava User Guide article on
+   * <a href="https://github.com/google/guava/wiki/CollectionUtilitiesExplained#comparators">{@code
+   * Comparators}</a>.
    *
    * @param a first value to compare, returned if greater than or equal to b.
    * @param b second value to compare.
