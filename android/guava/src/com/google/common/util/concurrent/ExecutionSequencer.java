2ef8463838b23ae8c40c022fd96e7b143e0de018
clm <clm@google.com>
2020-06-16 20:05:31 -0700

    Revert db53357fea477695deeaa32746660bf86d6d51c1
    Broke a test
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=316808913

--- android/guava/src/com/google/common/util/concurrent/ExecutionSequencer.java
+++ android/guava/src/com/google/common/util/concurrent/ExecutionSequencer.java
@@ -15,7 +15,6 @@
 package com.google.common.util.concurrent;
 
 import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.util.concurrent.ExecutionSequencer.RunningState.CANCELLED;
 import static com.google.common.util.concurrent.ExecutionSequencer.RunningState.NOT_RUN;
 import static com.google.common.util.concurrent.ExecutionSequencer.RunningState.STARTED;
@@ -59,8 +58,6 @@ public final class ExecutionSequencer {
   private final AtomicReference<ListenableFuture<Object>> ref =
       new AtomicReference<>(immediateFuture(null));
 
-  private ThreadAndTask threadAndTask = new ThreadAndTask();
-
   /**
    * Enqueues a task to run when the previous task (if any) completes.
    *
@@ -70,7 +67,6 @@ public final class ExecutionSequencer {
    */
   public <T> ListenableFuture<T> submit(final Callable<T> callable, Executor executor) {
     checkNotNull(callable);
-    checkNotNull(executor);
     return submitAsync(
         new AsyncCallable<T>() {
           @Override
@@ -96,7 +92,6 @@ public final class ExecutionSequencer {
   public <T> ListenableFuture<T> submitAsync(
       final AsyncCallable<T> callable, final Executor executor) {
     checkNotNull(callable);
-    checkNotNull(executor);
     final AtomicReference<RunningState> runningState = new AtomicReference<>(NOT_RUN);
     final AsyncCallable<T> task =
         new AsyncCallable<T>() {
@@ -135,7 +130,7 @@ public final class ExecutionSequencer {
             new Executor() {
               @Override
               public void execute(Runnable runnable) {
-                oldFuture.addListener(runnable, new NonReentrantExecutor(executor));
+                oldFuture.addListener(runnable, executor);
               }
             });
 
@@ -168,125 +163,5 @@ public final class ExecutionSequencer {
     taskFuture.addListener(listener, directExecutor());
 
     return outputFuture;
-  }
-
-  /**
-   * This object is unsafely published, but avoids problematic races by relying exclusively on the
-   * identity equality of its Thread field so that the task field is only accessed by a single
-   * thread.
-   */
-  private static final class ThreadAndTask {
-    /**
-     * This field is only used for identity comparisons with the current thread. Field assignments
-     * are atomic, but do not provide happens-before ordering; however:
-     *
-     * <ul>
-     *   <li>If this field's value == currentThread, we know that it's up to date, because write
-     *       operations in a thread always happen-before subsequent read operations in the same
-     *       thread
-     *   <li>If this field's value == null because of unsafe publication, we know that it isn't the
-     *       object associated with our thread, because if it was the publication wouldn't have been
-     *       unsafe and we'd have seen our thread as the value. This state is also why a new
-     *       ThreadAndTask object must be created for each inline execution, because observing a
-     *       null thread does not mean the object is safe to reuse.
-     *   <li>If this field's value is some other thread object, we know that it's not our thread.
-     *   <li>If this field's value == null because it originally belonged to another thread and that
-     *       thread cleared it, we still know that it's not associated with our thread
-     *   <li>If this field's value == null because it was associated with our thread and was
-     *       cleared, we know that we're not executing inline any more
-     * </ul>
-     *
-     * All the states where thread != currentThread are identical for our purposes, and so even
-     * though it's racy, we don't care which of those values we get, so no need to synchronize.
-     */
-    Thread thread;
-    /** Only used by the thread associated with this object */
-    Runnable task;
-  }
-
-  /**
-   * This class helps avoid a StackOverflowError when large numbers of tasks are submitted with
-   * {@link MoreExecutors#directExecutor}. Normally, when the first future completes, all the other
-   * tasks would be called recursively. Here, we detect that the delegate executor is executing
-   * inline, and maintain a queue to dispatch tasks iteratively.
-   *
-   * <p>This class would certainly be simpler and easier to reason about if it were built with
-   * ThreadLocal; however, ThreadLocal is not well optimized for the case where the ThreadLocal is
-   * non-static, and is initialized/removed frequently - this causes churn in the Thread specific
-   * hashmaps. Using a static ThreadLocal to avoid that overhead would mean that different
-   * ExecutionSequencer objects interfere with each other, which would be undesirable, in addition
-   * to increasing the memory footprint of every thread that interacted with it. In order to release
-   * entries in thread-specific maps when the ThreadLocal object itself is no longer referenced,
-   * ThreadLocal is usually implemented with a WeakReference, which can have negative performance
-   * properties; for example, calling WeakReference.get() on Android will block during an
-   * otherwise-concurrent GC cycle.
-   */
-  private final class NonReentrantExecutor implements Executor {
-    final Executor delegate;
-
-    private NonReentrantExecutor(Executor delegate) {
-      this.delegate = delegate;
-    }
-
-    @Override
-    public void execute(final Runnable task) {
-      final Thread submitting = Thread.currentThread();
-      final ThreadAndTask submittingThreadAndTask = threadAndTask;
-      if (submittingThreadAndTask.thread == submitting) {
-        // Submit from inside a reentrant submit. We don't know if this one will be reentrant (and
-        // can't know without submitting something to the executor) so queue to run iteratively.
-        // Task must be null, since each execution on this executor can only produce one more
-        // execution.
-        checkState(submittingThreadAndTask.task == null);
-        submittingThreadAndTask.task =
-            new Runnable() {
-              @Override
-              public void run() {
-                delegate.execute(task);
-              }
-            };
-      } else {
-        delegate.execute(
-            new Runnable() {
-              @Override
-              public void run() {
-                Thread executingThread = Thread.currentThread();
-                if (executingThread != submitting) {
-                  task.run();
-                  return;
-                }
-                // Executor called reentrantly! Make sure that further calls don't overflow stack.
-                // Further reentrant calls will see that their current thread is the same as the
-                // one set in threadAndTask, and queue rather than calling execute() directly.
-                ThreadAndTask executingThreadAndTask = new ThreadAndTask();
-                executingThreadAndTask.thread = executingThread;
-                // Unconditionally set; there is no risk of throwing away a queued task from
-                // another thread, because in order for the current task to run on this executor
-                // the previous task must have already started execution. Because each task on a
-                // NonReentrantExecutor can only produce one execute() call to another instance
-                // from the same ExecutionSequencer, we know by induction that the task that
-                // launched this one must not have added any other runnables to that thread's
-                // queue, and thus we cannot be replacing a TaskAndThread object that would
-                // otherwise have another task queued on to it.
-                threadAndTask = executingThreadAndTask;
-                try {
-                  task.run();
-                  // Now check if our task attempted to reentrantly execute the next task.
-                  Runnable queuedTask;
-                  while ((queuedTask = executingThreadAndTask.task) != null) {
-                    executingThreadAndTask.task = null;
-                    queuedTask.run();
-                  }
-                } finally {
-                  // Null out the thread field, so that we don't leak a reference to Thread, and
-                  // so that future `thread == currentThread()` calls from this thread don't
-                  // incorrectly queue instead of executing. Don't null out the threadAndTask
-                  // field, because it might not be ours any more.
-                  executingThreadAndTask.thread = null;
-                }
-              }
-            });
-      }
-    }
   }
 }
