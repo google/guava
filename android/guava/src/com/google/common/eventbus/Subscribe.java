554546c9718a8c0bdae233a4afd5102180ef55ca
dpb <dpb@google.com>
2020-08-24 13:04:23 -0700

    Throw if @Subscribe is applied to a method that takes a primitive parameter.
    Fixes https://github.com/google/guava/issues/3992.
    
    RELNOTES=Prevent @Subscribe being applied to a method that takes a primitive, as that will never be called.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=328195519

--- android/guava/src/com/google/common/eventbus/Subscribe.java
+++ android/guava/src/com/google/common/eventbus/Subscribe.java
@@ -23,9 +23,10 @@ import java.lang.annotation.Target;
 /**
  * Marks a method as an event subscriber.
  *
- * <p>The type of event will be indicated by the method's first (and only) parameter. If this
- * annotation is applied to methods with zero parameters, or more than one parameter, the object
- * containing the method will not be able to register for event delivery from the {@link EventBus}.
+ * <p>The type of event will be indicated by the method's first (and only) parameter, which cannot
+ * be primitive. If this annotation is applied to methods with zero parameters, or more than one
+ * parameter, the object containing the method will not be able to register for event delivery from
+ * the {@link EventBus}.
  *
  * <p>Unless also annotated with @{@link AllowConcurrentEvents}, event subscriber methods will be
  * invoked serially by each event bus that they are registered with.
