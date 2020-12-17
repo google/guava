f0b4777787a7fa50dfcede7797fb34098217f87a
cpovirk <cpovirk@google.com>
2020-12-17 11:44:05 -0800

    Set @Target on @IgnoreJRERequirement.
    Values:
    https://www.mojohaus.org/animal-sniffer/animal-sniffer-annotations/apidocs/org/codehaus/mojo/animal_sniffer/IgnoreJRERequirement.html
    
    Context:
    This doesn't "fix" anything, but it prevents us from replicating an error I just made in the future:
    https://github.com/mojohaus/animal-sniffer/issues/131#issuecomment-747628259
    RELNOTES=n/a
    PiperOrigin-RevId: 348062753

--- android/guava/src/com/google/common/util/concurrent/IgnoreJRERequirement.java
+++ android/guava/src/com/google/common/util/concurrent/IgnoreJRERequirement.java
@@ -14,4 +14,11 @@
 
 package com.google.common.util.concurrent;
 
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Target;
+
+@Target({METHOD, CONSTRUCTOR, TYPE})
 @interface IgnoreJRERequirement {}
