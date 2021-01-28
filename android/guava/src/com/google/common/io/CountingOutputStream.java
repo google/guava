d394bac847467039530f514f880ecca27263d0ff
cpovirk <cpovirk@google.com>
2021-01-28 10:00:58 -0800

    Remove `@Beta` from `CountingOutputStream`.
    RELNOTES=`io`: Removed `@Beta` from `CountingOutputStream`.
    PiperOrigin-RevId: 354334755

--- android/guava/src/com/google/common/io/CountingOutputStream.java
+++ android/guava/src/com/google/common/io/CountingOutputStream.java
@@ -16,7 +16,6 @@ package com.google.common.io;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import com.google.common.annotations.Beta;
 import com.google.common.annotations.GwtIncompatible;
 import java.io.FilterOutputStream;
 import java.io.IOException;
@@ -28,7 +27,6 @@ import java.io.OutputStream;
  * @author Chris Nokleberg
  * @since 1.0
  */
-@Beta
 @GwtIncompatible
 public final class CountingOutputStream extends FilterOutputStream {
 
