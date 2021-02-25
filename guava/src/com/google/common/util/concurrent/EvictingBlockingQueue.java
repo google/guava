09265948964c5362f58e9d96c27013ad35e109a7
Noam <noamgrinch@gmail.com>
2021-02-25 16:49:20 +0200

    Changed minSize logic to greater than 1. Added tests

--- guava/src/com/google/common/util/concurrent/EvictingBlockingQueue.java
+++ guava/src/com/google/common/util/concurrent/EvictingBlockingQueue.java
@@ -17,49 +17,61 @@ package com.google.common.util.concurrent;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Nullable;
+
+import com.google.common.annotations.GwtCompatible;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Iterables;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
 
 /**
- * A thread-safe implementation of the EvictingQueue data structure. It holds an ArrayBlockingQueue as the delegate
- * which performers most of the BlockingQueue interface API. It overrides the methods that adds new objects to the
- * data structure in order to implement the Evicting feature.
+ * A thread-safe implementation of the EvictingQueue data structure. It holds an
+ * ArrayBlockingQueue as the delegate which performers most of the BlockingQueue
+ * interface API. It overrides the methods that adds new objects to the data
+ * structure in order to implement the Evicting feature.
  * 
- * <p>An evicting blocking queue must be configured with a maximum size. Each time an element is added to a
- * full queue, the queue automatically removes its head element. This is different from conventional
- * bounded queues, which either block or reject new elements when full.
+ * <p>
+ * An evicting blocking queue must be configured with a maximum size. Each time
+ * an element is added to a full queue, the queue automatically removes its head
+ * element. This is different from conventional bounded queues, which either
+ * block or reject new elements when full.
  * 
- * <p>This class does not accept null elements.
+ * <p>
+ * This class does not accept null elements.
  * 
  * @author Noam Greenshtain
-
+ * 
  */
+@GwtCompatible
+public final class EvictingBlockingQueue<E> extends ForwardingBlockingQueue<E> implements Serializable {
 
-public final class EvictingBlockingQueue<E> extends ForwardingBlockingQueue<E> implements Serializable{
-	
 	private static final long serialVersionUID = -4305601088881265868L;
 
 	private final BlockingQueue<E> delegate;
-	
-	private final Object lock;
-	
-	@VisibleForTesting final int maxSize;
-	
+
+	private final Integer lock;
+
+	@VisibleForTesting
+	final int maxSize;
+
 	private EvictingBlockingQueue(int maxSize) {
-		  checkArgument(maxSize >= 0, "maxSize (%s) must >= 0", maxSize);
-		  this.delegate = new ArrayBlockingQueue<E>(maxSize);
-		  this.maxSize = maxSize;
-		  this.lock = new Object();
+		checkArgument(maxSize >= 1, "maxSize (%s) must >= 1", maxSize);
+		this.delegate = new ArrayBlockingQueue<E>(maxSize);
+		this.maxSize = maxSize;
+		this.lock = Integer.valueOf(0);
 	}
-	
+
 	/**
-	 * Creates and returns a new EvictingBlockingQueue that will hold up to {@code maxSize} elements.
+	 * Creates and returns a new EvictingBlockingQueue that will hold up to
+	 * {@code maxSize} elements.
 	 * 
 	 */
-	public static <E> EvictingBlockingQueue<E> create(int maxSize){
+	public static <E> EvictingBlockingQueue<E> create(int maxSize) {
 		return new EvictingBlockingQueue<E>(maxSize);
 	}
 
@@ -67,9 +79,10 @@ public final class EvictingBlockingQueue<E> extends ForwardingBlockingQueue<E> i
 	protected BlockingQueue<E> delegate() {
 		return this.delegate;
 	}
+
 	/**
-	 * Adds the given element to this queue. If the queue is currently full, the element at the head
-	 * of the queue is evicted to make room.
+	 * Adds the given element to this queue. If the queue is currently full, the
+	 * element at the head of the queue is evicted to make room.
 	 *
 	 * @return {@code true} always
 	 */
@@ -77,9 +90,10 @@ public final class EvictingBlockingQueue<E> extends ForwardingBlockingQueue<E> i
 	public void put(E e) {
 		add(e);
 	}
+
 	/**
-	 * Adds the given element to this queue. If the queue is currently full, the element at the head
-	 * of the queue is evicted to make room.
+	 * Adds the given element to this queue. If the queue is currently full, the
+	 * element at the head of the queue is evicted to make room.
 	 *
 	 * @return {@code true} always
 	 */
@@ -88,58 +102,78 @@ public final class EvictingBlockingQueue<E> extends ForwardingBlockingQueue<E> i
 	public boolean offer(E e) {
 		return add(e);
 	}
+
 	/**
-	 * Adds the given element to this queue. If the queue is currently full, the element at the head
-	 * of the queue is evicted to make room.
+	 * Adds the given element to this queue. If the queue is currently full, the
+	 * element at the head of the queue is evicted to make room.
 	 *
 	 * @return {@code true} always
 	 */
 	@Override
 	@CanIgnoreReturnValue
-	public boolean offer(E e, long timeout, TimeUnit unit) {
+	public boolean offer(E e, long timeout, @Nullable TimeUnit unit) {
 		return add(e); // Timeout is useless in an EvictingQueue feature.
 	}
 
 	/**
-	 * Adds the given element to this queue. If the queue is currently full, the element at the head
-	 * of the queue is evicted to make room. This operation is synchronized due to BlockingQueue semantics.
+	 * Adds the given element to this queue. If the queue is currently full, the
+	 * element at the head of the queue is evicted to make room. This operation is
+	 * synchronized due to BlockingQueue semantics.
 	 *
 	 * @return {@code true} always
 	 */
 	@Override
 	@CanIgnoreReturnValue
 	public boolean add(E e) {
-	    checkNotNull(e); // check before removing
-	    if (maxSize == 0) {
-	      return true;
-	    }
-	    synchronized(lock) {
-	    	if (size() == maxSize) {
-	    		delegate.remove();
-	    	}
-	    	delegate.add(e);
-	    	return true;
-	    }
+		checkNotNull(e); // check before removing
+		synchronized (lock) {
+			if (size() == maxSize) {
+				delegate.remove();
+			}
+			delegate.add(e);
+			return true;
+		}
 	}
-	
+
+	@Override
+	@CanIgnoreReturnValue
+	public boolean addAll(Collection<? extends E> collection) {
+		int size = collection.size();
+		synchronized (lock) {
+			if (size >= maxSize) {
+				clear();
+				return Iterables.addAll(this, Iterables.skip(collection, size - maxSize));
+			}
+			return standardAddAll(collection);
+		}
+	}
+
 	@Override
 	public boolean remove(Object o) {
-		synchronized(lock) {
+		checkNotNull(o);
+		synchronized (lock) {
 			return this.delegate.remove(o);
 		}
 	}
-	
+
 	@Override
 	public E remove() {
-		synchronized(lock) {
+		synchronized (lock) {
 			return this.delegate.remove();
 		}
 	}
-	
+
 	@Override
 	public E poll() {
-		synchronized(lock) {
+		synchronized (lock) {
 			return this.delegate.poll();
+		}
+	}
+
+	@Override
+	public boolean contains(Object object) {
+		synchronized (lock) {
+			return delegate().contains(checkNotNull(object));
 		}
 	}
 
