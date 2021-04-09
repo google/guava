1cedca062524b84684411910cb48c0491932a525
Éamonn McManus <emcmanus@google.com>
2021-04-08 17:23:34 -0700

    Simplify the logic of `CaseFormat.convert`.
    In this version, it is clearer to both humans and static analysis that the StringBuilder is not null when it is appended to. Additionally, the previous version could allocate a StringBuilder twice, while this version will not.
    
    The new logic produces different results for some edge cases. For example, converting `_FOO_BAR` from upper-underscore to lower-camel used to produce `FooBar` while now it produces `_fooBar`. The spec says _A "best effort" approach is taken; if {@code str} does not conform to the assumed format, then the behavior of this method is undefined but we make a reasonable effort at converting anyway._ None of the thousands of usages in Google's code base has any tests that are affected by this change.
    
    RELNOTES=n/a
    PiperOrigin-RevId: 367539236

--- guava/src/com/google/common/base/CaseFormat.java
+++ guava/src/com/google/common/base/CaseFormat.java
@@ -127,26 +127,27 @@ public enum CaseFormat {
     return (format == this) ? str : convert(format, str);
   }
 
-  /** Enum values can override for performance reasons. */
+  // Enum values can override.
+  // This version is used when at least one of "from" and "to" is camel case.
   String convert(CaseFormat format, String s) {
-    // deal with camel conversion
-    StringBuilder out = null;
-    int i = 0;
-    int j = -1;
-    while ((j = wordBoundary.indexIn(s, ++j)) != -1) {
-      if (i == 0) {
-        // include some extra space for separators
-        out = new StringBuilder(s.length() + 4 * format.wordSeparator.length());
-        out.append(format.normalizeFirstWord(s.substring(i, j)));
-      } else {
-        out.append(format.normalizeWord(s.substring(i, j)));
-      }
-      out.append(format.wordSeparator);
-      i = j + wordSeparator.length();
+    if (s.isEmpty()) {
+      return "";
     }
-    return (i == 0)
-        ? format.normalizeFirstWord(s)
-        : out.append(format.normalizeWord(s.substring(i))).toString();
+    int boundary = wordBoundary.indexIn(s, 1);
+    if (boundary < 0) {
+      return format.normalizeFirstWord(s);
+    }
+    // include some extra space for separators
+    StringBuilder out =
+        new StringBuilder(s.length() + 4 * format.wordSeparator.length())
+            .append(format.normalizeFirstWord(s.substring(0, boundary)));
+    do {
+      int start = boundary + wordSeparator.length();
+      boundary = wordBoundary.indexIn(s, start + 1);
+      int end = (boundary < 0) ? s.length() : boundary;
+      out.append(format.wordSeparator).append(format.normalizeWord(s.substring(start, end)));
+    } while (boundary >= 0);
+    return out.toString();
   }
 
   /**
