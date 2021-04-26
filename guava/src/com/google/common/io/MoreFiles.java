db6580e80e49b673b790164c08a5fc64380fc4e5
cpovirk <cpovirk@google.com>
2021-04-26 13:02:44 -0700

    Avoid unlikely NullPointerException in deleteDirectoryContents and deleteRecursively.
    RELNOTES=n/a
    PiperOrigin-RevId: 370523726

--- guava/src/com/google/common/io/MoreFiles.java
+++ guava/src/com/google/common/io/MoreFiles.java
@@ -803,16 +803,47 @@ public final class MoreFiles {
 
   private static @Nullable NoSuchFileException pathNotFound(
       Path path, Collection<IOException> exceptions) {
-    if (exceptions.size() == 1) {
-      IOException exception = getOnlyElement(exceptions);
-      if (exception instanceof NoSuchFileException) {
-        NoSuchFileException noSuchFileException = (NoSuchFileException) exception;
-        if (noSuchFileException
-            .getFile()
-            .equals(getParentPath(path).resolve(path.getFileName()).toString())) {
-          return noSuchFileException;
-        }
-      }
+    if (exceptions.size() != 1) {
+      return null;
+    }
+    IOException exception = getOnlyElement(exceptions);
+    if (!(exception instanceof NoSuchFileException)) {
+      return null;
+    }
+    NoSuchFileException noSuchFileException = (NoSuchFileException) exception;
+    String exceptionFile = noSuchFileException.getFile();
+    if (exceptionFile == null) {
+      /*
+       * It's not clear whether this happens in practice, especially with the filesystem
+       * implementations that are built into java.nio.
+       */
+      return null;
+    }
+    Path parentPath = getParentPath(path);
+    if (parentPath == null) {
+      /*
+       * This is probably impossible:
+       *
+       * - In deleteRecursively, we require the path argument to have a parent.
+       *
+       * - In deleteDirectoryContents, the path argument may have no parent. Fortunately, all the
+       *   *other* paths we process will be descendants of that. That leaves only the original path
+       *   argument for us to consider. And the only place we call pathNotFound is from
+       *   throwDeleteFailed, and the other place that we call throwDeleteFailed inside
+       *   deleteDirectoryContents is when an exception is thrown during the recusive steps. Any
+       *   failure during the initial lookup of the path argument itself is rethrown directly. So
+       *   any exception that we're seeing here is from a descendant, which naturally has a parent.
+       *   I think.
+       *
+       * Still, if this can happen somehow (a weird filesystem implementation that lets callers
+       * change its working directly concurrently with a call to deleteDirectoryContents?), it makes
+       * more sense for us to fall back to a generic FileSystemException (by returning null here)
+       * than to dereference parentPath and end up producing NullPointerException.
+       */
+      return null;
+    }
+    if (exceptionFile.equals(parentPath.resolve(path.getFileName()).toString())) {
+      return noSuchFileException;
     }
     return null;
   }
