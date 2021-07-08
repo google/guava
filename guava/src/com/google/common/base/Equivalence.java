d4cfe4f0bd19030876529c501b1c96178c40428c
JuHyun419 <zzang9ha@naver.com>
2021-07-08 09:06:26 +0900

    Remove Generic's type parameter

--- guava/src/com/google/common/base/Equivalence.java
+++ guava/src/com/google/common/base/Equivalence.java
@@ -165,7 +165,7 @@ public abstract class Equivalence<T> implements BiPredicate<@Nullable T, @Nullab
    * @since 10.0
    */
   public final <S extends @Nullable T> Wrapper<S> wrap(@ParametricNullness S reference) {
-    return new Wrapper<S>(this, reference);
+    return new Wrapper<>(this, reference);
   }
 
   /**
@@ -272,7 +272,7 @@ public abstract class Equivalence<T> implements BiPredicate<@Nullable T, @Nullab
    * @since 10.0
    */
   public final Predicate<@Nullable T> equivalentTo(@CheckForNull T target) {
-    return new EquivalentToPredicate<T>(this, target);
+    return new EquivalentToPredicate<>(this, target);
   }
 
   private static final class EquivalentToPredicate<T>
