354d78c6331cce5faae68275f81638f1894e0da8
kevinb <kevinb@google.com>
2021-10-18 12:00:33 -0700

    Range.intersect() with a disconnected range deserves a less confusing exception message
    RELNOTES=very minor improvement to Range.intersect() exception message
    PiperOrigin-RevId: 404021898

--- guava/src/com/google/common/collect/Range.java
+++ guava/src/com/google/common/collect/Range.java
@@ -16,6 +16,7 @@
 
 package com.google.common.collect;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.google.common.annotations.GwtCompatible;
@@ -556,6 +557,15 @@ public final class Range<C extends Comparable> extends RangeGwtSerializationDepe
     } else {
       Cut<C> newLower = (lowerCmp >= 0) ? lowerBound : connectedRange.lowerBound;
       Cut<C> newUpper = (upperCmp <= 0) ? upperBound : connectedRange.upperBound;
+
+      // create() would catch this, but give a confusing error message
+      checkArgument(
+          newLower.compareTo(newUpper) <= 0,
+          "intersection is undefined for disconnected ranges %s and %s",
+          this,
+          connectedRange);
+
+      // TODO(kevinb): all the precondition checks in the constructor are redundant...
       return create(newLower, newUpper);
     }
   }
