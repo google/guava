91591620e02b19ac50e75a227399f355ba555a48
cpovirk <cpovirk@google.com>
2021-11-17 07:42:18 -0800

    Elaborate on comment in AggregateFuture.addInitialException.
    RELNOTES=n/a
    PiperOrigin-RevId: 410524377

--- android/guava/src/com/google/common/util/concurrent/AggregateFuture.java
+++ android/guava/src/com/google/common/util/concurrent/AggregateFuture.java
@@ -238,8 +238,18 @@ abstract class AggregateFuture<InputT extends @Nullable Object, OutputT extends 
     checkNotNull(seen);
     if (!isCancelled()) {
       /*
-       * requireNonNull is safe because this is a TrustedFuture, and we're calling this method only
-       * if it has failed.
+       * requireNonNull is safe because:
+       *
+       * - This is a TrustedFuture, so tryInternalFastPathGetFailure will in fact return the failure
+       *   cause if this Future has failed.
+       *
+       * - And this future *has* failed: This method is called only from handleException (through
+       *   getOrInitSeenExceptions). handleException tried to call setException and failed, so
+       *   either this Future was cancelled (which we ruled out with the isCancelled check above),
+       *   or it had already failed. (It couldn't have completed *successfully* or even had
+       *   setFuture called on it: Neither of those can happen until we've finished processing all
+       *   the completed inputs. And we're still processing at least one input, the one that
+       *   triggered handleException.)
        *
        * TODO(cpovirk): Think about whether we could/should use Verify to check the return value of
        * addCausalChain.
