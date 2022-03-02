0ef6688750d6432e42830dbe5b522cf8c8182790
cpovirk <cpovirk@google.com>
2022-03-02 12:35:15 -0800

    Apply `@CheckReturnValue` to `Uninterruptibles#awaitUninterruptibly(CountDownLatch, long, TimeUnit)`.
    Work toward https://github.com/google/guava/issues/3655
    Somewhat relevant to https://github.com/google/error-prone/issues/1490
    
    PiperOrigin-RevId: 432003331

--- android/guava/src/com/google/common/util/concurrent/Uninterruptibles.java
+++ android/guava/src/com/google/common/util/concurrent/Uninterruptibles.java
@@ -73,7 +73,6 @@ public final class Uninterruptibles {
    * Invokes {@code latch.}{@link CountDownLatch#await(long, TimeUnit) await(timeout, unit)}
    * uninterruptibly.
    */
-  @CanIgnoreReturnValue // TODO(cpovirk): Consider being more strict.
   @GwtIncompatible // concurrency
   @SuppressWarnings("GoodTime") // should accept a java.time.Duration
   public static boolean awaitUninterruptibly(CountDownLatch latch, long timeout, TimeUnit unit) {
