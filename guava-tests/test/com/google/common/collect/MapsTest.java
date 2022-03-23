b318b5d3491a8f48b06d20f2d9ee316b06ba051f
Google Java Core Libraries <java-team-github-bot@google.com>
2022-03-23 05:29:59 -0700

    Temporary rollback of https://github.com/google/guava/commit/6ad621e76d6dec9f00804c53cbeaa3c9c5854d59 while we investigate a mystery test failure.
    RELNOTES=n/a
    PiperOrigin-RevId: 436712810

--- guava-tests/test/com/google/common/collect/MapsTest.java
+++ guava-tests/test/com/google/common/collect/MapsTest.java
@@ -125,10 +125,7 @@ public class MapsTest extends TestCase {
 
     for (int size = 1; size < 200; size++) {
       assertWontGrow(
-          size,
-          new HashMap<>(),
-          Maps.newHashMapWithExpectedSize(size),
-          Maps.newHashMapWithExpectedSize(size));
+          size, Maps.newHashMapWithExpectedSize(size), Maps.newHashMapWithExpectedSize(size));
     }
   }
 
@@ -141,7 +138,6 @@ public class MapsTest extends TestCase {
     for (int size = 1; size < 200; size++) {
       assertWontGrow(
           size,
-          new LinkedHashMap<>(),
           Maps.newLinkedHashMapWithExpectedSize(size),
           Maps.newLinkedHashMapWithExpectedSize(size));
     }
@@ -149,11 +145,7 @@ public class MapsTest extends TestCase {
 
   @GwtIncompatible // reflection
   private static void assertWontGrow(
-      int size,
-      HashMap<Object, Object> referenceMap,
-      HashMap<Object, Object> map1,
-      HashMap<Object, Object> map2)
-      throws Exception {
+      int size, HashMap<Object, Object> map1, HashMap<Object, Object> map2) throws Exception {
     // Only start measuring table size after the first element inserted, to
     // deal with empty-map optimization.
     map1.put(0, null);
@@ -175,16 +167,6 @@ public class MapsTest extends TestCase {
     assertWithMessage("table size after adding " + size + " elements")
         .that(bucketsOf(map1))
         .isEqualTo(initialBuckets);
-
-    // Ensure that referenceMap, which doesn't use WithExpectedSize, ends up with the same table
-    // size as the other two maps. If it ended up with a smaller size that would imply that we
-    // computed the wrong initial capacity.
-    for (int i = 0; i < size; i++) {
-      referenceMap.put(i, null);
-    }
-    assertWithMessage("table size after adding " + size + " elements")
-        .that(initialBuckets)
-        .isAtMost(bucketsOf(referenceMap));
   }
 
   @GwtIncompatible // reflection
