2be0834eafed9dd7ce94d60c581a0d3ee3e582f0
Kqliber <kqliber@gmail.com>
2022-04-21 20:20:40 +0100

    remove @Beta annotations from tryParse methods

--- guava/src/com/google/common/primitives/Ints.java
+++ guava/src/com/google/common/primitives/Ints.java
@@ -713,7 +713,6 @@ public final class Ints extends IntsMethodsForWeb {
    * @throws NullPointerException if {@code string} is {@code null}
    * @since 11.0
    */
-  @Beta
   @CheckForNull
   public static Integer tryParse(String string) {
     return tryParse(string, 10);
@@ -739,7 +738,6 @@ public final class Ints extends IntsMethodsForWeb {
    * @throws NullPointerException if {@code string} is {@code null}
    * @since 19.0
    */
-  @Beta
   @CheckForNull
   public static Integer tryParse(String string, int radix) {
     Long result = Longs.tryParse(string, radix);
