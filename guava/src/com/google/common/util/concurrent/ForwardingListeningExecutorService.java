18a696ad8948d966b5b0d0302551c39a49a8f543
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-01-18 07:16:48 -0800

    Document ForwardingExecutorService behavior for default methods.
    Currently, Guava is built with Java 9 and `ExecutorService` has no
    default methods. This changes in Java 19, where `ExecutorService.close`
    is added. Ensure users have right expectations about future evolution of
    the `ForwardingExecutorService` and its compatibility with newer Java
    versions.
    
    Fixes #6296
    Fixes #6299
    
    RELNOTES=n/a
    PiperOrigin-RevId: 502868842

--- guava/src/com/google/common/util/concurrent/ForwardingListeningExecutorService.java
+++ guava/src/com/google/common/util/concurrent/ForwardingListeningExecutorService.java
@@ -24,6 +24,10 @@ import org.checkerframework.checker.nullness.qual.Nullable;
  * executor service as desired per the <a
  * href="http://en.wikipedia.org/wiki/Decorator_pattern">decorator pattern</a>.
  *
+ * <p><b>{@code default} method warning:</b> This class does <i>not</i> forward calls to {@code
+ * default} methods. Instead, it inherits their default implementations. When those implementations
+ * invoke methods, they invoke methods on the {@code ForwardingListeningExecutorService}.
+ *
  * @author Isaac Shum
  * @since 10.0
  */
