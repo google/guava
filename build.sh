5ed0307b6b143eb54f118e0bfec5a5acbac5cba3
edonadei-google <113446536+edonadei-google@users.noreply.github.com>
2023-05-03 19:26:32 -0400

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Fail on any error.
+set -e
+
+# Display commands being run.
+# WARNING: please only enable 'set -x' if necessary for debugging, and be very
+#  careful if you handle credentials (e.g. from Keystore) with 'set -x':
+#  statements like "export VAR=$(cat /tmp/keystore/credentials)" will result in
+#  the credentials being printed in build logs.
+#  Additionally, recursive invocation with credentials as command-line
+#  parameters, will print the full command, with credentials, in the build logs.
+# set -x
+
+if [ "$1" == "release" ]; then
+  javac -g:none Hello.java
+else
+  javac Hello.java
+fi
+java Hello
