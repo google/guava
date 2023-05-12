fcec25f45ede9cd6c6f66cd066f8fa86a37b8b7e
Kurt Alfred Kluever <kak@google.com>
2023-05-12 09:31:09 -0700

    Remove `@Beta` from various `primitive` collection APIs.
    RELNOTES=`primitive`: Remove `@Beta` from various `primitive` collection APIs.
    PiperOrigin-RevId: 531527817

--- android/guava/src/com/google/common/primitives/ImmutableLongArray.java
+++ android/guava/src/com/google/common/primitives/ImmutableLongArray.java
@@ -16,7 +16,6 @@ package com.google.common.primitives;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
-import com.google.common.annotations.Beta;
 import com.google.common.annotations.GwtCompatible;
 import com.google.common.base.Preconditions;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
@@ -76,7 +75,6 @@ import javax.annotation.CheckForNull;
  *
  * @since 22.0
  */
-@Beta
 @GwtCompatible
 @Immutable
 @ElementTypesAreNonnullByDefault
