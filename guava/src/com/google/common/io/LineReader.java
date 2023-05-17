a5892560de926b0ab01ba2614f5002ff547a32a2
Kurt Alfred Kluever <kak@google.com>
2023-05-17 07:32:30 -0700

    Remove `@Beta` from various `io` APIs.
    RELNOTES=`io`: Remove `@Beta` from various `io` APIs.
    PiperOrigin-RevId: 532782558

--- guava/src/com/google/common/io/LineReader.java
+++ guava/src/com/google/common/io/LineReader.java
@@ -17,7 +17,6 @@ package com.google.common.io;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.io.CharStreams.createBuffer;
 
-import com.google.common.annotations.Beta;
 import com.google.common.annotations.GwtIncompatible;
 import com.google.common.annotations.J2ktIncompatible;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
@@ -36,7 +35,6 @@ import javax.annotation.CheckForNull;
  * @author Chris Nokleberg
  * @since 1.0
  */
-@Beta
 @J2ktIncompatible
 @GwtIncompatible
 @ElementTypesAreNonnullByDefault
