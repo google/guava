a3571b408f05950cf6988c7d4da59467e5f09e18
Kurt Alfred Kluever <kak@google.com>
2023-05-16 18:40:20 -0700

    Remove `@Beta` from various `MoreExecutors` APIs.
    RELNOTES=`concurrent`: Remove `@Beta` from various `MoreExecutors` APIs.
    PiperOrigin-RevId: 532635013

--- guava/src/com/google/common/util/concurrent/MoreExecutors.java
+++ guava/src/com/google/common/util/concurrent/MoreExecutors.java
@@ -19,7 +19,6 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.util.concurrent.Internal.toNanosSaturated;
 import static java.util.Objects.requireNonNull;
 
-import com.google.common.annotations.Beta;
 import com.google.common.annotations.GwtCompatible;
 import com.google.common.annotations.GwtIncompatible;
 import com.google.common.annotations.J2ktIncompatible;
@@ -82,7 +81,6 @@ public final class MoreExecutors {
    * @return an unmodifiable version of the input which will not hang the JVM
    * @since 28.0
    */
-  @Beta
   @J2ktIncompatible
   @GwtIncompatible // TODO
   public static ExecutorService getExitingExecutorService(
@@ -104,7 +102,6 @@ public final class MoreExecutors {
    * @param timeUnit unit of time for the time parameter
    * @return an unmodifiable version of the input which will not hang the JVM
    */
-  @Beta
   @J2ktIncompatible
   @GwtIncompatible // TODO
   @SuppressWarnings("GoodTime") // should accept a java.time.Duration
@@ -126,7 +123,6 @@ public final class MoreExecutors {
    * @param executor the executor to modify to make sure it exits when the application is finished
    * @return an unmodifiable version of the input which will not hang the JVM
    */
-  @Beta
   @J2ktIncompatible
   @GwtIncompatible // concurrency
   public static ExecutorService getExitingExecutorService(ThreadPoolExecutor executor) {
@@ -146,7 +142,6 @@ public final class MoreExecutors {
    * @return an unmodifiable version of the input which will not hang the JVM
    * @since 28.0
    */
-  @Beta
   @J2ktIncompatible
   @GwtIncompatible // java.time.Duration
   public static ScheduledExecutorService getExitingScheduledExecutorService(
@@ -168,7 +163,6 @@ public final class MoreExecutors {
    * @param timeUnit unit of time for the time parameter
    * @return an unmodifiable version of the input which will not hang the JVM
    */
-  @Beta
   @J2ktIncompatible
   @GwtIncompatible // TODO
   @SuppressWarnings("GoodTime") // should accept a java.time.Duration
@@ -191,7 +185,6 @@ public final class MoreExecutors {
    * @param executor the executor to modify to make sure it exits when the application is finished
    * @return an unmodifiable version of the input which will not hang the JVM
    */
-  @Beta
   @J2ktIncompatible
   @GwtIncompatible // TODO
   public static ScheduledExecutorService getExitingScheduledExecutorService(
@@ -210,7 +203,6 @@ public final class MoreExecutors {
    *     JVM
    * @since 28.0
    */
-  @Beta
   @J2ktIncompatible
   @GwtIncompatible // java.time.Duration
   public static void addDelayedShutdownHook(ExecutorService service, Duration terminationTimeout) {
@@ -228,7 +220,6 @@ public final class MoreExecutors {
    *     JVM
    * @param timeUnit unit of time for the time parameter
    */
-  @Beta
   @J2ktIncompatible
   @GwtIncompatible // TODO
   @SuppressWarnings("GoodTime") // should accept a java.time.Duration
@@ -904,7 +895,6 @@ public final class MoreExecutors {
    *
    * @since 14.0
    */
-  @Beta
   @J2ktIncompatible
   @GwtIncompatible // concurrency
   public static ThreadFactory platformThreadFactory() {
@@ -1093,7 +1083,6 @@ public final class MoreExecutors {
    *     if the call timed out or was interrupted
    * @since 28.0
    */
-  @Beta
   @CanIgnoreReturnValue
   @J2ktIncompatible
   @GwtIncompatible // java.time.Duration
@@ -1125,7 +1114,6 @@ public final class MoreExecutors {
    *     if the call timed out or was interrupted
    * @since 17.0
    */
-  @Beta
   @CanIgnoreReturnValue
   @J2ktIncompatible
   @GwtIncompatible // concurrency
