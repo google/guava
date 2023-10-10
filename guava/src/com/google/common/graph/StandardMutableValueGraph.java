16ce9795af94eea47f0e8ace0830db953c67c89f
ineuwirth <istvan.neuwirth@proton.me>
2023-10-10 10:01:48 -0700

    ensure that Graph/ValueGraph views for a node report empty when that node is removed from the graph
    RELNOTES=ensure that Graph/ValueGraph views for a node report empty when that node is removed from the graph
    PiperOrigin-RevId: 572284799

--- guava/src/com/google/common/graph/StandardMutableValueGraph.java
+++ guava/src/com/google/common/graph/StandardMutableValueGraph.java
@@ -24,6 +24,7 @@ import static com.google.common.graph.Graphs.checkNonNegative;
 import static com.google.common.graph.Graphs.checkPositive;
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.collect.ImmutableList;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import javax.annotation.CheckForNull;
 
@@ -136,17 +137,21 @@ final class StandardMutableValueGraph<N, V> extends StandardValueGraph<N, V>
       }
     }
 
-    for (N successor : connections.successors()) {
+    for (N successor : ImmutableList.copyOf(connections.successors())) {
       // requireNonNull is safe because the node is a successor.
       requireNonNull(nodeConnections.getWithoutCaching(successor)).removePredecessor(node);
+      requireNonNull(connections.removeSuccessor(successor));
       --edgeCount;
     }
     if (isDirected()) { // In undirected graphs, the successor and predecessor sets are equal.
-      for (N predecessor : connections.predecessors()) {
+      // Since views are returned, we need to copy the predecessors that will be removed.
+      // Thus we avoid modifying the underlying view while iterating over it.
+      for (N predecessor : ImmutableList.copyOf(connections.predecessors())) {
         // requireNonNull is safe because the node is a predecessor.
         checkState(
             requireNonNull(nodeConnections.getWithoutCaching(predecessor)).removeSuccessor(node)
                 != null);
+        connections.removePredecessor(predecessor);
         --edgeCount;
       }
     }
