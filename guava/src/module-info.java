b6d8ea5e3ad71f3d7329a29717d63e6cd9eb5ac0
Sam Gammon <sam@elide.ventures>
2024-03-08 12:43:34 -0800

    feat(jpms): add `module-info` definition for guava
    This changeset adds full support for modular Java builds in Guava,
    and in libraries which depend on Guava.
    
    The Guava JAR for JRE now structures as a Multi-Release JAR, with
    a module definition situated in `META-INF/versions/9/`. Guava
    remains compatible with JDK 8.
    
    - feat: add `module-info.java` to `guava` module
    - chore: update `guava` to build MRJAR
    - chore: adjust dev version → `1.0-HEAD-[jre|android]-SNAPSHOT`
    - chore: upgrade maven compiler plugin → `3.12.1`
    
    Fixes and closes google/guava#2970
    
    Relates-To: elide-dev/jpms#1
    Signed-off-by: Sam Gammon <sam@elide.ventures>

---
+++ guava/src/module-info.java
@@ -0,0 +1,44 @@
+// GENERATED FILE - DO NOT EDIT
+
+/*
+ * Copyright (C) 2008 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Google Guava
+ */
+module com.google.common {
+  requires java.base;
+  requires java.logging;
+  requires jdk.unsupported;
+
+  exports com.google.common.annotations;
+  exports com.google.common.base;
+  exports com.google.common.cache;
+  exports com.google.common.collect;
+  exports com.google.common.escape;
+  exports com.google.common.eventbus;
+  exports com.google.common.graph;
+  exports com.google.common.hash;
+  exports com.google.common.html;
+  exports com.google.common.io;
+  exports com.google.common.math;
+  exports com.google.common.net;
+  exports com.google.common.primitives;
+  exports com.google.common.reflect;
+  exports com.google.common.util.concurrent;
+  exports com.google.common.xml;
+  exports com.google.thirdparty.publicsuffix;
+}
