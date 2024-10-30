664c80b4eb4b1ecc96d7cedbbe30cdf63caf4e69
cpovirk <cpovirk@google.com>
2024-10-30 14:14:50 -0700

    Fix another accidental use of varargs `min`/`max` calls.
    As in cl/689775552, I'm fixing a mistaken change from cl/687381111.
    
    RELNOTES=n/a
    PiperOrigin-RevId: 691549601

--- guava/src/com/google/common/primitives/UnsignedInts.java
+++ guava/src/com/google/common/primitives/UnsignedInts.java
@@ -17,7 +17,6 @@ package com.google.common.primitives;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkPositionIndexes;
-import static java.lang.Math.min;
 
 import com.google.common.annotations.GwtCompatible;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
@@ -196,8 +195,10 @@ public final class UnsignedInts {
     INSTANCE;
 
     @Override
+    // A call to bare "min" or "max" would resolve to our varargs method, not to any static import.
+    @SuppressWarnings("StaticImportPreferred")
     public int compare(int[] left, int[] right) {
-      int minLength = min(left.length, right.length);
+      int minLength = Math.min(left.length, right.length);
       for (int i = 0; i < minLength; i++) {
         if (left[i] != right[i]) {
           return UnsignedInts.compare(left[i], right[i]);
