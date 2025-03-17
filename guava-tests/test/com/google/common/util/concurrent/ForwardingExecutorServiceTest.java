6b1a22f80cd9496f20913c5efbc656c9c4c5f912
cpovirk <cpovirk@google.com>
2025-03-17 11:40:25 -0700

    Test that `ForwardingExecutorServiceTest` does not forward the `default` method `close()`.
    This has always been the case ([as it should be](https://github.com/google/guava/issues/6296#issuecomment-1385916649), given the goal of preserving historical behavior), and it's been documented since cl/502868842. But I figured it would be nice to have a test, especially given the R8 upgrade that we're preparing for in cl/736111584.
    
    RELNOTES=n/a
    PiperOrigin-RevId: 737693700

--- guava-tests/test/com/google/common/util/concurrent/ForwardingExecutorServiceTest.java
+++ guava-tests/test/com/google/common/util/concurrent/ForwardingExecutorServiceTest.java
@@ -16,6 +16,15 @@
 
 package com.google.common.util.concurrent;
 
+import static com.google.common.base.StandardSystemProperty.JAVA_SPECIFICATION_VERSION;
+import static com.google.common.truth.Truth.assertThat;
+import static java.lang.Integer.parseInt;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import junit.framework.TestCase;
 import org.jspecify.annotations.NullUnmarked;
 
@@ -24,5 +33,43 @@ import org.jspecify.annotations.NullUnmarked;
 public class ForwardingExecutorServiceTest extends TestCase {
   public void testForwarding() {
     ForwardingObjectTester.testForwardingObject(ForwardingExecutorService.class);
+  }
+
+  public void testNoForwardingOfDefaultMethod() throws Exception {
+    ExecutorService delegate =
+        new ThreadPoolExecutor(0, Integer.MAX_VALUE, 60, SECONDS, new SynchronousQueue<>()) {
+          @Override
+          public void close() {
+            throw new AssertionError(
+                "ForwardingExecutorService should have used the default method"
+                    + " ExecutorService.close() (which would forward to methods like shutdown() on"
+                    + " the delegate) instead of forwarding to delegate.close()");
+          }
+        };
+    ExecutorService wrapper =
+        new ForwardingExecutorService() {
+          @Override
+          protected ExecutorService delegate() {
+            return delegate;
+          }
+        };
+    Method closeMethod;
+    try {
+      closeMethod = wrapper.getClass().getMethod("close");
+    } catch (NoSuchMethodException e) {
+      assertThat(isAndroid() || isBeforeJava19()).isTrue();
+      return; // close() doesn't exist, so we can't test it.
+    }
+    closeMethod.invoke(wrapper);
+    assertThat(delegate.isTerminated()).isTrue();
+  }
+
+  private static boolean isAndroid() {
+    return System.getProperty("java.runtime.name", "").contains("Android");
+  }
+
+  private static boolean isBeforeJava19() {
+    return JAVA_SPECIFICATION_VERSION.value().equals("1.8")
+        || parseInt(JAVA_SPECIFICATION_VERSION.value()) < 19;
   }
 }
