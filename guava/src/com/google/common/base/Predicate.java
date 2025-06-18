052437833d1de2ad6248ae4ccbea6e3dc9d351bf
Kurt Alfred Kluever <kak@google.com>
2025-06-18 08:19:00 -0700

    Add a warning to `Predicate.equals(Object)` (similar to cl/134125361).
    RELNOTES=n/a
    PiperOrigin-RevId: 772927807

--- guava/src/com/google/common/base/Predicate.java
+++ guava/src/com/google/common/base/Predicate.java
@@ -62,12 +62,12 @@ public interface Predicate<T extends @Nullable Object> extends java.util.functio
   /**
    * Indicates whether another object is equal to this predicate.
    *
-   * <p>Most implementations will have no reason to override the behavior of {@link Object#equals}.
-   * However, an implementation may also choose to return {@code true} whenever {@code obj} is a
-   * {@link Predicate} that it considers <i>interchangeable</i> with this one. "Interchangeable"
-   * <i>typically</i> means that {@code this.apply(t) == that.apply(t)} for all {@code t} of type
-   * {@code T}). Note that a {@code false} result from this method does not imply that the
-   * predicates are known <i>not</i> to be interchangeable.
+   * <p><b>Warning: do not depend</b> on the behavior of this method.
+   *
+   * <p>Historically, {@code Predicate} instances in this library have implemented this method to
+   * recognize certain cases where distinct {@code Predicate} instances would in fact behave
+   * identically. However, as code migrates to {@code java.util.function}, that behavior will
+   * disappear. It is best not to depend on it.
    */
   @Override
   boolean equals(@Nullable Object obj);
