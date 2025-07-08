0d70b3b934339be351c8059f38a368f37d8503c2
Kurt Alfred Kluever <kak@google.com>
2025-07-08 09:06:07 -0700

    Internal change.
    RELNOTES=n/a
    PiperOrigin-RevId: 780585878

--- guava/src/com/google/common/base/Objects.java
+++ guava/src/com/google/common/base/Objects.java
@@ -30,7 +30,6 @@ import org.jspecify.annotations.Nullable;
  */
 @GwtCompatible
 public final class Objects extends ExtraObjectsMethodsForWeb {
-  private Objects() {}
 
   /**
    * Determines whether two possibly-null objects are equal. Returns:
@@ -47,6 +46,7 @@ public final class Objects extends ExtraObjectsMethodsForWeb {
    *
    * <p><b>Note:</b> this method is now unnecessary and should be treated as deprecated; use {@link
    * java.util.Objects#equals} instead.
+   *
    */
   public static boolean equal(@Nullable Object a, @Nullable Object b) {
     return java.util.Objects.equals(a, b);
@@ -72,9 +72,12 @@ public final class Objects extends ExtraObjectsMethodsForWeb {
    *
    * <p><b>Note:</b> this method is now unnecessary and should be treated as deprecated; use {@link
    * java.util.Objects#hash} instead.
+   *
    */
   @SuppressWarnings("nullness") // https://github.com/jspecify/jdk/pull/127
   public static int hashCode(@Nullable Object @Nullable ... objects) {
     return java.util.Objects.hash(objects);
   }
+
+  private Objects() {}
 }
