59c4ecce89a556c205e38d75c4c201b229dc12f3
Laurie Crean <lmcrean@gmail.com>
2025-09-23 15:27:50 -0700

    Improve exception messages for annotation-related methods on synthetic `TypeVariable` instances.
    Fixes #7974
    
    RELNOTES=n/a
    PiperOrigin-RevId: 810604229

--- guava/src/com/google/common/reflect/Types.java
+++ guava/src/com/google/common/reflect/Types.java
@@ -382,6 +382,18 @@ final class Types {
       String methodName = method.getName();
       Method typeVariableMethod = typeVariableMethods.get(methodName);
       if (typeVariableMethod == null) {
+        if (methodName.equals("getAnnotatedBounds")
+            || methodName.equals("isAnnotationPresent")
+            // Each of these prefixes is shared by a family of methods:
+            || methodName.startsWith("getAnnotation")
+            || methodName.startsWith("getDeclaredAnnotation")) {
+          throw new UnsupportedOperationException(
+              "Annotation methods are not supported on synthetic TypeVariables created during type"
+                  + " resolution. The semantics of annotations on resolved types with modified"
+                  + " bounds are undefined. Use the original TypeVariable for annotation access."
+                  + " See b/147144588.");
+        }
+        // If any other method appears or if we forgot one, include it in the exception message:
         throw new UnsupportedOperationException(methodName);
       } else {
         try {
