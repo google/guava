b7e5b88d0a65d3b308e89705e36d7218581d1a8f
Chris Povirk <cpovirk@google.com>
2026-01-02 16:06:23 -0500

    Eliminate Maven transfer logging during doc generation.
    It makes it hard to wade through the output to find the failure. Of
    course, a bigger problem is that the failure sometimes appears *before*
    the giant dump of logs. Maybe we should be letting all the output
    through instead of directing some of it to a log, or maybe we should be
    directing *all* of it to a log?
    
    (The problem I was having most recently is that `cp` was failing.)

--- updaterelease.sh
+++ updaterelease.sh
@@ -119,6 +119,7 @@ generate_javadoc() {
   # Save the build classpath to a file in the tempdir.
   echo -n "Compiling and generating Javadoc for Guava $version..."
   "${mvnw}" \
+    -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
     clean \
     compile \
     javadoc:javadoc \
@@ -422,7 +423,7 @@ main() {
   git_checkout_ref "$RELEASE_REF"
 
   # We run this separately so that its change to the default toolchain doesn't affect anything else.
-  ./mvnw --projects '!guava-testlib,!guava-tests,!guava-bom,!guava-gwt' initialize -P print-java-11-home
+  ./mvnw -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn --projects '!guava-testlib,!guava-tests,!guava-bom,!guava-gwt' initialize -P print-java-11-home
   export JAVA_11_HOME=$(<target/java_11_home)
 
   for flavor in "${FLAVORS[@]}"; do
