google/guava

commit 9e8b9928fd24c60f381d34e2756249ce09a23005

Use mvnw in _util/util.sh.

_util/util.sh
@@ -22,7 +22,7 @@ function current_git_ref {
 
 # Returns the version of com.google.guava:guava at the current revision, pulled from Maven.
 function guava_version {
-  mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate \
+  ./mvnw org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate \
       -Dexpression=project.version \
       -pl guava 2> /dev/null \
       | grep -Ev '(^\[|Download.+:)'

commit 270cd472143c159f6d1bc8231492274a8d57896e

Look for `mvnw` in the parent directory if necessary.
This fixes a bug in the Android behavior that I introduced in
https://github.com/google/guava/commit/9e8b9928fd24c60f381d34e2756249ce09a23005.

_util/util.sh
@@ -22,7 +22,13 @@ function current_git_ref {
 
 # Returns the version of com.google.guava:guava at the current revision, pulled from Maven.
 function guava_version {
-  ./mvnw org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate \
+  local mvnw
+  if [[ -e mvnw ]]; then
+    mvnw=./mvnw
+  else
+    mvnw=../mvnw # We're in the android subdirectory of the main directory.
+  fi
+  "${mvnw}" org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate \
       -Dexpression=project.version \
       -pl guava 2> /dev/null \
       | grep -Ev '(^\[|Download.+:)'