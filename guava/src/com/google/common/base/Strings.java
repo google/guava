1d45ec804609b91743c88200b536ead1981e2a01
Kurt Alfred Kluever <kak@google.com>
2026-06-02 14:52:51 -0700

    fix tiny javadoc typo in `Strings.isNullOrEmpty()`.
    RELNOTES=n/a
    PiperOrigin-RevId: 925592699

--- guava/src/com/google/common/base/Strings.java
+++ guava/src/com/google/common/base/Strings.java
@@ -63,7 +63,7 @@ public final class Strings {
    * <p>Consider normalizing your string references with {@link #nullToEmpty}. If you do, you can
    * use {@link String#isEmpty()} instead of this method, and you won't need special null-safe forms
    * of methods like {@link String#toUpperCase} either. Or, if you'd like to normalize "in the other
-   * direction," converting empty strings to {@code null}, you can use {@link #emptyToNull}.
+   * direction" (converting empty strings to {@code null}), you can use {@link #emptyToNull}.
    *
    * @param string a string reference to check
    * @return {@code true} if the string is null or is the empty string
