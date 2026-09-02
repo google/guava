936fd96f77875f93f71ba1996804000e5c84816f
cpovirk <cpovirk@google.com>
2026-09-02 09:16:11 -0700

    Remove some unused members.
    RELNOTES=n/a
    PiperOrigin-RevId: 975165950

--- guava/src/com/google/common/math/DoubleUtils.java
+++ guava/src/com/google/common/math/DoubleUtils.java
@@ -41,10 +41,6 @@ final class DoubleUtils {
   // Double#doubleToRawLongBits(double)} spec.
   static final long SIGNIFICAND_MASK = 0x000fffffffffffffL;
 
-  // The mask for the exponent, according to the {@link
-  // Double#doubleToRawLongBits(double)} spec.
-  static final long EXPONENT_MASK = 0x7ff0000000000000L;
-
   // The mask for the sign, according to the {@link
   // Double#doubleToRawLongBits(double)} spec.
   static final long SIGN_MASK = 0x8000000000000000L;
